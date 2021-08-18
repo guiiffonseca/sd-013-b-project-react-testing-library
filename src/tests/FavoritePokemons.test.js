df1ec129543ab846de06c988d11cd6742f7edd51
André <andrebpessoa09@gmail.com>
2021-08-18 19:47:26 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import App from '../App';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe(('FavoritePokemons.js tests'), () => {
+  test('Testa se há um texto se não houver pokemons favoritos', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+
+    const favoritesNotFoundText = screen.getByText('No favorite pokemon found');
+    expect(favoritesNotFoundText).toBeInTheDocument();
+  });
+
+  // test('Testa se há cards se houver pokemons favoritos', () => {
+  //   Terminar
+  // });
+});

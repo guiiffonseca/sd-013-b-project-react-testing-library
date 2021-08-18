b6cdda35e2f6e0ee08d728c60d88ada46449c9d2
noel-srocha <manoelrocha.neto.93@hotmail.com>
2021-08-18 16:59:38 -0300

    Challenge 3 completed

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testando a página \'Favorite Pokémons\'', () => {
+  test('Caso não possuir Pokémons favoritos,'
+    + 'aparecer o texto \'No favorite pokemon found\'', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavoritesFound = screen.getByText('No favorite pokemon found');
+    expect(noFavoritesFound).toBeInTheDocument();
+  });
+});

ed053f1e6eb8f62af8729fb0686e682917922646
Guilherme da Costa <guilhermedc93@gmail.com>
2021-08-18 15:19:48 -0300

    finish requiriment 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../helper/renderWithRouter';
 import App from '../App';
 
-describe('Test Favorite Pokemons page', () => {
-  it('should no have any pokemons favorited', () => {
+describe('Test the notFound page when user try a backdoor', () => {
+  it('Should contain "Page requested not found 😭" text content', () => {
     const { history } = renderWithRouter(<App />);
-    history.push('/favorites');
-    expect(screen.getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+    history.push('/anynotfoundedpage');
+    const expectedText = 'Page requested not found 😭';
+    expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(expectedText);
   });
 
-  it('should have the favorited pokemons by the user', () => {
+  it('should contain a img o Pikachu crying with the correct src', () => {
     const { history } = renderWithRouter(<App />);
-    history.push('/pokemons/10');
-    userEvent.click(screen.getByLabelText('Pokémon favoritado?'));
-    userEvent.click(screen.getByRole('link', { name: /Favorite Pokémons/i }));
-    expect(screen.getByText(/Caterpie/i)).toBeInTheDocument();
+    history.push('/anynotfoundedpage');
+    const requestedSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const altText = /Pikachu crying because the page requested was not found/i;
+    expect(screen.getByAltText(altText).src).toBe(requestedSrc);
   });
 });

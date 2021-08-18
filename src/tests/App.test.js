488174bf24c73327a5b2fc28a793b934324b483e
Letícia Castro <aicitelks@gmail.com>
2021-08-18 17:35:57 -0300

    Requisito 1, teste 2/5 OK

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 
 import App from './../App';
@@ -17,4 +18,24 @@ describe('1. Teste o componente <App.js />', () => {
     const linkFavoritesPokemons = screen.getByRole('link', { name: 'Favorite Pokémons' });
     expect(linkFavoritesPokemons).toBeInTheDocument();
   });
+
+  test('Teste se a aplicação é redirecionada para a página inicial, na URL / ao clicar no link Home da barra de navegação.', () => {
+    renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: /home/i, });
+    userEvent.click(linkHome);
+
+    const titleEncounteredPokemons = screen.getByRole('heading', {
+      level: 2,
+      name: /encountered pokémons/i,
+    });
+    expect(titleEncounteredPokemons).toBeInTheDocument();
+
+  });
+
+  test.skip('Teste se a aplicação é redirecionada para a página de About, na URL /about, ao clicar no link About da barra de navegação.', () => {});
+
+  test.skip('Teste se a aplicação é redirecionada para a página de Pokémons Favoritados, na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação.', () => {});
+
+  test.skip('Teste se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida.', () => {});
 });

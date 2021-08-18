5ecdf4a062a464eac754b90506723770850088ba
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-08-18 15:34:35 -0300

    testando

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+describe('Testando o componente Pokedex.js', () => {
+  it('Testa se a página contém um heading h2 com o texto Encontered Pokémons', () => {
+    renderWithRouter(<App />);
+    const h2Pokedex = screen.getByRole('heading', {
+      level: 2,
+      name: 'Encountered pokémons',
+    });
+    expect(h2Pokedex).toBeInTheDocument();
+  });
+});

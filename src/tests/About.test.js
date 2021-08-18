f7aec340334e126fdd5515b02945d06b2df0568a
Alexandre Altgauzen <altgauzen@gmail.com>
2021-08-18 23:18:43 +0100

    requisito 2 em produçao

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from '../utils/renderWithRouter';
+
+describe('Verifica se a página contém as seguintes informações', () => {
+
+  it('informações sobre Pokédex', () => {
+    renderWithRouter(<About />);
+    const aboutPokedex = screen.getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
+    });
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+
+  //  o teste para a imagem foi retirado do site https://dev.to/raphaelchaula/a-simple-image-test-in-react-3p6f
+
+  it('About must have src="https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png"', () => {
+    renderWithRouter(<About />);
+    const img = screen.getByRole('img');
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(img).toHaveAttribute('alt', 'Pokédex');
+  });
+
+  it('', () => {});
+
+  it('', () => {});
+
+});

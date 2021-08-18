8b0813d03d3f8d1541a7513cacb13131e9ad2583
unknown <sergio.espp@gmail.com>
2021-08-18 18:22:43 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Tests 1 - Testing App', () => {
+  it('Testing home link', () => {
+    renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    expect(homeLink).toBeInTheDocument();
+  });
+
+  it('Testing about link', () => {
+    renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    expect(aboutLink).toBeInTheDocument();
+  });
+
+  it('Testing favorite pokémons link', () => {
+    renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: /favorite pokémons/i });
+    expect(aboutLink).toBeInTheDocument();
+  });
+});

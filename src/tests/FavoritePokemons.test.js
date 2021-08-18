e5132aa5ba6f53908e60720c91dfa5924f68aac2
Carol_Inglethe <carolinainglethe@gmail.com>
2021-08-18 19:19:10 -0300

    Requesito 3 - Teste componente <FavoritePokemons.js />

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('mensagem No favorite pokemon found, se pessoa não ter pokémons favoritos', () => {
+    // renderiza:
+    renderWithRouter(<FavoritePokemons />);
+    // testa:
+    expect(screen.getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+
+//   test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+//     // Renderiza :
+//     renderWithRouter(<FavoritePokemons />);
+//     // acessa:
+//     const pokemonName = screen.getByTestId('pokemon-name');
+//     // testes :
+//     expect(pokemonName).toBeInTheDocument();
+//   });
+});

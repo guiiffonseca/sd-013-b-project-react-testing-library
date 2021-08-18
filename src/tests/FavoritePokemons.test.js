2d949d426cf3cec7873fbdd7c47d451f37672151
luis carlos <luizcruz0809@gmail.com>
2021-08-18 17:35:56 -0300

    add 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './util/renderWithRouter';
+
+describe('testando "Favorite Pokemons"', () => {
+  it('testar montagem do componente "FavoritePokemons"', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const msgFavoriteNotFound = screen.getByText(/No favorite pokemon found/i);
+    expect(msgFavoriteNotFound).toBeInTheDocument();
+  });
+});

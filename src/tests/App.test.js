417ee8769744f9bd67c9a97a7cd49f1177d62bf2
rmiguel139 <migmenezes.mm2@gmail.com>
2021-08-18 16:19:28 -0300

    comecando requisitos

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,45 @@
-test('', () => {});
+import React from 'react';
+import { render } from 'react-dom';
+import App from '../App';
+
+Describe('Pokemon test',()=>(
+  test('Testa se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    render(<App />);
+
+      const linkHome = screen.getByRole('Link',{
+          name:/Home/,
+        });
+        userEvent.click(linkHome);
+       const homePage = screen.getByRole('Heading',{
+          level: 2 ,
+          name:/Encountered/ ,
+       })  
+
+        expect(homePage).toBeInTheDocument();
+
+        const linkAbout = screen.getByRole('Link',{
+            name:/About/,
+        });
+        userEvent.click(linkAbout);
+
+        const pageAbout = screen.getByRole('Heading',{
+            level:2,
+            name:/About Pokédex/i,
+        });
+      
+        expect(pageAbout).toBeInTheDocument();
+
+        const linkFavoritePokémons = screen.getByRole('Link',{
+            name:/Favorite Pokémons/,
+        });
+        userEvent.click(linkFavoritePokémons);
+
+        const pageFavoritePokémons = screen.getByRole('Article',{
+            name:/No favorite/,
+        });
+      
+        expect(pageFavoritePokémons).toBeInTheDocument();
+    })
+  
+)
+);

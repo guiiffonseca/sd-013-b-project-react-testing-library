759d6116d7d722c1a23a0bb5ff2bc4b244d53580
gustavoalves23 <gustavo.alves388@gmail.com>
2021-08-18 15:05:28 -0300

    Complementos finais do app

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
@@ -22,4 +22,28 @@ describe('Teste o componente <App.js />', () => {
         expect(getAllByRole.textContent).toBe('Favorite Pokémons');
       });
     });
+  test('É redirecionada para a página inicial, na URL / ao clicar no link', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeButton = screen.getAllByRole('link')[0];
+    expect(history.entries.length).toBe(1);
+    fireEvent.click(homeButton);
+    expect(history.entries.length).toBe(2);
+    expect(history.entries[1].pathname).toBe('/');
+  });
+  test('É redirecionada para a página About, na URL /about ao clicar no link', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutButton = screen.getAllByRole('link')[1];
+    expect(history.entries.length).toBe(1);
+    fireEvent.click(aboutButton);
+    expect(history.entries.length).toBe(2);
+    expect(history.entries[1].pathname).toBe('/about');
+  });
+  test('Redireciona para Pokémons Favoritados, na URL /favorites ao clicar', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoritesButton = screen.getAllByRole('link')[2];
+    expect(history.entries.length).toBe(1);
+    fireEvent.click(favoritesButton);
+    expect(history.entries.length).toBe(2);
+    expect(history.entries[1].pathname).toBe('/favorites');
+  });
 });

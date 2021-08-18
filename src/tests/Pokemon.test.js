5c2e20571a73253da27d81175a6c4dc720676d85
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2021-08-18 20:37:56 -0300

    requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getByRole, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import Pokemon from '../components/Pokemon';
@@ -25,9 +25,8 @@ describe('Testa o componente Pokemon.js', () => {
         map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
       },
     ],
-    summary: 'This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.',
   };
-  test('Teste se é renderizado um card com as informações de determinado pokémon.', () => {
+  test('Teste se é renderizado um card com as informações de determinado pokémon', () => {
     renderWithRouter(<Pokemon pokemon={ pokemon } isFavorite={ false } />);
     const namePokemon = screen.getByTestId('pokemon-name');
     const typePokemon = screen.getByTestId('pokemon-type');
@@ -42,18 +41,23 @@ describe('Testa o componente Pokemon.js', () => {
     expect(pokemonIMG.src).toBe(pokemon.image);
   });
   test('testa se ao clicar em "More Details" ele vai para rota correta', () => {
-    const { history } = renderWithRouter(<Pokemon pokemon={ pokemon } isFavorite={ false } />);
-    const moreDetails = screen.getByRole('link', {name: 'More details', });
+    const { history } = renderWithRouter(
+      <Pokemon
+        pokemon={ pokemon }
+        isFavorite={ false }
+      />,
+    );
+    const moreDetails = screen.getByRole('link', { name: 'More details' });
     userEvent.click(moreDetails);
     const { pathname } = history.location;
     expect(pathname).toBe(`/pokemons/${pokemon.id}`);
   });
+  const boleano = true;
   test('Testa se existe um ícone de estrela nos Pokémons favoritados.', () => {
-    renderWithRouter(<Pokemon pokemon={ pokemon } isFavorite={ true } />);
+    renderWithRouter(<Pokemon pokemon={ pokemon } isFavorite={ boleano } />);
     const altIMG = `${pokemon.name} is marked as favorite`;
     const img = screen.getByAltText(altIMG);
     expect(img.src).toBe('http://localhost/star-icon.svg');
     expect(img.alt).toBe(altIMG);
   });
 });
-

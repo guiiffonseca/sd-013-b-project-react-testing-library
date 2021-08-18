1e46c7e578f7656b6464484bd85484a3260dc8ca
Henrique Lima <hnamix@gmail.com>
2021-08-18 15:05:53 -0400

    requisito01 lint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -9,20 +9,20 @@ test('', () => {
       <App />
     </MemoryRouter>,
   );
-  const heading = getByText(/Pokédex/i);
+  const heading = screen.getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 
-  const home = getByRole('link', {
+  const home = screen.getByRole('link', {
     name: /home/i,
   });
   expect(home).toBeInTheDocument();
 
-  const about = getByRole('link', {
+  const about = screen.getByRole('link', {
     name: /About/i,
   });
   expect(about).toBeInTheDocument();
 
-  const favPokemon = getByRole('link', {
+  const favPokemon = screen.getByRole('link', {
     name: /favorite pokémon/i,
   });
   expect(favPokemon).toBeInTheDocument();

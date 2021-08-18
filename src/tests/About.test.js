3b3e997ddd1603a561a062b08977925dbeeddf4e
Letícia Castro <aicitelks@gmail.com>
2021-08-18 18:55:52 -0300

    Correções lint

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,19 +1,19 @@
 // commit inicial :rocket:
-import { screen } from '@testing-library/dom';
+import { screen } from '@testing-library/react';
 import React from 'react';
 import { About } from '../components';
 import renderWithRouter from './utils/renderWithRouter';
 
 describe('2. Teste o componente <About.js />.', () => {
   test('Teste se a página contém as informações sobre a Pokédex.', () => {
-    renderWithRouter(<About/>);
+    renderWithRouter(<About />);
 
     const aboutPokedex = screen.getByText(/This application simulates a Pokédex/i);
     expect(aboutPokedex).toBeInTheDocument();
   });
 
   test('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
-    renderWithRouter(<About/>);
+    renderWithRouter(<About />);
 
     const titleAboutPokedex = screen.getByRole('heading', {
       level: 2,

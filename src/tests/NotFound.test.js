68766160824bfb97ae43194656349aee57490533
Leonardo <leo_mc_inter@hotmail.com>
2021-08-18 16:54:41 -0300

    Requisito 3

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente NotFound', () => {
+  test('verifica se a página contém um h2 com o texto Page requested not found 😭', () => {
+    render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+
+    expect(
+      screen.getByRole('heading', {
+        level: 2,
+        name: /Page requested not found/,
+      }),
+    ).toBeInTheDocument();
+  });
+
+  test('verifica se a página contém a imagem especificada', () => {
+    render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+
+    const pikachuGif = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(pikachuGif).toHaveAttribute(
+      'src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    );
+    expect(pikachuGif).toBeInTheDocument();
+  });
+});

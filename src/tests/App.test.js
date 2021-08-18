fc69e864df7dc839f83f90c9379f8cad9a467992
André <andrebpessoa09@gmail.com>
2021-08-18 17:00:15 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,80 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('App.js tests', () => {
+  test('Verifica se o topo da aplicação contém um conjunto de links de navegação', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const homeLink = screen.getByRole('link', {
+      name: /Home/i,
+    });
+    userEvent.click(homeLink);
+
+    const aboutLink = screen.getByRole('link', {
+      name: /About/i,
+    });
+    userEvent.click(aboutLink);
+
+    const favoritePokemonsLink = screen.getByRole('link', {
+      name: /Favorite Pokémons/i,
+    });
+    userEvent.click(favoritePokemonsLink);
+  });
+
+  test('Verifica o redirecionamento para a página Home', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/');
+
+    const homePageText = screen.getByRole('heading', {
+      level: 1,
+      name: /Pokédex/i,
+    });
+
+    expect(homePageText).toBeInTheDocument();
+  });
+
+  test('Verifica o redirecionamento para a página About', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
+
+    const aboutPageText = screen.getByRole('heading', {
+      level: 1,
+      name: /Pokédex/i,
+    });
+
+    expect(aboutPageText).toBeInTheDocument();
+  });
+
+  test('Verifica o redirecionamento para a página Pokémons Favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
+
+    const favoritePokemonsPageText = screen.getByRole('heading', {
+      level: 1,
+      name: /Pokédex/i,
+    });
+
+    expect(favoritePokemonsPageText).toBeInTheDocument();
+  });
+
+  test('Verifica se redireciona à página Not Found caso a URL seja desconhecida', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
+
+    const notFoundPageText = screen.getByRole('heading', {
+      level: 1,
+      name: /Pokédex/i,
+    });
+
+    expect(notFoundPageText).toBeInTheDocument();
+  });
+});

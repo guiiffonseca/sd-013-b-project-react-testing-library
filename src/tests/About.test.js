16bc991fadee5d66fed8c03205e13f2d7561d599
Matheus <matheus.pormanhani@gmail.com>
2021-08-18 19:08:25 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { About } from '../components';
+
+describe('Testes da tela About', () => {
+  it('Test Heading', () => {
+    render(
+      <MemoryRouter>
+        <About />
+      </MemoryRouter>,
+    );
+    const heading = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(heading).toBeInTheDocument();
+  });
+  it('Test paragraph', () => {
+    render(
+      <MemoryRouter>
+        <About />
+      </MemoryRouter>,
+    );
+    const paragraph = screen.getAllByText(/Pokémons/);
+    expect(paragraph.length).toBe(2);
+  });
+  it('Test img', () => {
+    render(
+      <MemoryRouter>
+        <About />
+      </MemoryRouter>,
+    );
+    const img = screen.getByRole('img');
+    expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});

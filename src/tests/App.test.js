fa6e4be09a73257f3122fd9964a513b5cee5c0e1
ricand7 <ricantonioj@hotmail.com>
2021-08-18 19:10:56 -0300

    versao 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,3 +1,28 @@
-test('Teste nos links de navagação da página ', () => {
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
 
+describe('Links de navegação', () => {
+  test('Teste se há links de navegação da página ', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const link1 = getByText(/Home/);
+    expect(link1).toBeInTheDocument();
+
+    const link2 = getByText(/About/);
+    expect(link2).toBeInTheDocument();
+
+    const link3 = getByText(/Favorite Pokémons/);
+    expect(link3).toBeInTheDocument();
+  });
+});
+
+describe('Teste dos links', () => {
+  test('Teste se  é redirecionada ao clicar no link Home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    userEvent.click(getByText(/Home/i));
+    const pathname = history.location.pathname;
+    expect(pathname).toBe('/');
+  });
 });

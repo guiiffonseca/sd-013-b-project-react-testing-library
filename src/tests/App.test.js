73accc0508e194ab400c478443e0f878b5791329
JoãoBDf <joaobdf@hotmail.com>
2021-08-18 16:33:57 -0300

    Lint Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,61 +1,59 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 describe('Requisito 1', () => {
   test('se o topo da aplicação contém um conjunto fixo de links de navegação.', () => {
-    const { getByText } = render(
+    render(
       <MemoryRouter>
         <App />
       </MemoryRouter>,
     );
-  
-    const home = getByText(/Home/i);
-    const about = getByText(/About/i);
-    const FavoritePokemon = getByText(/Favorite Pokémons/i);
-  
+    const home = screen.getByText(/Home/i);
+    const about = screen.getByText(/About/i);
+    const FavoritePokemon = screen.getByText(/Favorite Pokémons/i);
+
     expect(home).toBeInTheDocument();
     expect(about).toBeInTheDocument();
     expect(FavoritePokemon).toBeInTheDocument();
   });
 
-  test('se a aplicação é redirecionada para a página inicial, na URL / ao clicar no link', () => {
-    const { getByText } = render(
+  test(`se a aplicação é redirecionada para a página inicial, na URL / 
+  ao clicar no link Home da barra de navegação.`, () => {
+    render(
       <MemoryRouter>
         <App />
       </MemoryRouter>,
     );
-  
-    const getLinkHome = getByText(/Home/i);
-    const getLinkAbout = getByText(/About/i);
-    const getLinkFavoritePokemon = getByText(/Favorite Pokémons/i);
-  
-  
+
+    const getLinkHome = screen.getByText(/Home/i);
+    const getLinkAbout = screen.getByText(/About/i);
+    const getLinkFavoritePokemon = screen.getByText(/Favorite Pokémons/i);
+
     userEvent.click(getLinkHome);
-    const homeLink = getByText(/Encountered pokémons/i);
+    const homeLink = screen.getByText(/Encountered pokémons/i);
     expect(homeLink).toBeInTheDocument();
-  
+
     userEvent.click(getLinkAbout);
-    const aboutLink = getByText(/About Pokédex/i);
+    const aboutLink = screen.getByText(/About Pokédex/i);
     expect(aboutLink).toBeInTheDocument();
-  
+
     userEvent.click(getLinkFavoritePokemon);
-    const FavoritePokemonLink = getByText(/No favorite pokemon found/i);
+    const FavoritePokemonLink = screen.getByText(/No favorite pokemon found/i);
     expect(FavoritePokemonLink).toBeInTheDocument();
   });
-  
-  
-  test('se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida.',
-   () => {
-    const { getByText } = render(
+
+  test(`se a aplicação é redirecionada para a página 
+Not Found ao entrar em uma URL desconhecida.`, () => {
+    render(
       <MemoryRouter initialEntries={ ['/asdasd'] }>
         <App />
-      </MemoryRouter>
+      </MemoryRouter>,
     );
-  
-    const PagDesconhecida = getByText(/Page requested not found/i);
+
+    const PagDesconhecida = screen.getByText(/Page requested not found/i);
     expect(PagDesconhecida).toBeInTheDocument();
   });
-})
+});

70dc54877b3ae5a6bc8c0051e426a85b73f56c1f
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-08-18 15:32:05 -0400

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Component About tests', () => {
+  it('should have a heading with text about pokédex', () => {
+    render(<About />);
+
+    const aboutText = screen.getByRole('heading', {
+      level: 2,
+      name: /about pokédex/i,
+    });
+    expect(aboutText).toBeInTheDocument();
+  });
+
+  it('should have two paragraphs with text about pokédex', () => {
+    render(<About />);
+
+    const paragraphs = screen.getAllByText(/pokémons/i);
+    expect(paragraphs.length).toBe(2);
+  });
+
+  it('should have a image of a pokédex', () => {
+    render(<About />);
+
+    const pokedexImage = screen.getByRole('img');
+    expect(pokedexImage.src).toEqual('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});

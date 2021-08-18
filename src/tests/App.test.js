ce7aea5b689025bc8f496dc8b361a98f126fd1fd
Letícia Castro <aicitelks@gmail.com>
2021-08-18 17:17:36 -0300

    Requisito 1, teste 1/5 OK

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+import App from './../App';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('1. Teste o componente <App.js />', () => {
+  test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação: Home, About, Favorite Pokémons ', () => {
+    renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: 'Home', });
+    expect(linkHome).toBeInTheDocument();
+
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeInTheDocument();
+
+    const linkFavoritesPokemons = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(linkFavoritesPokemons).toBeInTheDocument();
+  });
+});

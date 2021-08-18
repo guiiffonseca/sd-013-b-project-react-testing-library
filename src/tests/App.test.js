82b5250824718ff2255b20187577af855181033e
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-08-18 15:18:03 -0300

    resolve o primeiro requisito da pagina

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,3 +1,59 @@
 import React from 'react';
+import { getByRole, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-test('', () => {});
+describe('testing all links', () => {
+  it('sould test the home link', () => {
+    const { history } = renderWithRouter(<App />);
+    let path = history.location.pathname;
+    expect(path).toBe('/');
+    const link = screen.getByRole('link', {
+      name: 'Home',
+    });
+    expect(link).toBeInTheDocument();
+    userEvent.click(link);
+    path = history.location.pathname;
+    expect(path).toBe('/');
+  });
+
+  it('sould test the about link', () => {
+    const { history } = renderWithRouter(<App />);
+    let path = history.location.pathname;
+    expect(path).toBe('/');
+    const link = screen.getByRole('link', {
+      name: 'About',
+    });
+    expect(link).toBeInTheDocument();
+    userEvent.click(link);
+    path = history.location.pathname;
+    expect(path).toBe('/about');
+  });
+
+  it('sould test the favorites link', () => {
+    const { history } = renderWithRouter(<App />);
+    let path = history.location.pathname;
+    expect(path).toBe('/');
+    const link = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    expect(link).toBeInTheDocument();
+    userEvent.click(link);
+    path = history.location.pathname;
+    expect(path).toBe('/favorites');
+  });
+});
+
+describe('test notFound', () => {
+  it('should be page notfound', async () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/any-pag-enot-found');
+    const path = history.location.pathname;
+    expect(path).toBe('/any-pag-enot-found');
+    const image = screen.getByRole('img', {
+      name: 'Crying emoji',
+    });
+    expect(image).toBeInTheDocument();
+  });
+});

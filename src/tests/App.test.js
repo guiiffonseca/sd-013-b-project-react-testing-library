4b1fc3370d0a93e16723943884285eb7257321ea
Dororops <leeurw@hotmail.com>
2021-08-18 17:49:56 -0300

    Adicionando Requisito-1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,74 @@
-test('', () => {});
+import React from 'react';
+// import { Router } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+// import { createMemoryHistory } from 'history';
+// import { MemoryRouter } from 'react-router';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './utils/renderWithRouter';
+import App from '../App';
+
+describe('Testa o topo da aplicação', () => {
+  test('O primeiro link deve possuir o texto "Home"', () => {
+    const { history } = renderWithRouter(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    history.push('/');
+
+    const linkHomeText = screen.getByRole('link', {
+      name: 'Home',
+    });
+    expect(linkHomeText).toBeInTheDocument();
+
+    userEvent.click(linkHomeText);
+    const homePageText = screen.getByRole('heading', {
+      level: 2,
+      name: 'Encountered pokémons',
+    });
+    expect(homePageText).toBeInTheDocument();
+  });
+
+  test('O segundo link deve possuir o texto "About"', () => {
+    const { history } = renderWithRouter(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    history.push('/');
+
+    const linkAboutText = screen.getByRole('link', {
+      name: 'About',
+    });
+    expect(linkAboutText).toBeInTheDocument();
+
+    userEvent.click(linkAboutText);
+    const AboutPageText = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(AboutPageText).toBeInTheDocument();
+  });
+
+  test('O terceiro link deve possuir o texto "Favorite Pokémons"', () => {
+    const { history } = renderWithRouter(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    history.push('/');
+
+    const linkFavoritePokemonText = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    expect(linkFavoritePokemonText).toBeInTheDocument();
+
+    userEvent.click(linkFavoritePokemonText);
+    const AboutPageText = screen.getByRole('heading', {
+      level: 2,
+      name: 'Favorite pokémons',
+    });
+    expect(AboutPageText).toBeInTheDocument();
+  });
+});

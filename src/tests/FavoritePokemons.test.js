11ae31fa39fa0e9493c866de76268ba64ffc645d
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-08-18 16:11:43 -0400

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import renderWithRouter from './utils/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('Component FavoritePokemons tests', () => {
+  it('should return not found when has not favorite pokemons', () => {
+    render(<FavoritePokemons />);
+
+    const notFoundText = screen.getByText('No favorite pokemon found');
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  it('should ', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+
+    const pokemonsFound = screen.getAllByTestId('pokemon-name');
+    expect(pokemonsFound).not.toHaveLength(0);
+  });
+});

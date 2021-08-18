1d51419ec723261634a986dd4a7af008521e9e76
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2021-08-18 16:43:20 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,15 +1,12 @@
-// import React from 'react';
-// import FavoritePokemons from '../components/FavoritePokemons';
-// import renderWithRouter from '../renderWithRouter';
-// import { screen } from '@testing-library/react';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
 
-// describe('Testa o componente FavoritePokemons.js', () => {
-//   test('Testa se é exibido na tela a mensagem "No favorite pokemon found" ', () => {
-//     renderWithRouter(<FavoritePokemons />);
-//     const noFavoritePokemon = screen.getByText(/No favorite pokemon found/i);
-//     expect(noFavoritePokemon).toBeInTheDocument();
-//   });
-// })
-
-test('', () => {
+describe('Testa o componente FavoritePokemons.js', () => {
+  test('Testa se é exibido na tela a mensagem "No favorite pokemon found" ', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavoritePokemon = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavoritePokemon).toBeInTheDocument();
+  });
 });

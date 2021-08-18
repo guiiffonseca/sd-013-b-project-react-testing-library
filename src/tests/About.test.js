9e184d9bed1a9d010aba085bec1ed7948428cf8c
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-08-18 19:44:52 -0300

    realiza os testes do componente FavoritePokemons

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
 import renderWithRouter from './utils/renderWithRouter';
 import { About } from '../components';
-// import App from '../App';
 
 describe('testing cases for About component', () => {
   test('page have a title named "About Pokédex"', () => {
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,39 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './utils/renderWithRouter';
+import { FavoritePokemons } from '../components';
+import App from '../App';
+
+describe('tests about FavoritePokemons component', () => {
+  test('show the text "no favorite pokemon found" if has no favorite pokemon selected',
+    () => {
+      renderWithRouter(<FavoritePokemons />);
+      const noFavoriteText = screen.getByText(/no favorite pokemon found/i);
+      expect(noFavoriteText).toBeInTheDocument();
+    });
+  test(
+    'select a pokemon as favorite makes her shows on favorite pokémons screen',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      const moreDetailsLink = screen.getByRole('link', {
+        name: /more details/i,
+      });
+      userEvent.click(moreDetailsLink);
+      const { pathname } = history.location;
+      expect(pathname).toBe('/pokemons/25');
+      const favoriteInput = screen.getByRole('checkbox');
+      expect(favoriteInput).toBeInTheDocument();
+      userEvent.click(favoriteInput);
+      const favoritePokemonsLink = screen.getByRole('link', {
+        name: /favorite pokémons/i,
+      });
+      expect(favoritePokemonsLink).toBeInTheDocument();
+      userEvent.click(favoritePokemonsLink);
+      const favoritePath = history.location.pathname;
+      expect(favoritePath).toBe('/favorites');
+      const pokemonName = screen.getByTestId('pokemon-name');
+      expect(pokemonName).toBeInTheDocument();
+    },
+  );
+});

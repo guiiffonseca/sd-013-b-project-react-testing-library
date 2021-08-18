fa7710be27e89cd229981a9d32b9984728d829fa
luis carlos <luizcruz0809@gmail.com>
2021-08-18 16:57:42 -0300

    add 02 paragrafo e title

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import About from '../components/About';
+import renderWithRouter from './util/renderWithRouter';
+
+describe('Testando o "About"', () => {
+  it('Testando o "About"', () => {
+    renderWithRouter(<About />);
+    const headerAboutPokedex = screen.getByRole('heading', {
+      name: /About Pokédex/i,
+      level: 2,
+    });
+    expect(headerAboutPokedex).toBeInTheDocument();
+    const p = screen.getAllByText(/,/i);
+    expect(p.length).toBe(2);
+  });
+});

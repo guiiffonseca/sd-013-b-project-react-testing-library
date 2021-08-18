c65c755c990415c973ab377283e2ff9000d43138
Natalia Camarinha <natcamarinha@gmail.com>
2021-08-18 17:49:03 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,31 +1,71 @@
 import React from 'react';
 // import { fireEvent } from '@testing-library/react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
-import userEvent from '@testing-library/user-event';
 
 describe('testa o componente App', () => {
-  test('teste se o topo da aplicação contém conjunto fixo de links de navegação', () => {
-    renderWithRouter(<App />);
+  describe('testes no link home', () => {
+    test('testa se renderiza o link home', () => {
+      renderWithRouter(<App />);
 
-    const home = screen.getByText(/Home/);
-    expect(home).toBeInTheDocument();
-
-    const about = screen.getByText(/About/);
-    expect(about).toBeInTheDocument();
-
-    const favorite = screen.getByText(/Favorite Pokémons/);
-    expect(favorite).toBeInTheDocument();
-  });
-
-  test('teste se aplicação é redirecionada para pág. inicial ao clicar link Home', () => {
-    renderWithRouter(<App />);
-
-    const linkHome = screen.getByRole('link', {
-      name: '/',
+      const home = screen.getByText(/Home/i);
+      expect(home).toBeInTheDocument();
     });
-    userEvent.click(linkHome);
-    expect(linkHome).toBeInTheDocument();
+
+    test('testa se redireciona para a página /', () => {
+      const { history } = renderWithRouter(<App />);
+
+      const home = screen.getByText(/Home/i);
+      userEvent.click(home);
+      const { pathname } = history.location;
+      // no objeto history tem uma chave location, essa chave location contém outro objeto que tem o pathname, que vai conter a rota.
+      expect(pathname).toBe('/');
+    });
+  });
+  describe('testes no link about', () => {
+    test('testa se renderiza o link about', () => {
+      renderWithRouter(<App />);
+
+      const about = screen.getByText(/About/i);
+      expect(about).toBeInTheDocument();
+    });
+
+    test('testa se redireciona para a página /about', () => {
+      const { history } = renderWithRouter(<App />);
+
+      const about = screen.getByText(/About/i);
+      userEvent.click(about);
+      const { pathname } = history.location;
+      expect(pathname).toBe('/about');
+    });
+  });
+  describe('testes no link favorites', () => {
+    test('testa se renderiza o link favorites', () => {
+      renderWithRouter(<App />);
+
+      const favorites = screen.getByText(/Favorite Pokémons/i);
+      expect(favorites).toBeInTheDocument();
+    });
+    test('testa se redireciona para a página /favorites', () => {
+      const { history } = renderWithRouter(<App />);
+
+      const favorites = screen.getByText(/Favorite Pokémons/i);
+      userEvent.click(favorites);
+      const { pathname } = history.location;
+      expect(pathname).toBe('/favorites');
+    });
+  });
+  describe('testa página não encontrada', () => {
+    test('teste se renderiza Not Found quando url não for encontrada', () => {
+      const { history } = renderWithRouter(<App />);
+
+      history.push('rota-nao-existe');
+      // o .push permite mudar de rota dentro do ambiente de testes.
+
+      const notFound = screen.getByText(/not found/i);
+      expect(notFound).toBeInTheDocument();
+    });
   });
 });

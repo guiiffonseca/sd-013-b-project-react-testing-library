5e53064c70528454d56497a85026ac7f3a127272
alanmdf <alanmoreirascr@gmail.com>
2021-08-18 16:16:53 -0300

    Requisito 2 finalizado

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -23,4 +23,21 @@ describe('About.js tests', () => {
 
     expect(pokedexHeader).toBeInTheDocument();
   });
+
+  test('The page contains two paragraphs of text about Pokedex', () => {
+    renderWithRouter(<About />);
+
+    const paragraphs = screen.getAllByText(/poké/i);
+
+    expect(paragraphs[0]).toBeInTheDocument();
+    expect(paragraphs[1]).toBeInTheDocument();
+  });
+
+  test('The page contains a Pokedex image', () => {
+    renderWithRouter(<About />);
+
+    const pokedexImg = screen.getByRole('img');
+
+    expect(pokedexImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });

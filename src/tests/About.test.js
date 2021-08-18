7a695b95477b2eaff6ad4874132f7a80aaf155ba
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-08-18 16:15:40 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { About } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes no arquivo About.test.js', () => {
+  test('if the page contains info about the Pokédex', () => {
+    renderWithRouter(<About />);
+    const headingAbout = screen.getByRole('heading', {
+      name: /About Pokédex/i,
+      level: 2,
+    });
+    expect(headingAbout).toBeInTheDocument();
+  });
+
+  test('if the page contains an image', () => {
+    renderWithRouter(<About />);
+    const pokedexImg = screen.getByRole('img');
+    expect(pokedexImg).toBeInTheDocument();
+    expect(pokedexImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+
+  test('if the page contains two paragraphs with the text Pokédex', () => {
+    renderWithRouter(<About />);
+    const pokedexParagraph = screen.getAllByText(/pokémons/i);
+    expect(pokedexParagraph).toHaveLength(2);
+  });
+});

6e1a01faa920405b3003ee18eff6ad565e6a9cfa
EddieRAM <edusacarneiro@gmail.com>
2021-08-18 15:57:28 -0300

    Completed first requirement

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+/* import userEvent from '@testing-library/user-event'; */
+import { MemoryRouter } from 'react-router';
+import App from '../App';
+
+describe('should render 3 navigation links on top of the application', () => {
+  it('renders a link containing text "Home"', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+
+    const linkElement = screen.getByText('Home');
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('renders a link containing text "About"', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+
+    const linkElement = screen.getByText('About');
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('renders a link containing text "Favorite Pokémons"', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+
+    const linkElement = screen.getByText('Favorite Pokémons');
+    expect(linkElement).toBeInTheDocument();
+  });
+});

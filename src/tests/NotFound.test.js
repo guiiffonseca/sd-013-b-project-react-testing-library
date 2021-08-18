a04ffd412918d8ea4c66b43e5a80028ed2a19635
noel-srocha <manoelrocha.neto.93@hotmail.com>
2021-08-18 17:08:57 -0300

    Challenge 4 completed

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando a página \'NotFound\'', () => {
+  beforeEach(() => renderWithRouter(<NotFound />));
+  test('Se a página possui texto \'Page requested not found 😭\'', () => {
+    const notFoundText = screen.getByRole('heading',
+      { name: /Page requested not found/i });
+    expect(notFoundText).toBeInTheDocument();
+    const notFoundEmoji = screen.getByRole('img', { name: 'Crying emoji' });
+    expect(notFoundEmoji).toBeInTheDocument();
+  });
+  test('Se a página possui um gif do Pikachu chorando', () => {
+    const gifSource = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const notFoundGif = screen.getAllByRole('img');
+    expect(notFoundGif[1]).toHaveAttribute('src', gifSource);
+  });
+});

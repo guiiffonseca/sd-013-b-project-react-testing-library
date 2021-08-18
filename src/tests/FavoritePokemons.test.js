edbb5ace7deb5a07c6da71ef4c14f2f6d1e39c27
Henrique Lima <hnamix@gmail.com>
2021-08-18 17:07:20 -0400

    requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+test('not found pokemon', () => {
+  renderWithRouter(<FavoritePokemons />);
+  const noFavorite = screen.getByText('No favorite pokemon found');
+  expect(noFavorite).toBeInTheDocument();
+});
+
+test('Pokemon favoritado', () => {
+  renderWithRouter(<App />);
+  const moreDetails = screen.getByRole('link', {
+    name: /More details/i,
+  });
+  userEvent.click(moreDetails);
+
+  const checkbox = screen.getByText('Pokémon favoritado?');
+  userEvent.click(checkbox);
+
+  const weight = screen.getByTestId('pokemon-weight');
+  expect(weight).toBeInTheDocument();
+});

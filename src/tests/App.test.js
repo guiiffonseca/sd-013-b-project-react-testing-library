d6b4e7a61f0d9bf36d01be0fa93ed4f8d04072c6
Lucas T Franca <dev.lucasteixeira@gmail.com>
2021-08-18 15:34:06 -0300

    Requisito 1 Finalizado, com mais teste

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router';
+import { MemoryRouter, Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 describe('Testing App', () => {
-  it('Should be have home', () => {
+  it('Teste se a aplicação contém um conjunto fixo de links de navegação.', () => {
     render(
       <MemoryRouter>
         <App />
@@ -25,5 +27,70 @@ describe('Testing App', () => {
     expect(home).toBeInTheDocument();
     expect(about).toBeInTheDocument();
     expect(pokemon).toBeInTheDocument();
+  });
+
+  it('Teste se a aplicação é redirecionada para a página inicial', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    const home = screen.getByRole('link', {
+      name: 'Home',
+    });
+
+    userEvent.click(home);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it('Teste se a aplicação é redirecionada para a página de About', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    const about = screen.getByRole('link', {
+      name: 'About',
+    });
+
+    userEvent.click(about);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  it('Teste se a aplicação é redirecionada para a página de Pokémons Favoritados', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    const pokemon = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+
+    userEvent.click(pokemon);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  it('Teste se a aplicação é redirecionada para a página de Pokémons Favoritados', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    history.push('/Xablau');
+
+    const notFoundText = screen.getByRole('heading', {
+      name: /not found/i,
+    });
+
+    expect(notFoundText).toBeInTheDocument();
   });
 });

3154aee66997b21b6a48feb068b49d699d8bb10c
Lucas T Franca <dev.lucasteixeira@gmail.com>
2021-08-18 17:26:44 -0300

    Requisito 4 Finalizado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import NotFound from '../components/NotFound';
+
+describe('Testing NotFound', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found.', () => {
+    render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+
+    const notFoundText = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem.', () => {
+    render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+
+    const image = screen.getByAltText(/Pikachu crying/i);
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

e3af4f7a47898b0ca60f2bfece0e7425c2ec2fa1
Alex <alexcorreas1994@gmail.com>
2021-08-18 16:23:10 -0300

    Primeiro Requisito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,3 +1,60 @@
-describre('requisito 1', () => {
-  test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../unit/renderWithRouter';
+import App from '../App';
+
+describe('requisito 1', () => {
+  test('Testa se há links na navegação', () => {
+    renderWithRouter(<App />);
+    const nav = screen.getByRole('navigation');
+    const expectedLinksatNav = 3;
+    expect(nav.childElementCount).toBe(expectedLinksatNav);
+  });
+
+  test('O primeiro link deve possuir o texto Home', () => {
+    renderWithRouter(<App />);
+    const nav = screen.getByRole('navigation');
+    const navLinks1 = nav.childNodes;
+    expect(navLinks1[0].textContent).toBe('Home');
+  });
+
+  test('O segundo link deve possuir o texto About', () => {
+    renderWithRouter(<App />);
+    const nav = screen.getByRole('navigation');
+    const navLinks2 = nav.childNodes;
+    expect(navLinks2[1].textContent).toBe('About');
+  });
+
+  test('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+    const nav = screen.getByRole('navigation');
+    const navLinks3 = nav.childNodes;
+    expect(navLinks3[2].textContent).toBe('Favorite Pokémons');
+  });
+
+  test('Teste se ao clicar em home volta a página inicial', () => {
+    const { history } = renderWithRouter(<App />);
+    const nav = screen.getByRole('navigation');
+    const homeLink = nav.childNodes[0];
+    fireEvent.click(homeLink);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/');
+  });
+
+  test('Teste se ao clicar em About vai para a página About', () => {
+    const { history } = renderWithRouter(<App />);
+    const nav = screen.getByRole('navigation');
+    const AboutLink = nav.childNodes[1];
+    fireEvent.click(AboutLink);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/about');
+  });
+  test('Teste se clicar Favorite Pokémons renderiza na página Favorite Pokémons', () => {
+    const { history } = renderWithRouter(<App />);
+    const nav = screen.getByRole('navigation');
+    const favPokemonLink = nav.childNodes[2];
+    fireEvent.click(favPokemonLink);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/favorites');
+  });
 });

c90b20379d5481ff9e397db841b3dae4611295c4
Natalia Camarinha <natcamarinha@gmail.com>
2021-08-18 17:13:48 -0300

    inicializando o projeto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+// import { fireEvent } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import userEvent from '@testing-library/user-event';
+
+describe('testa o componente App', () => {
+  test('teste se o topo da aplicação contém conjunto fixo de links de navegação', () => {
+    renderWithRouter(<App />);
+
+    const home = screen.getByText(/Home/);
+    expect(home).toBeInTheDocument();
+
+    const about = screen.getByText(/About/);
+    expect(about).toBeInTheDocument();
+
+    const favorite = screen.getByText(/Favorite Pokémons/);
+    expect(favorite).toBeInTheDocument();
+  });
+
+  test('teste se aplicação é redirecionada para pág. inicial ao clicar link Home', () => {
+    renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', {
+      name: '/',
+    });
+    userEvent.click(linkHome);
+    expect(linkHome).toBeInTheDocument();
+  });
+});

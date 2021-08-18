86b093b3ddf56e49d5992b279c1571faa4da9121
GenesisHenriques <genesishenriques7@gmail.com>
2021-08-18 16:35:26 -0300

    Refatorando requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -14,27 +14,48 @@ describe('Testando o componente App.js', () => {
     expect(linkHome).toBeInTheDocument();
     expect(linkAbout).toBeInTheDocument();
     expect(linkFavoritePokemon).toBeInTheDocument();
+  });
 
-    // history.push('/');
+  test('Ao clicar em home deve ser direcionado para "/"', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByText('Home');
     userEvent.click(linkHome);
     const titleHome = screen.getByRole('heading', {
       level: 2,
       name: /Encountered pokémons/i,
     });
     expect(titleHome).toBeInTheDocument();
+  });
 
+  test('Ao clicar em about deve ser direcionado para "/about"', () => {
+    renderWithRouter(<App />);
+    const linkAbout = screen.getByText('About');
     userEvent.click(linkAbout);
     const titleAbout = screen.getByRole('heading', {
       level: 2,
       name: /About Pokédex/i,
     });
     expect(titleAbout).toBeInTheDocument();
+  });
 
+  test('Ao clicar em Favorite pokémons deve ser direcionado para "/favorites"', () => {
+    renderWithRouter(<App />);
+    const linkFavoritePokemon = screen.getByText('Favorite Pokémons');
     userEvent.click(linkFavoritePokemon);
     const titleFavoritePokemon = screen.getByRole('heading', {
       level: 2,
       name: /Favorite pokémons/i,
     });
     expect(titleFavoritePokemon).toBeInTheDocument();
+  });
+
+  test('Ao digitar uma URL desconhecida, redireciona para "Not Found"', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/outraUrl');
+    const titleNotFound = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(titleNotFound).toBeInTheDocument();
   });
 });

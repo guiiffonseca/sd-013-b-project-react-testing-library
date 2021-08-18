08c7bc307fd15e221acd85f2651a06e61d5efe76
Felipe Ventorim <felipeventorim11@gmail.com>
2021-08-18 19:31:10 -0300

    Requisito 4 - test NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import RenderWithRouter from './utils/RenderWithRouter';
+
+describe('Testa o componente NotFound.js', () => {
+  test('se página contém um heading h2 com o texto Page requested not found 😭', () => {
+    const { history } = RenderWithRouter(<App />);
+
+    history.push('/page-not-found');
+    const notFoundText = screen.getByRole('heading', { level: 2 });
+    const img = screen.getByAltText(/pikachu/i);
+    const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(img).toHaveAttribute('src', URL);
+    expect(notFoundText).toHaveTextContent('Page requested not found 😭');
+  });
+
+  test('se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
+    const { history } = RenderWithRouter(<App />);
+
+    history.push('/page-not-found');
+    const img = screen.getByAltText(/pikachu/i);
+    const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(img).toHaveAttribute('src', URL);
+  });
+});

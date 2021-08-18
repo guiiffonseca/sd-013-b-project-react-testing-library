899d8449dfeae8dee9afcabfe72b1ed16ac785b9
eaeniko <eaenikolai@gmail.com>
2021-08-18 16:40:13 -0300

    [Nikolai] fix error eslint

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+// import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import { FavoritePokemons } from '../components';
 
 test('FavoritePokemons.js tests - No fav pokemon found', () => {
-  renderWithRouter(<FavoritePokemons />);
-  
+  // renderWithRouter(<FavoritePokemons />);
+
+
 });

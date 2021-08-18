77c2f4bf079d5d9a7fa2f6a0fdeb307eb7dc1400
Carol_Inglethe <carolinainglethe@gmail.com>
2021-08-18 18:47:01 -0300

    Requesito 2 - Teste componente <About.js />

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,43 @@
-test('', () => {});
-// iniciando projeto
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+describe('Teste o componente <About.js />.', () => {
+  test('Teste se a pagina contém informações sobre pokédex', () => {
+    // Renderiza :
+    renderWithRouter(<About />);
+    // testes :
+    expect(screen.getAllByText(/Pokédex/i)[0]).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    // Renderiza :
+    renderWithRouter(<About />);
+    // Acessa :
+    const heading = screen.getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
+    });
+    // testes:
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém 2 paragrafos com texto sobre Pokédex', () => {
+    // renderiza :
+    renderWithRouter(<About />);
+    // testes :
+    expect(screen.getAllByText(/Pokédex/i).length).toBe(2);
+  });
+
+  test('Teste se a página contém Imagem de uma Pokédex', () => {
+    // renderiza:
+    renderWithRouter(<About />);
+    // acessa  por src:
+    const image = screen.getByAltText('Pokédex');
+    // testes :
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});

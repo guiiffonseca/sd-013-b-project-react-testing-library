67725886bd30649031cf611b75c84655b1deb283
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-08-18 15:14:45 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../helpers/renderWithRouter';
+import pokemons from '../data';
+
+describe('Requirement 3 - Test the favorite pokémons page', () => {
+  it('should no favorite pokemons found text', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notFoundText = screen.getByText('No favorite pokemon found');
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  it('should the favorite pokemon cards render', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+
+    const pokemonCard = screen.getAllByTestId('pokemon-name');
+    expect(pokemonCard.length).not.toBe(0);
+  });
+});

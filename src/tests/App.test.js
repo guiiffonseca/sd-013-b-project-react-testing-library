425e25ca77cb2010006bcf7d06754cd6d604b361
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-08-18 16:11:18 -0300

    implementa primeiro teste do requsito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('Teste o componente <App.js />', () => {
+  it('se contém um conjunto fixo de links de navegação.', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const linkHome = screen.getByRole('link', {
+      name: /Home/i,
+    });
+    expect(linkHome).toBeInTheDocument();
+
+    const linkAbout = screen.getByRole('link', {
+      name: /About/i,
+    });
+    expect(linkAbout).toBeInTheDocument();
+
+    const linkFavorite = screen.getByRole('link', {
+      name: /Favorite Pokémons/i,
+    });
+    expect(linkFavorite).toBeInTheDocument();
+  });
+});

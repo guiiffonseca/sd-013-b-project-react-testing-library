d40175f21e8af875853bdc6533c3551697195e12
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-08-18 16:38:51 -0300

    deleta e reisntala o node modules e atualisa o node para tentar corrigir problemas de avaliacao

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from './utils/renderWithRouter';
+import { About } from '../components';
+// import App from '../App';
+
+describe('testing cases for About component', () => {
+  test('page have infos about the pokedex', () => {
+    renderWithRouter(<About />);
+    const aboutTitle = screen.getByRole('heading', {
+      name: /about pokédex/i,
+      level: 2,
+    });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+});

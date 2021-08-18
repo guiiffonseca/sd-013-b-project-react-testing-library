6069dc5d6c4c15ed0335d4a027556483edb2d8e7
guiiffonseca <guii.ffonseca22@gmail.com>
2021-08-18 19:08:58 -0300

    Inicio Requisito 3 e Requisito 04 Completo

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import renderWithRouter from '../services/helper';
+// import FavoritePokemons from '../components/FavoritePokemons';
+
+// describe('Teste o componente <FavoritePokemons.js', () => {
+//  test('Se é exibido na tela a mensagem No favorite pokemon found', () => {
+//     const { history } = renderWithRouter(<FavoritePokemons />);
+//     history.push('/favorites');
+//     const noFavPokemonMessege = screen.getByText(/No favorite pokemon found/i);
+//     expect(noFavPokemonMessege).toBeInTheDocument();
+//   });
+// })

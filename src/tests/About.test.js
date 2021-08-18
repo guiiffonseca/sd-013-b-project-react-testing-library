2929c6170a57fc800e4a26ee7d8bedf43daaa480
Pedro <pedrorsehn@hotmail.com>
2021-08-18 18:58:13 -0300

    add ex2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import RenderWithRouter from './utils/RenderWithRouter';
+import { About } from '../components';
+
+describe('Testes About', () => {
+  test('Testa se existe textos e imagem na tela', () => {
+    RenderWithRouter(<About />);
+
+    expect(screen.getByText('About Pokédex')).toBeInTheDocument();
+    expect(screen.getByText(/This application simulates a Pokédex/i)).toBeInTheDocument();
+    expect(screen.getByText(/One can filter Pokémons by type/i)).toBeInTheDocument();
+  });
+
+  test('Testa se a imagem existe', () => {
+    RenderWithRouter(<About />);
+
+    const immage = screen.getByRole('img');
+
+    expect(immage).toBeInTheDocument();
+    expect(immage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(immage).toHaveAttribute('alt', 'Pokédex');
+  });
+});

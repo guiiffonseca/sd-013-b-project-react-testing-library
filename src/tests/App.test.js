30c3eba5fc0792947ac80b6d9dd21654305f0b67
enioandrade92 <enio.goulart@gmail.com>
2021-08-18 16:48:44 -0300

    iniciando o projeto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,8 @@
-test('', () => {});
+import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('Test', () => {
+  test('Rederiza o App', () => { render(<App />); });
+});

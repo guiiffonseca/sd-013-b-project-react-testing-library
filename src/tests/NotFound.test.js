704a0bad0b356a67c43dd44832f82efe099f184d
rmiguel139 <migmenezes.mm2@gmail.com>
2021-08-18 17:17:18 -0300

    req4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderRouter from '../services/renderWithRouter';
+
+describe('Teste do componente notFound', () => {
+  test('Teste se a página contem um heading h2 com o texto page request not found', () => {
+    renderRouter(<NotFound />);
+    const notFound = screen.getByRole('heading', {
+      name: /Page requested not found/,
+      level: 2,
+    });
+    expect(notFound).toBeInTheDocument();
+  });
+  test('Teste se página mostra a imagem', () => {
+    renderRouter(<NotFound />);
+    const notFoundImage = screen.getByAltText(/Pikachu crying because the page /);
+    expect(notFoundImage).toBeInTheDocument();
+    expect(notFoundImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

a15f80e3bfc1cd98e07d3cf24e25e3ba1475226a
Jeferson Oliveira <jeferson.contas@gmail.com>
2021-08-18 20:07:18 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { About } from '../components';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('Teste About', () => {
+  test('Teste se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const infoPage = screen.getByRole('heading', { name: /About Pokédex/i });
+    expect(infoPage).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const infoImg = screen.getByRole('img');
+    expect(infoImg.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});

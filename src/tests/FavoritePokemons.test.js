932661e13eeb2f2f32863974badb33b22fb67516
marcus <marcus.rodrigues10@hotmail.com>
2021-08-18 16:00:09 -0300

    requirement 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Testa o componente FavoritePokemons', () => {
+  test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const textNotFound = screen.getByText(/No favorite pokemon found/i);
+    expect(textNotFound).toBeInTheDocument();
+  });
+});

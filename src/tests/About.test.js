f1301f2e2352359c2c46fc0bd8a02eb097837c77
Letícia Castro <aicitelks@gmail.com>
2021-08-18 18:35:29 -0300

    Requisito 2, teste 2/4 OK

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,28 @@
 // commit inicial :rocket:
-test('', () => {});
+import { screen } from '@testing-library/dom';
+import React from 'react';
+import { About } from '../components';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('2. Teste o componente <About.js />.', () => {
+  test('Teste se a página contém as informações sobre a Pokédex.', () => {
+    renderWithRouter(<About/>);
+
+    const aboutPokedex = screen.getByText(/This application simulates a Pokédex/i);
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+    renderWithRouter(<About/>);
+
+    const titleAboutPokedex = screen.getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
+    });
+    expect(titleAboutPokedex).toBeInTheDocument();
+  });
+  test.skip('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {});
+  test.skip('Teste se a página contém a imagem de uma Pokédex', () => {
+    // https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.
+  });
+});

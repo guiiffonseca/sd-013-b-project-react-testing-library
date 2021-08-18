ac569cdfe5e8da08cfb49d9cca77d2fc3304e3ad
Letícia Castro <aicitelks@gmail.com>
2021-08-18 18:52:47 -0300

    Requisito 2, teste 4/4 OK

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -29,7 +29,10 @@ describe('2. Teste o componente <About.js />.', () => {
     expect(paragraphs).toHaveLength(2);
   });
 
-  test.skip('Teste se a página contém a imagem de uma Pokédex', () => {
-    // https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.
+  test('Teste se a página contém a imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const image = screen.getByAltText(/pokédex/i);
+    expect(image).toBeInTheDocument();
   });
 });

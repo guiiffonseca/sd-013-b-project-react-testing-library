a73039181fa72a0d130cfb670b9cc0a3473595b3
Eduardo Luiz Prando <eduardoprando159@hotmail.com>
2021-08-18 19:46:52 -0300

    req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { FavoritePokemons } from '../components';
+import App from '../App';
+import renderWithRouter from './helper/renderWithRouter';
+
+describe('Test FavoritePokemons Component', () => {
+  test('if message appear "No favorite pokemon found" when there is no favorite', () => {
+    renderWithRouter(<FavoritePokemons />);
+    expect(screen.getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+  test('if there is a favorite pokemon after select it', () => {
+    renderWithRouter(<App />);
+    userEvent.click(screen.getByText(/more details/i));
+    userEvent.click(screen.getByRole('checkbox'));
+    userEvent.click(screen.getByText(/Favorite Pokémons/i));
+    expect(screen.getByTestId('pokemon-name')).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from './helper/renderWithRouter';
+
+describe('NotFound component:', () => {
+  beforeEach(() => {
+    renderWithRouter(<NotFound />);
+  });
+  test('has a h2 whit text "Page requested not found:😭"', () => {
+    expect(screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    })).toBeInTheDocument();
+  });
+  test('has a img', () => {
+    expect(screen.getAllByRole('img')[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

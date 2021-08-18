f2cf2b3f5b660180ad23d1ea0113fa3fc45756e6
Guilherme da Costa <guilhermedc93@gmail.com>
2021-08-18 16:00:49 -0300

    finish requiriment 05

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,72 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helper/renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Test Pokedex', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+
+  const nextButton = 'Próximo pokémon';
+  const filters = ['Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+  const pokemonName = 'pokemon-name';
+
+  it('should have a h2 element with Encountered pokémons content', () => {
+    expect(screen.getByRole('heading', { level: 2 }))
+      .toHaveTextContent('Encountered pokémons');
+  });
+
+  it('should show the next pokemon when click in "Próximo pokémon"', () => {
+    expect(screen.getByText(pokemons[0].name)).toBeInTheDocument();
+    userEvent.click(screen.getByRole('button', { name: nextButton }));
+    expect(screen.getByText(pokemons[1].name)).toBeInTheDocument();
+  });
+
+  it('Should show the first one when press "Próximo pokémon" on the last one', () => {
+    expect(screen.getByText(pokemons[0].name)).toBeInTheDocument();
+    for (let c = 0; c < pokemons.length; c += 1) {
+      userEvent.click(screen.getByRole('button', { name: nextButton }));
+    }
+    expect(screen.getByText(pokemons[0].name)).toBeInTheDocument();
+  });
+
+  it('Should show just one pokémon per time in pokedex', () => {
+    for (let c = 0; c < pokemons.length; c += 1) {
+      expect(screen.getAllByTestId(pokemonName).length).toBe(1);
+      userEvent.click(screen.getByRole('button', { name: nextButton }));
+      expect(screen.getAllByTestId(pokemonName).length).toBe(1);
+    }
+  });
+
+  it('should contains all the filter buttons requested', () => {
+    filters.forEach((filter) => {
+      expect(screen.getByRole('button', { name: filter })).toBeInTheDocument();
+    });
+  });
+
+  it('Should show just filtered pokémons and always show all option', () => {
+    filters.forEach((filter) => {
+      userEvent.click(screen.getByRole('button', { name: filter }));
+      const filteredPokemons = pokemons.filter(({ type }) => type === filter);
+      filteredPokemons.forEach(({ type }) => {
+        expect(screen.getByRole('button', { name: 'All' })).toBeInTheDocument();
+        expect(screen.getByTestId('pokemon-type')).toHaveTextContent(type);
+        userEvent.click(screen.getByRole('button', { name: nextButton }));
+      });
+    });
+  });
+
+  it('Should show all pokemons when the filter "All" is pressed', () => {
+    pokemons.forEach(({ name }) => {
+      expect(screen.getByTestId(pokemonName)).toHaveTextContent(name);
+      userEvent.click(screen.getByRole('button', { name: nextButton }));
+    });
+  });
+
+  it('should start with "All" filter selected', () => {
+    userEvent.click(screen.getByTestId(''));
+  });
+});

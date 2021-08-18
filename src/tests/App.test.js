5feda0df0ad40400e008d4a332c7ad19a5cfe83e
iroDevs <ironegrolol@gamil.com>
2021-08-18 14:51:18 -0300

    primeiro commit

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+describe('Teste App ', () => {
+  test('os links estão aparecendo', () => {
+
+  });
+});

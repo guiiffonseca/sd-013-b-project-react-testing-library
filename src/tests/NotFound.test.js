93e4e8a172a896b1399feb2442e833766e6eca1f
diogodlemos <diogodrummond@hotmail.com>
2021-08-18 19:13:30 -0300

    Requisito 4 finalizado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import rendeRWithRouter from '../utils/renderWithRouter';
+import App from '../App';
+
+describe('Test of component <notFound />', () => {
+  test('Verify if this page contains text "Page requested not found"', () => {
+    const { history } = rendeRWithRouter(<App />);
+    history.push('/route-no-exists');
+    const notFoundText = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(notFoundText).toBeInTheDocument();
+  });
+  test('Verify if in this page have image error ', () => {
+    const { history } = rendeRWithRouter(<App />);
+    history.push('/route-no-exists');
+    const imgError = screen.getByAltText(/Pikachu crying because the page requested/i);
+    expect(imgError).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

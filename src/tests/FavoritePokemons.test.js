eaa2a3f366d6ce749f10bc2ed4e7e7cb2d1a5f77
Arthur-Jr <arthurjr1906@gmail.com>
2021-08-18 19:26:04 -0300

    Resolve o terceiro requisito.

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Favorite Pokémons page tests', () => {
+  test('If the page is empty', () => {
+    render(<FavoritePokemons />);
+    const text = screen.getByText(/No favorite pokemon found/i);
+    expect(text).toBeInTheDocument();
+  });
+
+  test('If favorite pokémon page has something after user interaction', () => {
+    renderWithRouter(<App />);
+    const details = screen.getByRole('link', { name: /more details/i });
+
+    expect(details).toBeInTheDocument();
+    userEvent.click(details);
+
+    const input = screen.getByLabelText(/pokémon favoritado?/i);
+    expect(input).toBeInTheDocument();
+    userEvent.click(input);
+
+    const favorite = screen.getByRole('link', { name: /favorite pokémons/i });
+    expect(favorite).toBeInTheDocument();
+    userEvent.click(favorite);
+
+    const pokemon = screen.getByText(/pikachu/i);
+    expect(pokemon).toBeInTheDocument();
+  });
+});

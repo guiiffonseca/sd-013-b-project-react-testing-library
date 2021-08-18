f7afbd15395e4ba644eb161cab88310d8a23a170
eaeniko <eaenikolai@gmail.com>
2021-08-18 15:17:19 -0300

    [Nikolai] Req 1 - Links

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('App.js tests - Expect Home, About and Favorites Pokemons links', () => {
+  renderWithRouter(<App />);
+  const homeLink = screen.getByText(/home/i);
+  expect(homeLink).toBeInTheDocument();
+
+  const aboutLink = screen.getByText(/about/i);
+  expect(aboutLink).toBeInTheDocument();
+
+  const favPokemonsLink = screen.getByText(/favorite/i);
+  expect(favPokemonsLink).toBeInTheDocument();
+});

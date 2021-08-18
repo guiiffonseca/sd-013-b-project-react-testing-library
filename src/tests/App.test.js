824b4b5333cbde7fe21233df02c1cf98a4dd5253
EddieRAM <edusacarneiro@gmail.com>
2021-08-18 16:26:46 -0300

    testing submission

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -6,21 +6,33 @@ import App from '../App';
 
 describe('should render 3 navigation links on top of the application', () => {
   it('renders a link containing text "Home"', () => {
-    render(<MemoryRouter><App /></MemoryRouter>);
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
 
     const linkElement = screen.getByText('Home');
     expect(linkElement).toBeInTheDocument();
   });
 
   it('renders a link containing text "About"', () => {
-    render(<MemoryRouter><App /></MemoryRouter>);
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
 
     const linkElement = screen.getByText('About');
     expect(linkElement).toBeInTheDocument();
   });
 
   it('renders a link containing text "Favorite Pokémons"', () => {
-    render(<MemoryRouter><App /></MemoryRouter>);
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
 
     const linkElement = screen.getByText('Favorite Pokémons');
     expect(linkElement).toBeInTheDocument();

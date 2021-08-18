966f10d83f504f17b0815cb64bfa31382a25ee6a
luis carlos <luizcruz0809@gmail.com>
2021-08-18 18:08:21 -0300

    add 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './util/renderWithRouter';
+
+describe('Testando o "NotFound"', () => {
+  it('Testando o "NotFound"', () => {
+    renderWithRouter(<NotFound />);
+    const h2 = screen.getByRole('heading', {
+      name: /Page requested not found /i,
+      level: 2,
+    });
+    expect(h2).toBeInTheDocument();
+    const url = ('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    const alt = (/Pikachu crying because the page requested was not found/i);
+    const img = screen.getByAltText(alt);
+    expect(img.src).toBe(url);
+    expect(img).toBeInTheDocument();
+  });
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,8 @@
-test('', () => {});
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// // import userEvent from '@testing-library/user-event';
+// import Pokedex from '../components/Pokedex';
+// import renderWithRouter from './util/renderWithRouter';
+
+test('Testando o "Pokedex"', () => {
+});

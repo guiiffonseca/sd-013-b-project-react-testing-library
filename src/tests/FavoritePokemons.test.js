4f765fa7656dfb32ce445327006750b42f94e4e8
Lopes <gus.lcampos38@gmail.com>
2021-08-18 18:40:40 -0300

    finaliza terceiro requisito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../Helper/RenderWithRouter';
+import pokemons from '../data';
+
+describe('Testing FavoritePokemons.js component', () => {
+  it('should display no fav pok found if there are none', ()=> {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavFound = screen.getByText('No favorite pokemon found');
+    expect(noFavFound).toBeInTheDocument();
+  });
+
+  it('should display fav poke when receveid poke as prop', ()=> {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    const favPokeImg = screen.getAllByRole('img');
+    expect(favPokeImg.length).not.toBe(0);
+  });
+});

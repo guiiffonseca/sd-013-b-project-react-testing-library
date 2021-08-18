6a6797e3a1d0d2dc1674bceed7d2337ea4c540aa
LucianoJunior <lucianojunior4242@gmail.com>
2021-08-18 18:43:20 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { About } from '../components';
+
+describe('About.js tests', () => {
+  beforeEach(() => {
+    render(<About />);
+  });
+
+  it('should have a heading h2 with the text "About Pokédex"', () => {
+    expect(
+      screen.getByRole('heading', { level: 2, name: 'About Pokédex' }),
+    ).toBeInTheDocument();
+  });
+
+  it('should have two paragraphs with text about pokedex', () => {
+    expect(screen.getAllByText(/Pokémons/).length).toBe(2);
+  });
+
+  it('should have a specific image', () => {
+    const img = screen.getByAltText('Pokédex');
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});

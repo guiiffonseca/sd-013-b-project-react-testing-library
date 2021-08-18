4dd402e38933438f746fe5134772af5be8135362
LucianoJunior <lucianojunior4242@gmail.com>
2021-08-18 19:29:51 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('NotFound.js tests', () => {
+  beforeEach(() => {
+    render(<NotFound />);
+  });
+
+  it('should have a heading h2 with the text "Page requested not found 😭"', () => {
+    expect(
+      screen.getByRole('heading', { level: 2, name: /Page requested not found/ }),
+    ).toBeInTheDocument();
+  });
+
+  it('should show a picture', () => {
+    const img = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

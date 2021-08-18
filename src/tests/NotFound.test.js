f0e0b37353990984c3b5d7c451eb150228bbd8fc
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-08-18 20:30:22 -0300

    realiza os testes de comportamento do componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './utils/renderWithRouter';
+import { NotFound } from '../components';
+
+describe('testing cases of NotFound component', () => {
+  test('has the text "Page request not found"', () => {
+    const { history } = renderWithRouter(<NotFound />);
+    history.push('/path-undefined');
+    const notFoundText = screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    });
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  test('has the image with right alt and src', () => {
+    const { history } = renderWithRouter(<NotFound />);
+    history.push('/path-undefined');
+    const notFoundPageImage = screen.getByAltText(/Pikachu crying/i);
+    expect(notFoundPageImage).toBeInTheDocument();
+    expect(notFoundPageImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

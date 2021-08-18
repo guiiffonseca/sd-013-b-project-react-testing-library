cb602e6255871ff54f590ccc65819de6328020cb
Henrique Lima <hnamix@gmail.com>
2021-08-18 15:02:33 -0400

    requisito01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+test('', () => {
+  const { getByText, getByRole } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const heading = getByText(/Pokédex/i);
+  expect(heading).toBeInTheDocument();
+
+  const home = getByRole('link', {
+    name: /home/i,
+  });
+  expect(home).toBeInTheDocument();
+
+  const about = getByRole('link', {
+    name: /About/i,
+  });
+  expect(about).toBeInTheDocument();
+
+  const favPokemon = getByRole('link', {
+    name: /favorite pokémon/i,
+  });
+  expect(favPokemon).toBeInTheDocument();
+});

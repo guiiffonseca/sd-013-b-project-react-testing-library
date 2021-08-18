f6a4e28ca93db27c6c6d383c765a3414dba07ed0
alanmdf <alanmoreirascr@gmail.com>
2021-08-18 16:53:50 -0300

    Requisito 4 finalizado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import NotFound from '../components/NotFound';
+import renderWithRouter from './helpers/renderWithRouter';
+
+describe('NotFound.js tests', () => {
+  test('The page contains a header with the text "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundHeader = screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    });
+
+    expect(notFoundHeader).toBeInTheDocument();
+  });
+  test('The page contains a Pikachu crying gif', () => {
+    renderWithRouter(<NotFound />);
+
+    const pikachuGif = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+
+    expect(pikachuGif).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

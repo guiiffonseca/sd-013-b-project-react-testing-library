77b74f5521fad282503bc1614378c63876e25f5b
Guilherme da Costa <guilhermedc93@gmail.com>
2021-08-18 15:03:47 -0300

    finish requiriment 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helper/renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Test Favorite Pokemons page', () => {
+  it('should no have any pokemons favorited', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+    expect(screen.getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+
+  it('should have the favorited pokemons by the user', () => {
+    const { history } = renderWithRouter(<App />);
+    const [pokemon] = pokemons;
+    history.push(`/pokemons/${pokemon.id}`);
+    userEvent.click(screen.getByLabelText('Pokémon favoritado?'));
+    userEvent.click(screen.getByRole('link', { name: /Favorite Pokémons/i }));
+    expect(screen.getByText(pokemon.name)).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helper/renderWithRouter';
+import App from '../App';
+
+describe('Test Favorite Pokemons page', () => {
+  it('should no have any pokemons favorited', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+    expect(screen.getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+
+  it('should have the favorited pokemons by the user', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/10');
+    userEvent.click(screen.getByLabelText('Pokémon favoritado?'));
+    userEvent.click(screen.getByRole('link', { name: /Favorite Pokémons/i }));
+    expect(screen.getByText(/Caterpie/i)).toBeInTheDocument();
+  });
+});

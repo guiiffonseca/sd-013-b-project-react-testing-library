93c2a40a5a5e696c944f85968eada62b12b944ad
marcus <marcus.rodrigues10@hotmail.com>
2021-08-18 16:13:02 -0300

    requirement 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Testa o Componente Not Found', () => {
+  beforeEach(() => {
+    render(<NotFound />);
+  });
+
+  test('Teste se contém um heading h2 com o texto Page requested not found', () => {
+    const heading = screen.getByRole('heading', {
+      level: 2,
+      name: 'Page requested not found Crying emoji',
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem', () => {
+    const image = screen.getByRole('img', { name:
+      'Pikachu crying because the page requested was not found',
+    });
+    expect(image.src).toStrictEqual('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

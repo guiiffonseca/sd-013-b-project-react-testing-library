5b2df3b7208b88256b6cce4d11d4f3cb4da38b1f
Jessica Naiara <jessicanaiarag@gmail.com>
2021-08-18 16:08:41 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import { screen, fireEvent } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o App.js', () => {
+  test('Testa se contém um conjunto de links de navegação', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkHome = screen.getByText(/Home/);
+    fireEvent.click(linkHome);
+    let url = history.location.pathname;
+    expect(url).toBe('/');
+
+    const linkAbout = screen.getByText(/About/);
+    fireEvent.click(linkAbout);
+    url = history.location.pathname;
+    expect(url).toBe('/about');
+
+    const linkFavorite = screen.getByText(/Favorite Pokémons/);
+    fireEvent.click(linkFavorite);
+    url = history.location.pathname;
+    expect(url).toBe('/favorites');
+  });
+});

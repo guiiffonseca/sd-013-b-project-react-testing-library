8be27d71984a72aac972ecfa91aa947cb1ac1416
Guilherme da Costa <guilhermedc93@gmail.com>
2021-08-18 14:48:47 -0300

    make helper file and finish requiriment 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helper/renderWithRouter';
+import App from '../App';
+
+describe('Testing if the top of application have three fixed links', () => {
+
+  it('should have three requested links', () => {
+    renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    expect(links[0]).toHaveTextContent(/Home/i);
+    expect(links[1]).toHaveTextContent(/About/i);
+    expect(links[2]).toHaveTextContent(/Favorite pokémons/i);
+  });
+
+  it('should redirect to home path when click in home link', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: /Home/i });
+    userEvent.click(homeLink);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/');
+  });
+
+  it('should redirect to about path when click in about link', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    userEvent.click(aboutLink);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/about');
+  });
+
+  it('shoud redirect to favorites path when click in favorite link', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoriteLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(favoriteLink);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/favorites');
+  });
+});

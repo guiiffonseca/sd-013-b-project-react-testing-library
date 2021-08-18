509c473b4a6a0c3c46bec19014bef0f6d6b7ef19
marcus <marcus.rodrigues10@hotmail.com>
2021-08-18 15:01:05 -0300

    requirement 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Teste se contém um conjunto fixo de links de navegação', () => {
+  test('Teste se a aplicação é redirecionada para a página inicial', () => {
+    const { history } = renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: /Home/i });
+    expect(home).toBeDefined();
+
+    fireEvent.click(home);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('Teste se a aplicação é redirecionada para a página de About.', () => {
+    const { history } = renderWithRouter(<App />);
+    const about = screen.getByRole('link', { name: /About/i });
+    expect(about).toBeDefined();
+
+    fireEvent.click(about);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('Teste se é redirecionada para a página de Pokémons Favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoritePokemons = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(favoritePokemons).toBeDefined();
+
+    fireEvent.click(favoritePokemons);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+});

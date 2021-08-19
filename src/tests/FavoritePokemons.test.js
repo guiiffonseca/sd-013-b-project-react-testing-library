c0d4ee9f1d330ceec4d657c20cbc1cf0490e7e3e
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-19 18:23:34 -0300

    Requisito 3 - test 1 - Exibe  quando não há pokemons favoritados

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Favorite =Pokemons funciona corretamente', () => {
+  it('Exibe `No favorite pokemon found` quando não há pokemons favoritados', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noFavoritedMessage = screen.getByText('No favorite pokemon found');
+
+    expect(noFavoritedMessage).toBeInTheDocument();
+  });
+
+  it('', () => {
+
+  });
+});

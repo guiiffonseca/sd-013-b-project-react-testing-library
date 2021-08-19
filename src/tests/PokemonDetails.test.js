9b2f5f1bab3d530c298515472b9456cc759c234b
JoãoBDf <joaobdf@hotmail.com>
2021-08-19 18:44:02 -0300

    Requisito 7 Finalizado Graças a Deus

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { screen, render } from '@testing-library/react';
+import App from '../App';
+
+describe('Requisito 7', () => {
+  test('Teste se as informações About Pokemon aparecem na tela', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/pokemons/4'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const game = screen.getByText(/Game Locations of Charmander/i);
+    expect(game).toBeInTheDocument();
+
+    const map = screen.getAllByAltText(/Charmander location/i);
+    expect(map[0]).toBeInTheDocument();
+    expect(map[0]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png');
+
+    const detailsPokemon = screen.getByText(/Charmander Details/i);
+    expect(detailsPokemon).toBeInTheDocument();
+
+    const paragraph = screen.getByText(/The flame on its tail shows /i);
+    expect(paragraph).toBeInTheDocument();
+
+    const sumary = screen.getByRole('heading', {
+      level: 2,
+      name: 'Summary',
+    });
+    expect(sumary).toBeInTheDocument();
+
+    const favorite = screen.getByText(/favoritado/i);
+    expect(favorite).toBeInTheDocument();
+  });
+});

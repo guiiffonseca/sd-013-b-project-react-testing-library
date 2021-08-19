5dd0a4fc977a25a7a501cbcccca543e31bccd761
andre9653 <andresena9653@gmail.com>
2021-08-18 22:18:18 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+test('Testa pagina Not Found', () => {
+  renderWithRouter(<NotFound />);
+
+  expect(screen.getByRole('heading', { name: /Page requested not found/i, level: 2 }));
+  expect(screen.getAllByRole('img')[1].src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif')
+});

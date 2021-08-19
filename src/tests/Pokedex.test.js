f7d4832c025bc659e129fc0ab2d5bff21309c04d
Henrique Lima <hnamix@gmail.com>
2021-08-19 16:38:37 -0400

    requisito05

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+test('H2 com texto', () => {
+  renderWithRouter(<App />);
+  const text = screen.getByText(/Encountered pokémons/i);
+  expect(text).toBeInTheDocument();
+});
+test('Botão e filtro filtro', () => {
+  renderWithRouter(<App />);
+  const resetBtn = screen.getByRole('button', { name: 'All' });
+  expect(resetBtn).toBeInTheDocument();
+});

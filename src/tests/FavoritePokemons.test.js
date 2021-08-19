851d64d8d5807b7467d57c0d549367f700ebe068
guiiffonseca <guii.ffonseca22@gmail.com>
2021-08-19 15:38:22 -0300

    Requisito 3 finalizado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,14 +1,29 @@
-// import React from 'react';
-// import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import renderWithRouter from '../services/helper';
-// import FavoritePokemons from '../components/FavoritePokemons';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../services/helper';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
 
-// describe('Teste o componente <FavoritePokemons.js', () => {
-//  test('Se é exibido na tela a mensagem No favorite pokemon found', () => {
-//     const { history } = renderWithRouter(<FavoritePokemons />);
-//     history.push('/favorites');
-//     const noFavPokemonMessege = screen.getByText(/No favorite pokemon found/i);
-//     expect(noFavPokemonMessege).toBeInTheDocument();
-//   });
-// })
+describe('Teste o componente <FavoritePokemons.js', () => {
+  test('Se é exibido na tela a mensagem No favorite pokemon found', () => {
+    const { history } = renderWithRouter(<FavoritePokemons />);
+    history.push('/favorites');
+    const noFavPokemonMessege = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavPokemonMessege).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/');
+    const moreDetailsLink = screen.getByRole('link', { name: /More Details/i });
+    userEvent.click(moreDetailsLink);
+
+    const favoritePokeButton = screen.getByText(/Pokémon favoritado/i);
+    userEvent.click(favoritePokeButton);
+
+    renderWithRouter(<FavoritePokemons />);
+    const pokemonId = screen.getByTestId(/pokemon-name/i);
+    expect(pokemonId).toBeInTheDocument();
+  });
+});

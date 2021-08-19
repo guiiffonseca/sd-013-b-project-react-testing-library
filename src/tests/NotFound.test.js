1410fc2e252f4dbc532f25a4d9f5324d1538c7be
GenesisHenriques <genesishenriques7@gmail.com>
2021-08-19 17:06:11 -0300

    Requisito 4 pronto

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../utils/renderWithRouter';
+import App from '../App';
+
+describe('Testando o componente NotFound.js', () => {
+  test('Teste se contém um heading h2 com o texto Page requested not found 😭', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/algo qualquer');
+    const titleNotFound = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(titleNotFound).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/algo qualquer');
+
+    const imgNotFound = screen.getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found',
+    });
+
+    expect(imgNotFound).toBeInTheDocument();
+    expect(imgNotFound.src).toStrictEqual('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

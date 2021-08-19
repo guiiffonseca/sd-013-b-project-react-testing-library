b43e9e42df11ea2a0c4fdce5c9d74f993e9ff521
Henrique Cursino <henriquecursino46@gmail.com>
2021-08-19 15:21:42 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste <NotFound.js />', () => {
+  test('Verifica se renderiza um h2 na página', () => {
+    renderWithRouter(<NotFound />);
+
+    const headerText = screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    });
+
+    expect(headerText).toBeInTheDocument();
+  });
+
+  test('Verifica se mostra a imagem na página', () => {
+    renderWithRouter(<NotFound />);
+
+    const pikachuImg = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+
+    expect(pikachuImg).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

a91958325d1090378c6889f76ab9bbfec1801911
andre9653 <andresena9653@gmail.com>
2021-08-19 16:58:02 -0300

    Requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,39 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import { Pokemon } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+const pokemon = {
+  id: 25,
+  name: 'Pikachu',
+  type: 'Electric',
+  averageWeight: {
+    value: '6.0',
+    measurementUnit: 'kg',
+  },
+  image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+  moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+};
+
+test('Teste se é renderizado um card com as informações de determinado pokémon.', () => {
+  renderWithRouter(<App />);
+  expect(screen.getByTestId('pokemon-name')).toBeInTheDocument();
+  expect(screen.getByTestId('pokemon-weight')).toBeInTheDocument();
+  expect(screen.getByRole('img').src).toContain(pokemon.image);
+  expect(screen.getByRole('img').alt).toContain(`${pokemon.name} sprite`);
+  const linkMoreDetails = screen.getByRole('link', { name: /more details/i });
+  expect(linkMoreDetails.href).toContain(`pokemons/${pokemon.id}`);
+  userEvent.click(linkMoreDetails);
+  expect(screen.getByText('Summary'));
+  expect(screen.getByTestId('pokemon-type')).toBeInTheDocument();
+  userEvent.click(screen.getByRole('checkbox', { checked: false }));
+  expect(screen.getByRole('checkbox', { checked: true })).toBeInTheDocument();
+  const imgFavorite = screen.getByAltText(/pikachu is marked/i);
+  expect(imgFavorite.src).toContain('/star-icon.svg');
+
+  // const { history } = renderWithRouter(<App />);
+  // history.push(`pokemons/${pokemon.id}`);
+  // expect(screen.getByRole('img'));
+});

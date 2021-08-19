51c1882781f6c76289d22b12fb70095ed87397a2
fabio <fabiojuvenalpereira@hotmail.com>
2021-08-19 15:16:16 -0300

    atualização requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-describe('Name of the group', () => {
-  test('testa se existe no topo da página o conjunto de links', () => {
+describe('Testando componentes da Tela inicial', () => {
+  it('testa se existe no topo da página o conjunto de links', () => {
     renderWithRouter(<App />);
 
-    const home = screen.getByText(/Home/i);
-    const about = screen.getByText(/About/i);
-    const favorites = screen.getByText(/Favorite Pokémons/i);
+    const home = screen.getByRole('link', {
+      name: /Home/i,
+    });
+    const about = screen.getByRole('link', {
+      name: /About/i,
+    });
+    const favorites = screen.getByRole('link', {
+      name: /Favorite Pokémons/i,
+    });
 
     expect(home).toBeInTheDocument();
     expect(about).toBeInTheDocument();
     expect(favorites).toBeInTheDocument();
   });
 
-  test('testa se a aplicação é redirecionada para "/" após clicar no botão home', () => {
+  it('testa se a aplicação é redirecionada para "/" após clicar no botão home', () => {
     renderWithRouter(<App />);
     const home = screen.getByText(/Home/i);
     expect(home).toBeInTheDocument();

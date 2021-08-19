f856095c2136c350e58468708fb93cd35260333a
Lopes <gus.lcampos38@gmail.com>
2021-08-19 11:50:37 -0300

    finaliza requisito 6 Pokemon.js

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../Helper/RenderWithRouter';
+import App from '../App';
+
+describe('5, testing component pokedex.js', () => {
+  it('should contain a h2 with the text encountered pokemons', () => {
+    renderWithRouter(<App />);
+
+    const heading = screen.getByRole('heading', {
+      level: 2,
+      name: /encountered pokémons/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('should render the next pokemon charmander', () => {
+    renderWithRouter(<App />);
+
+    const nextBtn = screen.getByRole('button', {
+      name: /Próximo pokémon/i,
+    });
+    expect(nextBtn).toBeInTheDocument();
+
+    userEvent.click(nextBtn);
+
+    const nextPoke = screen.getByText('Charmander');
+    expect(nextPoke).toBeInTheDocument();
+  });
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,57 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../Helper/RenderWithRouter';
+import pokemons from '../data';
+import App from '../App';
+
+describe('Testing Pokemon.js component', () => {
+  it('should render a pokecard on the screen with its correct info', () => {
+    renderWithRouter(<App />);
+    pokemons.forEach((pokemon) => {
+      const { name, type, averageWeight: { value, measurementUnit }, image } = pokemon;
+
+      const pokeName = screen.getByTestId('pokemon-name');
+      const pokeType = screen.getByTestId('pokemon-type');
+      const pokeWeight = screen.getByTestId('pokemon-weight');
+      const pokeImg = screen.getByRole('img');
+      const next = screen.getByRole('button', { name: /Próximo pokémon/i });
+
+      expect(pokeName).toBeInTheDocument();
+      expect(pokeName).toHaveTextContent(name);
+      expect(pokeType).toBeInTheDocument();
+      expect(pokeType).toHaveTextContent(type);
+      expect(pokeWeight).toBeInTheDocument();
+      expect(pokeWeight).toHaveTextContent(`Average weight: ${value} ${measurementUnit}`);
+      expect(pokeImg).toBeInTheDocument();
+      expect(pokeImg).toHaveAttribute('src', image);
+      expect(pokeImg).toHaveAttribute('alt', `${name} sprite`);
+
+      userEvent.click(next);
+    });
+  });
+
+  it('Should go to pokedetails upon mais detalhes click', () => {
+    const { history } = renderWithRouter(<App />);
+    const { id } = pokemons[0];
+    const moreDetails = screen.getByText('More details');
+    expect(moreDetails).toBeInTheDocument();
+    userEvent.click(moreDetails);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe(`/pokemons/${id}`);
+  });
+
+  it('should mark up as favorite', () => {
+    renderWithRouter(<App />);
+
+    const moreDetails = screen.getByText('More details');
+    userEvent.click(moreDetails);
+
+    const checkBox = screen.getByRole('checkbox');
+    userEvent.click(checkBox);
+
+    const favImg = screen.getByAltText('Pikachu is marked as favorite');
+    expect(favImg).toBeInTheDocument();
+    expect(favImg).toHaveAttribute('src', '/star-icon.svg');
+  });
+});

f58de635866fc56e88365156ef01ea2932887eb2
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-08-19 11:54:15 -0300

    resolve o requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -3,25 +3,15 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './utils/renderWithRouter';
 import { Pokedex } from '../components';
-
-const mockPokemons = [{
-  id: 25,
-  name: 'Pikachu',
-  type: 'Electric',
-  averageWeight: { value: '6.0', measurementUnit: 'kg' },
-  image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
-  moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
-  foundAt: [{ location: 'Kanto Viridian Forest',
-    map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png' }],
-  summary: 'This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.',
-}];
+import App from '../App';
+import pokemons from '../data';
 
 describe('testing cases of Pokedex component', () => {
   test('has a title of level 2 with the text "Encountered pokémons"', () => {
     renderWithRouter(
       <Pokedex
-        pokemons={ mockPokemons }
-        isPokemonFavoriteById={ mockPokemons[0] }
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ pokemons }
       />,
     );
     const pokedexTitle = screen.getByRole('heading', {
@@ -31,5 +21,68 @@ describe('testing cases of Pokedex component', () => {
     expect(pokedexTitle).toBeInTheDocument();
   });
 
-	
+  test('the next button have the text "Próximo pokémon"', () => {
+    renderWithRouter(<App />);
+    const nextButton = screen.getByRole('button', {
+      name: /Próximo pokémon/i,
+    });
+    expect(nextButton).toBeInTheDocument();
+  });
+
+  test('show the next pokemon when click in next button', () => {
+    renderWithRouter(<App />);
+    const nextButton = screen.getByTestId('next-pokemon');
+    userEvent.click(nextButton);
+    const pokemonName = screen.getByText(/charmander/i);
+    expect(pokemonName).toBeInTheDocument();
+  });
+
+  test('the Pokédex has filter buttons and next button', () => {
+    renderWithRouter(<App />);
+    const buttonContainer = screen.getAllByRole('button');
+    const BUTTONS_LENGTH = 9;
+    expect(buttonContainer).toHaveLength(BUTTONS_LENGTH);
+  });
+
+  test('click in button type show pokémons of this type', () => {
+    renderWithRouter(<App />);
+    const fireTypeButton = screen.getByRole('button', {
+      name: /fire/i,
+    });
+    expect(fireTypeButton).toBeInTheDocument();
+    userEvent.click(fireTypeButton);
+    const pokemonName = screen.getByText(/charmander/i);
+    expect(pokemonName).toBeInTheDocument();
+    const nextButton = screen.getByTestId('next-pokemon');
+    userEvent.click(nextButton);
+    const nextPokemon = screen.getByText(/rapidash/i);
+    expect(nextPokemon).toBeInTheDocument();
+  });
+
+  test(
+    'click on type button show this pokémons, click on "All" button show first pokemon',
+    () => {
+      renderWithRouter(<App />);
+      const fireTypeButton = screen.getByRole('button', {
+        name: /fire/i,
+      });
+      expect(fireTypeButton).toBeInTheDocument();
+      userEvent.click(fireTypeButton);
+      const pokemonName = screen.getByText(/charmander/i);
+      expect(pokemonName).toBeInTheDocument();
+      const allButton = screen.getByRole('button', {
+        name: /all/i,
+      });
+      userEvent.click(allButton);
+      const firstPokemon = screen.getByText(/pikachu/i);
+      expect(firstPokemon).toBeInTheDocument();
+    },
+  );
+
+  test('has type pokemons button with right test ids', () => {
+    renderWithRouter(<App />);
+    const typeButtons = screen.getAllByTestId('pokemon-type-button');
+    const TYPE_BUTTONS_LENGTH = 7;
+    expect(typeButtons).toHaveLength(TYPE_BUTTONS_LENGTH);
+  });
 });

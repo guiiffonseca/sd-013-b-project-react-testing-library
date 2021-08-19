b64f68ee126b24d349639330ab866dba8b5feaad
Aleilton Chavenco <aleilton@gmail.com>
2021-08-18 21:55:33 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+
+import { MemoryRouter } from 'react-router';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('página contém um heading h2 com o texto Page requested not found', () => {
+    render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+
+    const h2NotFoundPage = screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    });
+
+    expect(h2NotFoundPage).toBeInTheDocument();
+  });
+
+  test('página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+
+    const imgNotFoundPage = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(imgNotFoundPage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

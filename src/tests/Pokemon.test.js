b027aac3e6cdc6496d7afa522d5f7a7ab6244244
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-08-19 18:29:24 -0300

    realiza implementacoes dos testes do requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,26 +1,53 @@
-/* import React from 'react';
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import Pokedex from '../components/Pokedex';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import Pokemon from '../components/Pokemon';
 import Data from '../data';
 
-describe('Teste o componente <Pokedex.js />', () => {
-  it('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
-    render(
-      <MemoryRouter>
-        <Pokedex pokemons={ Data } isPokemonFavoriteById={ { } } />
-      </MemoryRouter>,
+function renderWithRouter(componentToRender) {
+  const customHistory = createMemoryHistory();
+
+  return {
+    ...render(
+      <Router history={ customHistory }>
+        {componentToRender}
+      </Router>,
+    ),
+    history: customHistory,
+  };
+}
+
+const pokemon = Data[0];
+const favorite = true;
+
+describe('Teste o componente <Pokemon.js />', () => {
+  it('Teste se é renderizado um card com as informações de determinado pokémon', () => {
+    renderWithRouter(
+      <Pokemon
+        isFavorite={ favorite }
+        pokemon={ pokemon }
+        showDetailsLink={ favorite }
+      />,
     );
 
-    const component = screen.getByRole('heading', {
-      level: 2,
-      name: /Encountered pokémons/i,
-    });
-    expect(component).toBeInTheDocument();
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName).toHaveTextContent(pokemon.name);
+
+    const imgUrl = pokemon.image;
+    const image = screen.getByAltText(/sprite/);
+    expect(image).toHaveAttribute('src', imgUrl);
+
+    const imageFavorite = screen.getByAltText(/is marked as favorite/);
+    expect(imageFavorite).toHaveAttribute('src', '/star-icon.svg');
+
+    const pokemonType = screen.getByTestId('pokemon-type');
+    expect(pokemonType).toHaveTextContent(pokemon.type);
+
+    const componentLink = screen.getByRole('link');
+    expect(componentLink).toHaveAttribute('href', `/pokemons/${pokemon.id}`);
+
+    const componentDetails = screen.getByText(/More details/);
+    expect(componentDetails).toBeInTheDocument();
   });
-
-  // it('', () => {
-
-  // });
-}); */
-test('', () => {});
+});

811f4b45194c12423991a15858d65bebb8f864eb
rmiguel139 <migmenezes.mm2@gmail.com>
2021-08-19 16:24:22 -0300

    req 6

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderRouter from '../components/renderRouter';
+import App from '../App';
+
+describe('Teste do componente notFound', () => {
+  test('Teste se página contém um h2 com o texto Encountered pokémons', () => {
+    renderRouter(<App />);
+    const pokedexh2 = screen.getByRole('heading', {
+      name: 'Encountered pokémons',
+      level: 2,
+    });
+    expect(pokedexh2).toBeInTheDocument();
+  });
+  test('Testa se renderiza um botao e mostra proximo pokemon', () => {
+    renderRouter(<App />);
+    const botaoNext = screen.getByRole('button', {
+      name: 'Próximo pokémon',
+    });
+    expect(botaoNext).toBeInTheDocument();
+  });
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,48 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderRouter from '../components/renderRouter';
+
+describe('Teste do componente notFound', () => {
+  test('Teste se é renderizado um card com as informações de determinado pokémon', () => {
+    const { history } = renderRouter(<App />);
+    const pikachuName = screen.getByTestId('pokemon-name');
+    expect(pikachuName).toBeInTheDocument();
+    expect(pikachuName).toHaveTextContent('Pikachu');
+
+    const pikachuType = screen.getByTestId('pokemon-type');
+    expect(pikachuType).toBeInTheDocument();
+    expect(pikachuType).toHaveTextContent('Electric');
+
+    const pikachuPeso = screen.getByText('Average weight: 6.0 kg');
+    expect(pikachuPeso).toBeInTheDocument();
+
+    const pikachuImage = screen.getByAltText('Pikachu sprite');
+    expect(pikachuImage).toBeInTheDocument();
+    expect(pikachuImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+
+    const maisDetalhes = screen.getByRole('link', {
+      name: 'More details',
+    });
+    expect(maisDetalhes).toBeInTheDocument();
+    userEvent.click(maisDetalhes);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+  });
+  test('Verifica favorito', () => {
+    renderRouter(<App />);
+    const maisDetalhes = screen.getByText('More details');
+    userEvent.click(maisDetalhes);
+
+    const checkboxInput = screen.getByRole('checkbox');
+    userEvent.click(checkboxInput);
+
+    const homeText = screen.getByText('Home');
+    userEvent.click(homeText);
+
+    const favoriteStart = screen.getByAltText('Pikachu is marked as favorite');
+    expect(favoriteStart).toBeInTheDocument();
+    expect(favoriteStart).toHaveAttribute('src', '/star-icon.svg');
+  });
+});

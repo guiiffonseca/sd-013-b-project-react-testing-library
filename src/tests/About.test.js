65407b43a8a9bb1629abad58dc2fdfb792db4bf4
paulomichael <paulo.michael@gmail.com>
2021-08-19 17:21:11 -0300

    requisito 2 - Testa o componente <About.js />

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('2. Testa o componente <About.js />.', () => {
+  it('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const headingAbout = screen.getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
+    });
+    expect(headingAbout).toBeInTheDocument();
+  });
+  it('Testa se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    renderWithRouter(<About />);
+    const firstParagraph = screen.getByText(/This application simulates a Pokédex,/i);
+    const secondParagraph = screen.getByText(/One can filter Pokémons by type,/i);
+    expect(firstParagraph).toBeInTheDocument();
+    expect(secondParagraph).toBeInTheDocument();
+  });
+  it('Testa se a página contém imagem de uma Pokédex com URL dada', () => {
+    renderWithRouter(<About />);
+    const pokedexImageUrl = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const pokedexImage = screen.getByRole('img');
+    expect(pokedexImage).toBeInTheDocument();
+    expect(pokedexImage.src).toBe(pokedexImageUrl);
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,2 +1,30 @@
-/* alteracao para o commit inicial e criacao do PR */
-test('', () => {});
+import React from 'react';
+import { /* render, */ screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('1. Testa o componente <App.js />', () => {
+  test('Testa se o topo contém um conjunto fixo de links de navegação.', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByText(/home/i);
+    const aboutLink = screen.getByText(/about/i);
+    const favoriteLink = screen.getByText(/favorite pokémon/i);
+
+    expect(history.location.pathname).toBe('/');
+    expect(homeLink).toBeDefined();
+    expect(aboutLink).toBeDefined();
+    expect(favoriteLink).toBeDefined();
+
+    userEvent.click(aboutLink);
+    expect(history.location.pathname).toBe('/about');
+    userEvent.click(favoriteLink);
+    expect(history.location.pathname).toBe('/favorites');
+    userEvent.click(homeLink);
+    expect(history.location.pathname).toBe('/');
+
+    history.push('/notFound');
+    const notFound = screen.getByText(/not found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+});

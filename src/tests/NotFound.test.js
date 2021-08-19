3387cc3f402e8e40749c447e5155f5bb0c31f761
Letícia Castro <aicitelks@gmail.com>
2021-08-19 16:56:15 -0300

    Requisito 4, teste 2/2 OK

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -19,6 +19,9 @@ describe('4. Teste o componente <NotFound.js />', () => {
 
   test(`Teste se página mostra a imagem
   https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`, () => {
-      
+    renderWithRouter(<NotFound />);
+
+    const imagemPikachuCry = screen.getByAltText(/not found/i);
+    expect(imagemPikachuCry).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
 });

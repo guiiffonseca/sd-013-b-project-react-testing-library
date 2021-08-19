0313f31a92e67439063df15518fb5e92fd91fbe8
unknown <sergio.espp@gmail.com>
2021-08-19 18:03:06 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Test 4 - Testing Not Found', () => {
+  it('Test if Page requested not found is shown', () => {
+    renderWithRouter(<NotFound />);
+
+    const textNotFound = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(textNotFound).toBeInTheDocument();
+  });
+
+  it('Test if the page contains a pikachu crying gif', () => {
+    renderWithRouter(<NotFound />);
+
+    const pikachuGif = screen.getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found',
+    });
+    expect(pikachuGif).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

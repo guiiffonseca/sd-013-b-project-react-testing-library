2f20fcb708002035a13ae32510dbaf08ea4f950b
FilipeGuto <filipeguto95@gmail.com>
2021-08-18 21:13:01 -0300

    Exercise 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import renderWithRouter from './utils/renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Teste componente NotFound', () => {
+  it('Verifica se a página contém o texto "Page requested not found 😭"', () => {
+    renderWithRouter(<NotFound />);
+
+    const imgUrlNotFound = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imgPikachuAlt = 'Pikachu crying because the page requested was not found';
+
+    const titleText = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(titleText).toBeInTheDocument();
+
+    const imgNotFound = screen.getByAltText(imgPikachuAlt);
+    expect(imgNotFound).toHaveAttribute('src', imgUrlNotFound);
+    expect(imgNotFound).toBeInTheDocument();
+  });
+
+});

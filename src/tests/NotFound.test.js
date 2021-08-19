7a53e945be93dd8c525b208299bb9f5c3a6b7b7c
Jeferson Oliveira <jeferson.contas@gmail.com>
2021-08-19 00:11:04 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('Teste NotFound.js', () => {
+  test('Se página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    expect(screen.getAllByText(/Page requested not found/i)).toBeInTheDocument();
+  });
+  test('Teste se página mostra a imagem X', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen
+      .getByAltText(/Pikachu crying because the page requested was not found/i);
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

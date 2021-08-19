ac775b926f493f3d2f5f0341558371786ed746bd
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-08-18 23:59:21 -0300

    Adiciona requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../components';
+
+test('se página contém um heading h2 com o texto Page requested not found 😭', () => {
+  renderWithRouter(<NotFound />);
+  const h2Text = screen.getByRole('heading', {
+    level: 2,
+    name: 'Page requested not found Crying emoji',
+  });
+  expect(h2Text).toBeInTheDocument();
+});
+
+test('se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+  renderWithRouter(<NotFound />);
+  const image = screen.getByAltText(/Pikachu crying/i);
+  expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});

b9c71dfff753a730f5bce071de1d094eebf58dc3
Letícia Castro <aicitelks@gmail.com>
2021-08-19 16:50:34 -0300

    Requisito 4, teste 1/2 OK

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './utils/renderWithRouter';
+import { NotFound } from '../components'
+import { screen } from '@testing-library/dom';
+
+describe('4. Teste o componente <NotFound.js />', () => {
+  test('Teste se página contém um heading h2 com o texto "Page requested not found 😭"', () => {
+    const { history } = renderWithRouter(<NotFound />);
+    
+    history.push('/rota-inexistente');
+
+    const notFoundMessage = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+
+    expect(notFoundMessage).toBeInTheDocument();
+  });
+
+  test(`Teste se página mostra a imagem
+  https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`, () => {
+      
+  });
+});

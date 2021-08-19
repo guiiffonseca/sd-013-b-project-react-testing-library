9451b94f479063f3aff2b1e4a03abad014bc2ea6
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-08-18 21:54:36 -0300

    Finaliza requisito 3.

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,44 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import App from '../App';
+
+import renderWithRouter from './utils/renderWithRouter';
+import pokemons from '../data';
+
+describe('FavoritePokemons.js Tests', () => {
+  let history;
+  const favPokemonsData = {
+    noFavMessage: 'No favorite pokemon found',
+  };
+
+  beforeEach(() => {
+    history = renderWithRouter(<App />).history;
+    history.push('/favorites');
+  });
+
+  test('Exibe na tela a mensagem No favorite pokemon found, caso sem favoritos.', () => {
+    const noFavMessage = screen.getByText(favPokemonsData.noFavMessage);
+
+    expect(noFavMessage).toBeInTheDocument();
+  });
+
+  test('Exibe todos os cards de pokémons favoritados.', async () => {
+    pokemons.forEach((pokemon) => {
+      history.push(`/pokemons/${pokemon.id}`);
+
+      const favoriteCheck = screen.getByRole('checkbox');
+      userEvent.click(favoriteCheck);
+    });
+
+    await history.push('/favorites');
+
+    const pokemonNames = screen.getAllByTestId('pokemon-name');
+
+    pokemons.forEach((pokemon, index) => {
+      expect(pokemonNames[index]).toBeInTheDocument();
+      expect(pokemonNames[index].innerHTML).toBe(pokemon.name);
+    });
+  });
+});

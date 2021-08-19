dd9b9fe65c439e198383a162f2e5efa38976b423
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-19 20:27:55 -0300

    Requisito 3 - test 2 - Exibe todos os cards de pokémons favoritados

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import { FavoritePokemons } from '../components';
+import App from '../App';
 
 describe('Favorite =Pokemons funciona corretamente', () => {
   it('Exibe `No favorite pokemon found` quando não há pokemons favoritados', () => {
@@ -12,7 +14,33 @@ describe('Favorite =Pokemons funciona corretamente', () => {
     expect(noFavoritedMessage).toBeInTheDocument();
   });
 
-  it('', () => {
+  it('Exibe todos os cards de pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
 
+    const moreDetails = screen.getByText('More details');
+    const favoritePokemonsLink = screen.getByText('Favorite Pokémons');
+
+    expect(moreDetails).toBeInTheDocument();
+    userEvent.click(moreDetails);
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/pokemons/25');
+
+    const favoriteOption = screen.getByLabelText('Pokémon favoritado?');
+    userEvent.click(favoriteOption);
+
+    const isFavoriteImg = screen.getByRole('img', { name: /is marked as favorite/ });
+    expect(isFavoriteImg).toBeInTheDocument();
+
+    userEvent.click(favoritePokemonsLink);
+    expect(pathname).toBe('/pokemons/25');
+
+    const pokeName = screen.getByTestId('pokemon-name');
+    const pokeType = screen.getByTestId('pokemon-type');
+    const pokeWeight = screen.getByText(/weight/);
+
+    const pokemonInfos = [pokeName, pokeType, pokeWeight];
+
+    pokemonInfos.forEach((info) => expect(info).toBeInTheDocument());
   });
 });

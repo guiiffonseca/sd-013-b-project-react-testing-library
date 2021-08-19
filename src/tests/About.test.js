212a686d7e5bcc07913916c1b6ba17e462254e84
andre9653 <andresena9653@gmail.com>
2021-08-18 21:06:38 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { About } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+test('Teste se a página contém as informações sobre a Pokédex.', () => {
+  renderWithRouter(<About />);
+
+  expect(screen.getByRole('heading', {
+    level: 2,
+    name: /about pokédex/i,
+  })).toBeInTheDocument();
+
+  expect(screen.getByText(/This application simulates/i))
+    .toBeInTheDocument();
+  expect(screen.getByText(/One can filter Pokémons/i))
+    .toBeInTheDocument();
+  expect(screen.getByRole('img').src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});

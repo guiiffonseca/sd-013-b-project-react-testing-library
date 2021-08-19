31536098b4aa54967c72f403999c3302acc68867
EddieRAM <edusacarneiro@gmail.com>
2021-08-18 23:41:13 -0300

    Completed requirement 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('if component FavoritePokemons.js is working properly', () => {
+  it('should display message "No favorite pokemon found" if no favorited pokémon', () => {
+    render(<MemoryRouter><FavoritePokemons /></MemoryRouter>);
+
+    const noFavText = screen.getByText('No favorite pokemon found');
+    expect(noFavText).toBeInTheDocument();
+  });
+});

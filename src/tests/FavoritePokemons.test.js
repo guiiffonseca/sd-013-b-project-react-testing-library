dc13563fbb0d6dda4fc2a28dff9f58f483d8e796
Gustavo Fernando <gu.bressan@outlook.com>
2021-08-19 10:46:12 -0700

    Lintizada

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -4,10 +4,12 @@ import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 describe('Test FavoritePokemons', () => {
-  test('No favorite pokemon found', () =>{
+  test('No favorite pokemon found', () => {
     const { history } = renderWithRouter(<App />);
     history.push('/favorites');
     const text = screen.getByText('No favorite pokemon found');
     expect(text).toBeInTheDocument();
   });
 });
+
+// npx stryker run ./stryker/FavoritePokemons.conf.json

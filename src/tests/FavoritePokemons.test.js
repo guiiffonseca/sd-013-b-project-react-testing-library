e8e642bf9686065ad4b9404ba8264ae1a237dd85
paulomichael <paulo.michael@gmail.com>
2021-08-19 18:10:09 -0300

    requisito 3 - Testa o componente <FavoritePokemons.js />

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe(' 3. Testa o componente <FavoritePokemons.js />', () => {
+  it('Testa a mensagem `No favorite pokemon found`.', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const notFoundMessage = screen.getByText(/No favorite pokemon found/i);
+    expect(notFoundMessage).toBeInTheDocument();
+  });
+});

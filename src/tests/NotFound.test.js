9bdd6828e418ac471be5ba2adc29f0afd8fbbf70
EddieRAM <edusacarneiro@gmail.com>
2021-08-19 00:07:17 -0300

    Completed requirement 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import NotFound from '../components/NotFound';
+
+describe('if component NotFound.js is working properly', () => {
+  it('should display message "Page requested not found" as invalid input throw', () => {
+    render(<MemoryRouter><NotFound /></MemoryRouter>);
+
+    const notFoundText = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  it('should contain crying Pokachu gif', () => {
+    render(<MemoryRouter><NotFound /></MemoryRouter>);
+
+    const cryingPikachuGif = screen.getByAltText(/Pikachu crying/);
+    expect(cryingPikachuGif.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

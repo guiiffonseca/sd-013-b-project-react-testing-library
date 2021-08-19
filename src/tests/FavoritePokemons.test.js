b3f09384c7286f1560da53515a75b73c8143ab23
Letícia Castro <aicitelks@gmail.com>
2021-08-19 16:22:40 -0300

    Requisito 3, teste 1/2 OK

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../tests/utils/renderWithRouter';
+import { getByRole, getByTestId, screen } from '@testing-library/react';
+import { FavoritePokemons, PokemonDetails } from '../components/';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe(`3. Teste o componente <FavoritePokemons.js />`, () => {
+  test(`Teste se é exibido na tela a mensagem 'No favorite pokemon found',
+  se a pessoa não tiver Pokémons favoritos.`, () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const msgFavoritePokemon = screen.getByText('No favorite pokemon found');
+    expect(msgFavoritePokemon).toBeInTheDocument();
+  });
+  
+
+});

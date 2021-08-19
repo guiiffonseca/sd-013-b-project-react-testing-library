28cc6ca6d384feb53f0f256437e9872a54259f2e
unknown <sergio.espp@gmail.com>
2021-08-19 17:02:35 -0300

    Requisito 3

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,13 +4,13 @@ import renderWithRouter from '../renderWithRouter';
 import About from '../components/About';
 
 describe('Test 2- Testing about', () => {
-  test('Testing if page shows information about pokedex', () => {
+  it('Testing if page shows information about pokedex', () => {
     renderWithRouter(<About />);
 
     const aboutText = screen.getByText(/containing all pokémons/i);
     expect(aboutText).toBeInTheDocument();
   });
-  test('Test if page have a heading H2 with text "About Pokédex"', () => {
+  it('Test if page have a heading H2 with text "About Pokédex"', () => {
     renderWithRouter(<About />);
 
     const aboutHeader = screen.getByRole('heading', { level: 2,
@@ -19,13 +19,13 @@ describe('Test 2- Testing about', () => {
     expect(aboutHeader).toBeInTheDocument();
   });
 
-  test('Test if page has 2 paragraphs', () => {
+  it('Test if page has 2 paragraphs', () => {
     renderWithRouter(<About />);
 
     const aboutParaphs = screen.getAllByText(/pokémons/i);
     expect(aboutParaphs).toHaveLength(2);
   });
-  test('Test if images of pokedex are shown', () => {
+  it('Test if images of pokedex are shown', () => {
     renderWithRouter(<About />);
 
     const pokedexImg = screen.getByRole('img');
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('Test 3 - Testing Favorite Pokemons ', () => {
+  it('Test if "No favorite pokemon found" is shown', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const pokemonNotFound = screen.getByText('No favorite pokemon found');
+    expect(pokemonNotFound).toBeInTheDocument();
+  });
+
+  it('Test if favorites pokemons are on the screen', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+
+    const favoritePokemons = screen.getByTestId('pokemon-name');
+    expect(favoritePokemons).toBeInTheDocument();
+    expect(favoritePokemons.length).not.toBe(0);
+  });
+});

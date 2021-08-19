94738915e121fafad5ddf6294490f0d5e246af1a
Polyana Balotin <polybalotin@hotmail.com>
2021-08-19 20:31:10 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testing NotFound component', () => {
+  test('If page contains a heading h2 with the text Page requested not found', () => {
+    render(<NotFound />);
+    const title = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found Crying emoji/i,
+    });
+    expect(title).toBeInTheDocument();
+  });
+
+  test('If an image is shown', () => {
+    render(<NotFound />);
+    const image = screen.getByRole('img', {
+      name: /Pikachu crying because the page requested was not found/i,
+    });
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

d43af9bf1a17095985d4ebc23adb80b114b0edc8
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-08-19 16:35:36 -0300

    Adiciona requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons, PokemonDetails } from '../components';
+
+test('se mostra No favorite pokemon found, quando não tiver pokémon favoritos', () => {
+  renderWithRouter(<FavoritePokemons />);
+  const textPokemons = screen.getByText('No favorite pokemon found');
+  expect(textPokemons).toBeInTheDocument();
+});
+
+// test('se é exibido todos os cards de pokémons favoritados', () => {
+//   renderWithRouter(<PokemonDetails />);
+//   const checkboxFavoritePokemons = screen.getByRole('checkbox', {
+//     checked: true,
+//   });
+// });

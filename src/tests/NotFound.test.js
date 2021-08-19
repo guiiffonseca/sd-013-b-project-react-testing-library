523c708b0d130a4639effa471261e069f0ee001c
andre9653 <andresena9653@gmail.com>
2021-08-18 22:18:38 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -7,5 +7,5 @@ test('Testa pagina Not Found', () => {
   renderWithRouter(<NotFound />);
 
   expect(screen.getByRole('heading', { name: /Page requested not found/i, level: 2 }));
-  expect(screen.getAllByRole('img')[1].src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif')
+  expect(screen.getAllByRole('img')[1].src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
 });

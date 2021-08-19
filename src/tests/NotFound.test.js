b2ad9efd4b5bce7f6a0137e442745edc2615f9bc
Pedro <pedrorsehn@hotmail.com>
2021-08-19 15:14:30 -0300

    add ex4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import RenderWithRouter from './utils/RenderWithRouter';
+import App from '../App';
+
+describe('Testa pagina Not Found', () => {
+  test('Testa se existe texto Not Fund na pagina', () => {
+    const { history } = RenderWithRouter(<App />);
+    history.push('/testeTeste');
+
+    const noFoundText = screen.getByText('Page requested not found');
+
+    expect(noFoundText).toBeInTheDocument();
+  });
+
+  test('Testa se existe um gif', () => {
+    const { history } = RenderWithRouter(<App />);
+    history.push('/testeTeste');
+
+    const noFoundGif = screen.getAllByRole('img');
+
+    expect(noFoundGif[1]).toBeInTheDocument();
+    expect(noFoundGif[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

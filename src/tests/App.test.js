f48970feb8e94705933071d35eae3b4be10d7fef
Gustavo Fernando <gu.bressan@outlook.com>
2021-08-18 20:12:44 -0700

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('testa link', () => {
+  test('testa link Home', () => {
+    renderWithRouter(<App />);
+    const textHome = screen.getByRole('link', { name: 'Home' });
+    expect(textHome).toBeInTheDocument();
+  });
+
+  test('testa link Favorite Pokemons', () => {
+    renderWithRouter(<App />);
+    const textFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(textFavorite).toBeInTheDocument();
+  });
+
+  test('testa link About', () => {
+    renderWithRouter(<App />);
+    const textAbout = screen.getByRole('link', { name: 'About' });
+    expect(textAbout).toBeInTheDocument();
+  });
+});
+
+// https://testing-library.com/docs/example-react-router/;

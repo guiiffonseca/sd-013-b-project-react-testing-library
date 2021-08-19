7ea89f5482dbda1435954d16004a5caba54ccec8
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-19 12:40:37 -0300

    Requisito 1 - test 3 - clicar no link About redireciona usuário para a página about pokédex

--- src/renderWithRouter.js
+++ src/renderWithRouter.js
@@ -1,3 +1,4 @@
+//src/renderWithRouter.js
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
@@ -5,9 +6,9 @@ import { render } from '@testing-library/react';
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
-  return({
-    ...render(<Router history={history}>{component}</Router>), history
+  return ({
+    ...render(<Router history={history}>{component}</Router>), history,
   });
-} 
+};
 
 export default renderWithRouter;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('Teste do componente App', () => {
@@ -13,5 +15,23 @@ describe('Teste do componente App', () => {
     expect(linkToHome).toBeInTheDocument();
     expect(linkToAbout).toBeInTheDocument();
     expect(linkToFavorites).toBeInTheDocument();
+  });
+
+  it('Ao clicar em Home a aplicação deve ser redirecionada à sua página inicial', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkToHome = screen.getByText('Home');
+    const { location: { pathname } } = history;
+
+    userEvent.click(linkToHome);
+    expect(pathname).toBe('/');
+  });
+  it('Ao clicar em About a aplicação deve ser redirecionada à página About', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkToAbout = screen.getByText('About');
+
+    const { location: { pathname } } = history;
+    userEvent.click(linkToAbout);
+    expect(pathname).toBe('/about');
   });
 });

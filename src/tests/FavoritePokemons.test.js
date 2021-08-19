092d089cb7ca10ae0eacfdc205f81e1ee71a1228
rmiguel139 <migmenezes.mm2@gmail.com>
2021-08-19 15:09:42 -0300

    req3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+
+describe('Pokemon test', () => {
+  test('Testa se página contém um texto no favorite', () => {
+    render(<FavoritePokemons />);
+    const noFavorite = screen.getByText('No favorite pokemon found');
+    expect(noFavorite).toBeInTheDocument();
+  });
+});

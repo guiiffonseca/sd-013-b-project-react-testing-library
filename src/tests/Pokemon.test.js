4dee00e77f2971b524840126ab14d6566edc31aa
Lucas <lucascardim777@gmail.com>
2021-08-19 16:28:21 -0300

    lint issue, resolved

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -3,7 +3,6 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../utils/renderWithRouter';
 import App from '../App';
-import Pokemon from '../components/Pokemon';
 
 describe('Testar o component Pokemon.js', () => {
   test('Testar se é renderizado um card com as informações do pokémon.', () => {

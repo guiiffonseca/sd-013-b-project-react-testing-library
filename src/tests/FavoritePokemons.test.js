2bc364bd96b874c9d93e95d6d66962c59be245cd
Gustavo Fernando <gu.bressan@outlook.com>
2021-08-19 10:44:05 -0700

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Test FavoritePokemons', () => {
+  test('No favorite pokemon found', () =>{
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+    const text = screen.getByText('No favorite pokemon found');
+    expect(text).toBeInTheDocument();
+  });
+});

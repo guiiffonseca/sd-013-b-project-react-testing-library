e7fe4576d4e7f3f55e249b60b00358130cd6566d
MilenaCLeme <milenaleme4@hotmail.com>
2021-08-18 22:58:53 -0300

    Arrumando lint pasta About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -15,14 +15,14 @@ describe('Teste se a página contém as informações sobre a Pokédex', () => {
 
   test('A página contém dois parágrafos com texto sobre a Pokédex', () => {
     renderWithRouter(<About />);
-    const p = document.querySelectorAll('p')
+    const p = screen.getAllByText(/Pokémons/i);
     expect(p.length).toBe(2);
   });
 
   test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
     renderWithRouter(<About />);
-    const image = screen.getByRole('img')
-    expect(image).toHaveAttribute('alt', 'Pokédex')
-    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png')
+    const image = screen.getByRole('img');
+    expect(image).toHaveAttribute('alt', 'Pokédex');
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });

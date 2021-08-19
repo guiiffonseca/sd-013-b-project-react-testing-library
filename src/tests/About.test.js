04b12a20df2dad6d4f944ffa5a326a37c35bc564
your_github_enioandrade92 <your_github_enio.goulart@gmail.com>
2021-08-18 21:42:00 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,50 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('about test', () => {
+  test('Existe informações sobre Pokédex', () => {
+    const customHistory = createMemoryHistory();
+    render(
+      <Router history={ customHistory }>
+        <App />
+      </Router>,
+    );
+    customHistory.push('/about');
+    const searchInfo1 = screen.getByText(/This application simulates a Pokédex/);
+    expect(searchInfo1).toBeInTheDocument();
+    const searchInfo2 = screen.getByText(/One can filter Pokémons by type/);
+    expect(searchInfo2).toBeInTheDocument();
+  });
+
+  test('should ', () => {
+    const customHistory = createMemoryHistory();
+    render(
+      <Router history={ customHistory }>
+        <App />
+      </Router>,
+    );
+    customHistory.push('/about');
+    const searchTitle = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(searchTitle).toBeInTheDocument();
+  });
+
+  test('should ', () => {
+    const customHistory = createMemoryHistory();
+    render(
+      <Router history={ customHistory }>
+        <App />
+      </Router>,
+    );
+    customHistory.push('/about');
+
+    const image = screen.getByAltText('Pokédex');
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});

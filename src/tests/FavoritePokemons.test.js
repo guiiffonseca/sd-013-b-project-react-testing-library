d46e138b289ceb12982a3280d2a7ab2ff8bcb86a
Natalia Camarinha <natcamarinha@gmail.com>
2021-08-19 17:05:39 -0300

    requisito 5 parcial

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
 
 describe('testa o componente Favorite Pokemons', () => {
-  test('teste se é exibido No favorite pokemon na tela', () => {});
+  test('teste se a mensagem "No favorite pokemon found" é exibida na tela', () => {
+    render(<FavoritePokemons />);
 
-  test('teste se é exibido todos os cards de pokemons favoritados', () = > {
-    
-  })
+    const favoritePokemon = screen.getByText('No favorite pokemon found');
+    expect(favoritePokemon).toBeInTheDocument();
+  });
 
-})
+  // test('teste se é exibido todos os cards de pokemons favoritados', () => {
+  //   render(<FavoritePokemons />);
+  // });
+});

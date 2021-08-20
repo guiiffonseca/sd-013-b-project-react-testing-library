c4b341cbbf5d1c53266614235cc2a07462e07485
ViniciusMossi <vini_agg@hotmail.com>
2021-08-20 15:32:14 -0400

    Implementa requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import RouterMemory from './RouterMemory';
+import About from '../components/About';
+
+describe('Testando o About.js', () => {
+  test('se a página contém as informações sobre a Pokédex', () => {
+    RouterMemory(<About />);
+
+    const pokedexInfo = screen.getByText(/digital encyclopedia/);
+
+    expect(pokedexInfo).toBeInTheDocument();
+  });
+
+  test('se a página contém um heading h2 com o texto About Pokédex', () => {
+    RouterMemory(<About />);
+
+    const header = screen.getByRole('heading', { level: 2 });
+
+    expect(header.textContent).toBe('About Pokédex');
+  });
+
+  test('se a página contém uma imagem de uma Pokédex', () => {
+    RouterMemory(<About />);
+
+    const image = screen.getByAltText('Pokédex');
+
+    expect(image.src).toBe(
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  });
+});

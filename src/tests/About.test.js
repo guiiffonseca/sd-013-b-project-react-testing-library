fa98230e83f19b9183c0a9c9195661ded9e82c34
Jessica Naiara <jessicanaiarag@gmail.com>
2021-08-19 22:36:45 -0300

    requisito 2-refatorado

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,19 +4,29 @@ import About from '../components/About';
 import renderWithRouter from './renderWithRouter';
 
 describe('Testa o About.js', () => {
-  test('Teste se a página contém informações sobre a Pokédex', () => {
+  beforeEach(() => {
     renderWithRouter(<About />);
-    let textAbout = screen.getByText(/About Pokédex/i);
-    expect(textAbout).toBeInTheDocument();
+  });
 
-    textAbout = screen.getByRole('heading', {
+  test('Teste se a página contém informações sobre a Pokédex', () => {
+    const textAbout = screen.getByText(/About Pokédex/i);
+    expect(textAbout).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    const textAbout = screen.getByRole('heading', {
       level: 2,
     });
     expect(textAbout).toBeInTheDocument();
+    expect(textAbout).toHaveTextContent('About Pokédex');
+  });
 
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
     const elementP = screen.getAllByTestId('p-element');
     expect(elementP.length).toBe(2);
+  });
 
+  test('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
     const elementImg = screen.getByRole('img');
     expect(elementImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });

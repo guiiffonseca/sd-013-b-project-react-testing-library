f08241ca4f3f86605bf5862256d7b1fd098e990c
Leonardo <leo_mc_inter@hotmail.com>
2021-08-20 14:59:58 -0300

    Lint

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import renderWithRouter from './router/renderWithRouter';
+// import userEvent from '@testing-library/user-event';
+// import renderWithRouter from './router/renderWithRouter';
 import App from '../App';
 
 describe('Testa o componente Pokemon', () => {

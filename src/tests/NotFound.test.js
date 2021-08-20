abf2c74140eb80afe34f81b8c5840352f650ca9b
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-20 14:32:19 -0300

    Requisito 4 - test 1 - Página contém um heading h2 com o texto Page requested not found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Página NotFound funciona corretamente', () => {
+  it('Página contém um heading h2 com o texto Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundMessage = screen.getByRole('heading',
+      { name: 'Page requested not found Crying emoji' },
+      { level: 2 });
+    expect(notFoundMessage).toBeInTheDocument();
+  });
+
+  it('', () => {
+
+  });
+});

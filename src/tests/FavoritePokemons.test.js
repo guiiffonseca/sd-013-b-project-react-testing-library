4563ae9e730186f03d396382df03ce7d6cefee64
PedroSpelta <p.h.s.8989@hotmail.com>
2021-08-20 19:06:44 -0300

    terminando o req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste da componente Favorite Pokémon', () => {
+  it('Mensagem No "favorite pokemon found" quando o usuária não tem favorito', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noMessage = screen.getByText('No favorite pokemon found');
+    expect(noMessage).toBeInTheDocument();
+  });
+
+  it('Todos os cards de pokemons favoritos são exibidos', () => {
+    const pokemons = [
+      {
+        id: 24,
+        name: 'Pikachu',
+        type: 'Electric',
+        averageWeight: {
+          value: '6.0',
+          measurementUnit: 'kg',
+        },
+        image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+      },
+      {
+        id: 23,
+        name: 'Ekans',
+        type: 'Poison',
+        averageWeight: {
+          value: '6.9',
+          measurementUnit: 'kg',
+        },
+        image: 'https://cdn2.bulbagarden.net/upload/1/18/Spr_5b_023.png',
+      },
+    ];
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    const names = screen.getAllByTestId('pokemon-name');
+    expect(names).toHaveLength(2);
+  });
+});

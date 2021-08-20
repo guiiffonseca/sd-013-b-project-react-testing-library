5244af2245596e7820453e14d3192a482122f8b6
Daniel Teixeira <daniel.o.teixeira@gmail.com>
2021-08-20 16:22:09 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '.renderWithRouter';
+
+describe('Testa se o App.js possui links por nome e redirecionamento.', () => {
+  test('Teste que verifica a existência dos links', () => {
+    const { history } = renderWithRouter(<App />);
+    const home = screen.getByText(/home/i);
+    const favorite = screen.getByText(/favorite/i);
+    const about = screen.getByText(/about/i);
+    const homePath = history.location.pathname;
+    const favoritePath = history.location.pathname;
+    const aboutPath = history.location.pathname;
+    const notFound = screen.getByText(/not found/i);
+
+    expect(home).toBeDefined();
+    expect(favorite).toBeDefined();
+    expect(about).toBeDefined();
+    expect(mainPath).toBe('/');
+
+    userEvent.click(home);
+    expect(homePath).toBe('/')
+    userEvent.click(favorite);
+    expect(favoritePath).toBe('/favorites');
+    userEvent.click(about);
+    expect(aboutPath).toBe('/about');
+    
+    history.push('/notFound');
+    expect(notFound).toBeInTheDocument();
+  });
+});

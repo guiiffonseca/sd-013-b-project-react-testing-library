7e8f0c3d53712bac984ede9ad1d2be326b7b8dbb
PedroSpelta <p.h.s.8989@hotmail.com>
2021-08-20 18:01:44 -0300

    terminando o req 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Teste componente About', () => {
+  it('A página deve conter um h2 com texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const infos = screen.getByRole('heading', {
+      name: 'About Pokédex',
+      level: 2,
+    });
+    expect(infos).toBeInTheDocument();
+  });
+
+  it('A página deve conter dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const paragraph1 = screen.getByText(/containing all Pokémons/);
+    const paragraph2 = screen.getByText(/filter Pokémons/);
+    expect(paragraph1).toBeInTheDocument();
+    expect(paragraph2).toBeInTheDocument();
+  });
+
+  it('A página deve conter uma imagem de uma pokédex', () => {
+    renderWithRouter(<About />);
+    const image = screen.getByRole('img');
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(image).toHaveAttribute('alt', 'Pokédex');
+  });
+});

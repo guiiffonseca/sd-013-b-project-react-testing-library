7a4d6f6788d4a84c889f80faee19d160e898a8fa
mateusparr <mateus.dji@icloud.com>
2021-08-20 16:19:17 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa a pagina de favoritos', () => {
+  it('Renderiza a mensagem de quando nao houver favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const mesage = screen.getByText('No favorite pokemon found');
+    expect(mesage).toBeInTheDocument();
+  });
+});

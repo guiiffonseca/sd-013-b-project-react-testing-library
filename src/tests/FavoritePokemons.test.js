e96473a18bd2460ed67c0e91e919716866b7f33f
Jessica Naiara <jessicanaiarag@gmail.com>
2021-08-19 22:48:25 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,11 +2,26 @@ import { screen } from '@testing-library/react';
 import React from 'react';
 import FavoritePokemons from '../components/FavoritePokemons';
 import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
 
 describe('Testa o FavoritePokemons.js', () => {
   test('Testa se é exibido a msm No favorite pokemon found', () => {
     renderWithRouter(<FavoritePokemons />);
     const msg = screen.getByText(/No favorite pokemon found/i);
     expect(msg).toBeInTheDocument();
+  });
+
+  test('Testa se é exibido todos os cards de pokémons favoritados', () => {
+    const favoritePokemons = [
+      pokemons[0],
+      pokemons[1],
+    ];
+
+    renderWithRouter(<FavoritePokemons pokemons={ favoritePokemons } />);
+    const pokemonName = screen.getAllByTestId('pokemon-name');
+    expect(pokemonName.length).toBe(favoritePokemons.length);
+    pokemonName.forEach((pokemon, indice) => {
+      expect(pokemon).toHaveTextContent(favoritePokemons[indice].name);
+    });
   });
 });

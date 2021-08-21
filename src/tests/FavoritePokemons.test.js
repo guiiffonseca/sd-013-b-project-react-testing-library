2717177e36984a458cb2cbc76bc072c6533986eb
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-08-21 14:12:16 -0300

    Requisito 3 funcionando

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-// test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './util/renderWithRouter';
+import App from '../App';
+
+describe('tests the component about and its elements', () => {
+  beforeEach(() => renderWithRouter(<App />), 0);
+
+  it('should have "No favorite pokemon found" text', () => {
+    userEvent.click(screen.getByRole('link', { name: /Favorite Pokémon/ }));
+
+    const notFoundText = screen.getByText('No favorite pokemon found');
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  it('should have a favorite pokemon information', () => {
+    userEvent.click(screen.getByRole('link', { name: /more details/i }));
+    userEvent.click(screen.getByLabelText('Pokémon favoritado?'));
+    userEvent.click(screen.getByRole('link', { name: /Favorite Pokémon/ }));
+    userEvent.click(screen.getByRole('link', { name: /more details/i }));
+  });
+});

c3a6b9d0b9ed193c4ec6c2ad65a99f5e7333971b
Luiz Carlos <luizcarlosvl@hotmail.com>
2021-08-21 19:39:49 -0300

    requisito 2

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('testando o componente NotFound', () => {
+  test('se página contém um heading h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+    const heading = screen.getByText('Page requested not found');
+    expect(heading).toBeInTheDocument();
+  });
+  test('se existe a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    render(<NotFound />);
+    const image = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

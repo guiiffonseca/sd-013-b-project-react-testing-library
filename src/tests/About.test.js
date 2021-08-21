6a7e5252a6817685cdb691ae7a25f786d12d7972
fabio <fabiojuvenalpereira@hotmail.com>
2021-08-21 20:57:20 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,44 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+
+import About from '../components/About';
+
+describe('Testando componentes da tela About', () => {
+  beforeEach(() => {
+    renderWithRouter(<About />);
+  });
+
+  it('testa se existe o texto h2 com texto "about Pokédex" ', () => {
+    const mainTextPage = screen.getByRole('heading', {
+      name: /About Pokédex/i,
+      level: 2,
+    });
+
+    expect(mainTextPage).toBeInTheDocument();
+  });
+
+  it('Testa se a página contem informações sobre a pokedex', () => {
+    const image = screen.getByRole('img', {
+      name: /Pokédex/i,
+    });
+
+    expect(image).toBeInTheDocument();
+  });
+
+  it('Testa se a páginacontem 2 paragrafos com texto sobre a pokédex', () => {
+    const haveParagraphs = screen.getAllByText(/pokémon/i);
+
+    expect(haveParagraphs).toHaveLength(2);
+    haveParagraphs.forEach((paragraph) => {
+      expect(paragraph).toBeInTheDocument();
+    });
+  });
+
+  it('Testa se a imagem "https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png" aparece na tela', () => {
+    const haveImage = screen.getByAltText(/Pokédex/i);
+
+    expect(haveImage).toBeInTheDocument();
+    expect(haveImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});

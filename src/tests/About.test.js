fe837a6c063612c656652a48a03fdb4da1ae7740
AC-Oliveira <allancardoso21@gmail.com>
2021-08-21 11:50:01 -0300

    Requisito 3

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -9,14 +9,30 @@ test('A página deve conter um heading h2 com o texto About Pokédex.', () => {
       <About />
     </MemoryRouter>,
   );
-  const pokedexImageUrl = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
   const aboutPokedexText = screen.getByRole('heading', {
     name: /about pokédex/i,
     level: 2,
   });
   expect(aboutPokedexText).toBeInTheDocument();
-  const pokemonText = screen.getAllByText(/pokémons/i);
+});
+
+test('A página contém dois parágrafos com texto sobre a Pokédex.', () => {
+  render(
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>,
+  );
+  const pokemonText = screen.getAllByText(/pokédex/i);
   expect(pokemonText.length).toBe(2);
+});
+
+test('A página deve conter a imagem de uma Pokédex.', () => {
+  render(
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>,
+  );
+  const pokedexImageUrl = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
   const pokedexImage = screen.getByRole('img');
   expect(pokedexImage).toHaveAttribute('src', pokedexImageUrl);
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router';
+import { screen, render } from '@testing-library/react';
+import App from '../App';
+
+function renderWithRoute(component) {
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={ history }>{component}</Router>), history,
+  });
+}
+
+test(
+  'Exibe na tela a mensagem No favorite pokemon found,se não tiver pokémons favoritos',
+  () => {
+    const { history } = renderWithRoute(<App />);
+    history.push('/favorites');
+    const noFavText = screen.getByText(/no favorite pokemon found/i);
+    expect(noFavText).toBeInTheDocument();
+  },
+);

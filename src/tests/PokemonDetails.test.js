708107c79d25baa0a7e38cc7e08870c030103619
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-21 15:01:52 -0300

    Requisito 6 - test 2 - Página contém seção com os mapas contendo as localizações do pokémon

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -5,7 +5,7 @@ import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('PokemonDetails funciona corretamente', () => {
-  it('Informações detalhadas do Pokémon selecionado são mostradas na tela', () => {
+  it('Exibe informações detalhadas do Pokémon selecionado são mostradas na tela', () => {
     renderWithRouter(<App />);
 
     const moreDetailsLink = screen.getByText(/more details/i);
@@ -26,7 +26,25 @@ describe('PokemonDetails funciona corretamente', () => {
     expect(pokeDetails).toBeInTheDocument();
   });
 
-  it('', () => {});
+  it('Página contém seção com os mapas contendo as localizações do pokémon', () => {
+    renderWithRouter(<App />);
+
+    const moreDetailsLink = screen.getByText(/more details/i);
+    const poke = 'Pikachu';
+
+    userEvent.click(moreDetailsLink);
+    const gameLocations = screen
+      .getByRole('heading', { name: `Game Locations of ${poke}`, level: 2 });
+
+    expect(gameLocations).toBeInTheDocument();
+
+    const pokePlacesFounded = screen.getByText('Kanto Power Plant');
+    const maps = screen.getAllByRole('img', { name: `${poke} location` });
+
+    expect(maps.length).toBe(2);
+    expect(pokePlacesFounded).toBeInTheDocument();
+    expect(maps[1].src).toBe('https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+  });
 
   it('', () => {});
 });

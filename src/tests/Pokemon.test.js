d65124018b2246e3cfe50d4f56250ced3bfb6c6e
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-21 19:21:05 -0300

    Pokemon - test 1 - É renderizado um card com as informações de determinado pokémon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Componente Pokemon funciona corretamente', () => {
+  it('É renderizado um card com as informações de determinado pokémon', () => {
+    renderWithRouter(<App />);
+
+    const pokeName = screen.getByTestId('pokemon-name');
+    const pokeType = screen.getByTestId('pokemon-type');
+    const pokeWeight = screen.getByTestId('pokemon-weight');
+    const pokeImg = screen.getByRole('img', { name: 'Pikachu sprite' });
+    const pokeImgAlt = screen.getByAltText('Pikachu sprite');
+
+    expect(pokeName).toBeInTheDocument();
+    expect(pokeType).toBeInTheDocument();
+    expect(pokeWeight).toBeInTheDocument();
+    expect(pokeImg).toBeInTheDocument();
+    expect(pokeImgAlt).toBeInTheDocument();
+
+    expect(pokeName).toHaveTextContent('Pikachu');
+    expect(pokeType).toHaveTextContent('Electric');
+    expect(pokeWeight).toHaveTextContent('Average weight: 6.0 kg');
+    expect(pokeImg.src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+  it('', () => {});
+  it('', () => {});
+  it('', () => {});
+  it('', () => {});
+});

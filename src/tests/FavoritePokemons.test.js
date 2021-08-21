fe91483b279f234a24d165ea45e423eb2cc138fc
Paulo Lima <paulynho_lima@hotmail.com>
2021-08-21 18:53:45 -0300

    requisito 3 e 4 resolv

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('FavoritePokemons.js', () => {
+  test(`testa se é exibido na tela a msn "No favorite pokemon found"
+    quando não há pokemon favorito`, () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+
+    const textNotFavorit = screen.getByText('No favorite pokemon found');
+    expect(textNotFavorit).toBeInTheDocument();
+  });
+
+  test('testa se é exibido todos os cards de "pokémons favoritados"', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/78');
+
+    const cardsFavorit = screen.getByRole('img', {
+      name: 'Rapidash sprite',
+    });
+    expect(cardsFavorit).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/5/58/Spr_5b_078.png');
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('NotFound.js', () => {
+  test('testa se contém um h2 com texto "Page requested not found 😭"', () => {
+    renderWithRouter(<NotFound />);
+
+    const textNotFund = screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    });
+
+    expect(textNotFund).toBeInTheDocument();
+  });
+  test('testa se mostra imagem ', () => {
+    renderWithRouter(<NotFound />);
+
+    // https://stackoverflow.com/questions/54593369/unable-to-find-an-element-with-the-text-mytext-error-when-using-react-testing
+    const image = screen.getByAltText(/pikachu crying because the page/i);
+
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

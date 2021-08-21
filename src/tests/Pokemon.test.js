69fde8565ab1cf9cc812ce5b96fedcb8aeef6650
Natalia Camarinha <natcamarinha@gmail.com>
2021-08-21 18:57:58 -0300

    correção lint

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -4,6 +4,10 @@ import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
+const moreDetails = screen.getByRole('link', {
+  name: 'More details',
+});
+
 describe('testa o componente Pokemon', () => {
   test('testa se é renderizado o pokemon card com as informações', () => {
     renderWithRouter(<App />);
@@ -20,25 +24,25 @@ describe('testa o componente Pokemon', () => {
     const pokemonImg = screen.getByAltText('Pikachu sprite');
     expect(pokemonImg).toBeInTheDocument();
     expect(pokemonImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
-    const moreDetails = screen.getByRole('link', {
-      name: 'More details',
-    });
+    // const moreDetails = screen.getByRole('link', {
+    //   name: 'More details',
+    // });
     expect(moreDetails).toBeInTheDocument();
   });
   test('testa se o card contem link para exibir detalhes do pokemon', () => {
     renderWithRouter(<App />);
 
-    const moreDetails = screen.getByRole('link', {
-      name: 'More details',
-    });
+    // const moreDetails = screen.getByRole('link', {
+    //   name: 'More details',
+    // });
     expect(moreDetails).toHaveAttribute('href', '/pokemons/25');
   });
   test('testa se link more details redireciona para a página de detalhes', () => {
     renderWithRouter(<App />);
 
-    const moreDetails = screen.getByRole('link', {
-      name: 'More details',
-    });
+    // const moreDetails = screen.getByRole('link', {
+    //   name: 'More details',
+    // });
     userEvent.click(moreDetails);
     const detailsPage = screen.getByRole('heading', {
       level: 2,
@@ -49,9 +53,9 @@ describe('testa o componente Pokemon', () => {
   test('testa se a URL contem pokemons/<id>', () => {
     const { history } = renderWithRouter(<App />);
 
-    const moreDetails = screen.getByRole('link', {
-      name: 'More details',
-    });
+    // const moreDetails = screen.getByRole('link', {
+    //   name: 'More details',
+    // });
     userEvent.click(moreDetails);
     const { pathname } = history.location;
     expect(pathname).toBe('/pokemons/25');
@@ -59,9 +63,9 @@ describe('testa o componente Pokemon', () => {
   test('testa se existe icone de estrela nos pokemons favoritados', () => {
     renderWithRouter(<App />);
 
-    const moreDetails = screen.getByRole('link', {
-      name: 'More details',
-    });
+    // const moreDetails = screen.getByRole('link', {
+    //   name: 'More details',
+    // });
     userEvent.click(moreDetails);
     const favoriteInput = screen.getByLabelText(/pokémon favoritado/i);
     userEvent.click(favoriteInput);

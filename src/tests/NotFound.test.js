8e40a05e2f5d64ffd2c8552fbadbb41027f925ea
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2021-08-21 18:48:48 -0300

    desafio 4 - NotFound - concluido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente NotFound', () => {
+  test('testa se é renderizada uma pagina de notFoud quando a rota não existe', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('./rotaErrada');
+    const textNotFound = screen.getByRole('heading',
+      { level: 2, name: /page requested not found/i });
+    expect(textNotFound).toBeInTheDocument();
+
+    const imgNotFound = screen.getByRole('img', { name: /Pikachu crying because/i });
+    expect(imgNotFound).toBeInTheDocument();
+    expect(imgNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

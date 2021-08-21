4baa12f11bbf218d3bf4ba647b48947fb40a03e3
andre9653 <andresena9653@gmail.com>
2021-08-20 22:49:46 -0300

    Edições requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -17,15 +17,6 @@ const pokemon = {
   moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
 };
 
-test('', () => {
-  renderWithRouter(<Pokemon pokemon={ pokemon } isFavorite={ false } />);
-  expect(screen.getByTestId('pokemon-name')).toBeInTheDocument();
-  expect(screen.getByText('Electric')).toBeInTheDocument();
-  expect(screen.getByTestId('pokemon-weight')).toBeInTheDocument();
-  expect(screen.getByRole('img').src).toContain(pokemon.image);
-  expect(screen.getByRole('img').alt).toContain(`${pokemon.name} sprite`);
-});
-
 test('Teste se é renderizado um card com as informações de determinado pokémon.', () => {
   renderWithRouter(<App />);
   const linkMoreDetails = screen.getByRole('link', { name: /more details/i });
@@ -36,4 +27,13 @@ test('Teste se é renderizado um card com as informações de determinado pokém
   expect(screen.getByRole('checkbox', { checked: true })).toBeInTheDocument();
   const imgFavorite = screen.getByAltText('Pikachu is marked as favorite');
   expect(imgFavorite.src).toContain('/star-icon.svg');
+});
+
+test('', () => {
+  renderWithRouter(<Pokemon pokemon={ pokemon } isFavorite={ false } />);
+  expect(screen.getByText('Pikachu')).toBeInTheDocument();
+  expect(screen.getByText('Electric')).toBeInTheDocument();
+  expect(screen.getByText('Average weight: 6.0 kg')).toBeInTheDocument();
+  expect(screen.getByRole('img').src).toContain(pokemon.image);
+  expect(screen.getByRole('img').alt).toContain(`${pokemon.name} sprite`);
 });

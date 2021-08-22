2b996f06085afae5bb1c9d7f60303a80f58ffe16
Daniel Teixeira <daniel.o.teixeira@gmail.com>
2021-08-22 05:12:49 -0300

    requisito 3 concluido

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa pokemons favoritados', () => {
+  test('Testa se é exibida a mensagem No favorite pokemon found', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+    const msg = screen.getByText(/No favorite pokemon found/i);
+    expect(msg).toBeInTheDocument();
+  });
+});

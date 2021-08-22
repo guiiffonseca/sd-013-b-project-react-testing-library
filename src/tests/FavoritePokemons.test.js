20380e634fee042106f4a16569439ca91da1d9ec
Dororops <leeurw@hotmail.com>
2021-08-22 17:24:49 -0300

    Adicionando requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+// import { Router } from 'react-router';
+// import { BrowserRouter } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+// import { createMemoryHistory } from 'history';
+// import { MemoryRouter } from 'react-router';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from './utils/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const infoPokemons = screen.getByText('No favorite pokemon found');
+    expect(infoPokemons).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    const charmanderId = 4;
+
+    const favoritePokemon = pokemons.filter((pokemon) => pokemon.id === charmanderId);
+
+    renderWithRouter(<FavoritePokemons pokemons={ favoritePokemon } />);
+
+    const charmanderName = screen.getByText('Charmander');
+    expect(charmanderName).toBeInTheDocument();
+    const pokemonName = screen.getByTestId('pokemon-name');
+    const pokemonType = screen.getByTestId('pokemon-type');
+    const pokemonWeight = screen.getByTestId('pokemon-weight');
+    expect(pokemonName).toBeTruthy();
+    expect(pokemonType).toBeTruthy();
+    expect(pokemonWeight).toBeTruthy();
+  });
+});

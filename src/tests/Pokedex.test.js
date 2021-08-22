fd4630647c98c55b09e1f397d65823eb298005b4
Aleilton Chavenco <aleilton@gmail.com>
2021-08-21 21:16:10 -0300

    Requisito 5 - ESLint

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -9,63 +9,63 @@ const pokemonsMock = [
     id: 25,
     name: 'Pikachu',
     type: 'Electric',
-    averageWeight: {value: '6.0', measurementUnit: 'kg',},
+    averageWeight: { value: '6.0', measurementUnit: 'kg' },
     image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
   },
   {
     id: 4,
     name: 'Charmander',
     type: 'Fire',
-    averageWeight: {value: '8.5', measurementUnit: 'kg',},
+    averageWeight: { value: '8.5', measurementUnit: 'kg' },
     image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
   },
   {
     id: 10,
     name: 'Caterpie',
     type: 'Bug',
-    averageWeight: {value: '2.9', measurementUnit: 'kg',},
+    averageWeight: { value: '2.9', measurementUnit: 'kg' },
     image: 'https://cdn2.bulbagarden.net/upload/8/83/Spr_5b_010.png',
   },
   {
     id: 23,
     name: 'Ekans',
     type: 'Poison',
-    averageWeight: {value: '6.9', measurementUnit: 'kg',},
+    averageWeight: { value: '6.9', measurementUnit: 'kg' },
     image: 'https://cdn2.bulbagarden.net/upload/1/18/Spr_5b_023.png',
   },
   {
     id: 65,
     name: 'Alakazam',
     type: 'Psychic',
-    averageWeight: {value: '48.0', measurementUnit: 'kg',},
+    averageWeight: { value: '48.0', measurementUnit: 'kg' },
     image: 'https://cdn2.bulbagarden.net/upload/8/88/Spr_5b_065_m.png',
   },
   {
     id: 151,
     name: 'Mew',
     type: 'Psychic',
-    averageWeight: {value: '4.0', measurementUnit: 'kg',},
+    averageWeight: { value: '4.0', measurementUnit: 'kg' },
     image: 'https://cdn2.bulbagarden.net/upload/4/43/Spr_5b_151.png',
   },
   {
     id: 78,
     name: 'Rapidash',
     type: 'Fire',
-    averageWeight: {value: '95.0', measurementUnit: 'kg',},
+    averageWeight: { value: '95.0', measurementUnit: 'kg' },
     image: 'https://cdn2.bulbagarden.net/upload/5/58/Spr_5b_078.png',
   },
   {
     id: 143,
     name: 'Snorlax',
     type: 'Normal',
-    averageWeight: {value: '460.0', measurementUnit: 'kg',},
+    averageWeight: { value: '460.0', measurementUnit: 'kg' },
     image: 'https://cdn2.bulbagarden.net/upload/4/40/Spr_5b_143.png',
   },
   {
     id: 148,
     name: 'Dragonair',
     type: 'Dragon',
-    averageWeight: {value: '16.5', measurementUnit: 'kg',},
+    averageWeight: { value: '16.5', measurementUnit: 'kg' },
     image: 'https://cdn2.bulbagarden.net/upload/2/2c/Spr_5b_148.png',
   },
 ];
@@ -131,8 +131,8 @@ describe(' Teste o componente <Pokedex.js />', () => {
       </MemoryRouter>,
     );
 
-    const pokemonsExibidos = screen.getAllByTestId('pokemon-name');
-    expect(pokemonsExibidos.length).toBe(1);
+    const namePokemon = screen.getAllByTestId('pokemon-name');
+    expect(namePokemon.length).toBe(1);
   });
 
   test('se a Pokédex tem os botões de filtro', () => {
@@ -164,13 +164,13 @@ describe(' Teste o componente <Pokedex.js />', () => {
       name: /fire/i,
     });
     userEvent.click(buttonFire);
-    const selectedPokemon = screen.getByTestId('pokemon-name');
-    expect(selectedPokemon).toHaveTextContent(firePokemon[0].name);
+    const namePokemon = screen.getByTestId('pokemon-name');
+    expect(namePokemon).toHaveTextContent(firePokemon[0].name);
     const buttonProximoPokemon = screen.getByRole('button', {
       name: /próximo pokémon/i,
     });
     userEvent.click(buttonProximoPokemon);
-    expect(selectedPokemon).toHaveTextContent(firePokemon[1].name);
+    expect(namePokemon).toHaveTextContent(firePokemon[1].name);
 
     // O botão All está sempre visível
     const buttonAll = screen.getByRole('button', {

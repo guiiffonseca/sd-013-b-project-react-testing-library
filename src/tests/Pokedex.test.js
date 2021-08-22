9f50bdc9a6db8fe64862def6120899a41b75c3c8
Alexsander-Trybe <Alexsander81002745@gmail.com>
2021-08-22 02:18:36 -0300

    Lint correção

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,5 +1,4 @@
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from '../services/renderWithRouter';
@@ -26,6 +25,4 @@ test('', () => {
   const numberOfButtons = 7;
   expect(filterBurrtons.length).toBe(numberOfButtons);
   expect(filterBurrtons[0]).toHaveTextContent('Electric');
-
-
 });

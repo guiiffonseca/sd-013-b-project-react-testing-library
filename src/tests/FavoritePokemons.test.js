338af5a9d07f7903d003fdab193c4a878ac65fe0
Luiz Carlos <luizcarlosvl@hotmail.com>
2021-08-21 22:11:00 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('testando o componente FavoritePokemons', () => {
+  test(`se é exibido na tela a mensagem No favorite pokemon found,
+    se a pessoa não tiver pokémons favoritos`, () => {
+    render(<FavoritePokemons />);
+    const message = screen.getByText('No favorite pokemon found');
+    expect(message).toBeInTheDocument();
+  });
+});

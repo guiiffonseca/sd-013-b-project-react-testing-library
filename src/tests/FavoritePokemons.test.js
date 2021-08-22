c1b485a6f4bfebfda555856e163b54106acd4619
BrunoDevMoraes <brunodevmoraes@gmail.com>
2021-08-22 02:10:47 -0300

    Adiciona req 5

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,8 +1,29 @@
-// import React from 'react';
-// import { screen } from '@testing-library/react';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-// import renderWithRouter from '../renderWithRouter';
-// import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
 
-test('', () => {
+// inspirado no código de Rafael Reis - Turma 13 - Tribo B
+test('if it renders a message saying that there isn\'t favorite pokemons', () => {
+  renderWithRouter(<App />);
+
+  const favorites = screen.getByRole('link', { name: 'Favorite Pokémons' });
+  userEvent.click(favorites);
+  const message = screen.getByText('No favorite pokemon found');
+  expect(message).toBeInTheDocument();
+});
+
+test('if it renders favorite pokémons', () => {
+  renderWithRouter(<App />);
+
+  const details = screen.getByRole('link', { name: 'More details' });
+  userEvent.click(details);
+  const checkbox = screen.getByLabelText('Pokémon favoritado?');
+  userEvent.click(checkbox);
+  const favorites = screen.getByRole('link', { name: 'Favorite Pokémons' });
+  userEvent.click(favorites);
+  const pikachuStarImg = screen.getByAltText('Pikachu is marked as favorite');
+  expect(pikachuStarImg).toBeInTheDocument();
 });

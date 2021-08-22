a6bd6e3ea8c8384fdd7723d4805e5bed51a7bb00
Luiz Carlos <luizcarlosvl@hotmail.com>
2021-08-22 09:58:50 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+import App from '../App';
+
+describe('testando o componente App', () => {
+  test('se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByText('Home');
+    const about = screen.getByText('About');
+    const favorites = screen.getByText('Favorite Pokémons');
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favorites).toBeInTheDocument();
+  });
+  test(`se a aplicação é redirecionada para a página inicial,
+   na URL / ao clicar no link Home da barra de navegação`, () => {
+    renderWithRouter(<App />);
+    const home = screen.getByText('Home');
+    expect(home).toHaveAttribute('href', '/');
+  });
+  test(`se a aplicação é redirecionada para a página de About,
+   na URL /about, ao clicar no link About da barra de navegação`, () => {
+    renderWithRouter(<App />);
+    const about = screen.getByText('About');
+    expect(about).toHaveAttribute('href', '/about');
+  });
+  test(`se a aplicação é redirecionada para a página de Pokémons Favoritados,
+   na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação`, () => {
+    renderWithRouter(<App />);
+    const favorites = screen.getByText('Favorite Pokémons');
+    expect(favorites).toHaveAttribute('href', '/favorites');
+  });
+});

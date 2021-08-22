56aa7eb643e23c33640a31d1dda0311e18cdd1a6
Jonmorais <jonathan_ms15@hotmail.com>
2021-08-22 17:31:08 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Test the component <FavoritePokemons.js />', () => {
+  it('show the message No favorite pokemon found, if there are no favorites', () => {
+    // Render:
+    renderWithRouter(<FavoritePokemons />);
+
+    // Get the elements and then test if they exist in the document:
+    const NoFavorites = screen.getByText('No favorite pokemon found');
+    expect(NoFavorites).toBeInTheDocument();
+  });
+});

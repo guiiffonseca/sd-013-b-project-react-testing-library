55d25d7d63f3614e0e65de06ac941774c5b40105
Leonardo <leo_mc_inter@hotmail.com>
2021-08-22 20:34:23 -0300

    Requisito pokemon teste para 80%

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -17,47 +17,5 @@ describe('Testa o componente Pokemon', () => {
     expect(screen.getAllByText('Electric')[0]).toBeInTheDocument();
     expect(screen.getByText(/Average weight/)).toBeInTheDocument();
     expect(screen.getByAltText('Pikachu sprite')).toBeInTheDocument();
-    expect(screen.getByRole('img')).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
-  });
-
-  test('verifica se há um link com id do pokemon e se redireciona para a página correta',
-    () => {
-      const { history } = renderWithRouter(<App />);
-      const POKEMON_LINK = '/pokemons/25';
-      const moreDetailsLink = screen.getByText('More details');
-      expect(moreDetailsLink).toHaveAttribute('href', POKEMON_LINK);
-      userEvent.click(moreDetailsLink);
-      history.push(POKEMON_LINK);
-      expect(screen.getByRole('heading', {
-        level: 2,
-        name: /Summary/,
-      }));
-      const { location: { pathname } } = history;
-      expect(pathname).toBe(POKEMON_LINK);
-    });
-
-  test('verifica se há uma estrela no pokemon favoritado', () => {
-    render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
-
-    const moreDetailsLink = screen.getByText('More details');
-    userEvent.click(moreDetailsLink);
-
-    expect(screen.getByRole('heading', {
-      level: 2,
-      name: /Summary/,
-    }));
-
-    const favoriteButton = screen.getByRole('checkbox', {
-      checked: false,
-    });
-    userEvent.click(favoriteButton);
-
-    const starImage = screen.getByAltText('Pikachu is marked as favorite');
-    expect(starImage).toBeInTheDocument();
-    expect(starImage).toHaveAttribute('src', '/star-icon.svg');
   });
 });

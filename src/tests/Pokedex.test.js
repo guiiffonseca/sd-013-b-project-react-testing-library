05ce2790e1d93026ffcda53f458184dd420d73cb
Alexsander-Trybe <Alexsander81002745@gmail.com>
2021-08-22 02:16:31 -0300

    Requisito 5: 83

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+
+test('', () => {
+  renderWithRouter(<App />);
+
+  const headingPokedex = screen.getByRole('heading', {
+    level: 2,
+    name: /Encountered pokémons/i,
+  });
+  expect(headingPokedex).toBeInTheDocument();
+
+  const buttons = screen.getAllByTestId('pokemon-type-button');
+  const numberOoFilters = 7;
+  expect(buttons.length).toBe(numberOoFilters);
+
+  const buttonAll = screen.getByTestId('pokemon-all-button');
+  expect(buttonAll).toHaveTextContent('All');
+  const nextPokemon = screen.getByTestId('next-pokemon');
+  expect(nextPokemon).toBeInTheDocument();
+
+  const filterBurrtons = screen.getAllByTestId('pokemon-type-button');
+  const numberOfButtons = 7;
+  expect(filterBurrtons.length).toBe(numberOfButtons);
+  expect(filterBurrtons[0]).toHaveTextContent('Electric');
+
+
+});

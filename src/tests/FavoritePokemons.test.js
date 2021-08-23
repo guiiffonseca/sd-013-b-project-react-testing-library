8e5a650743c25e0d447547df61b612b2a7fa873d
Mayu <mayusatori@gmail.com>
2021-08-22 23:09:53 -0300

    Resolve o requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa o componente FavoritePokemons', () => {
+  test('Teste se é exibido No favorite pokemon found caso não tenha favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    expect(screen.getByText(/No favorite/i)).toBeInTheDocument();
+  });
+  test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/more details/i));
+    fireEvent.click(screen.getByText(/favoritado/i));
+    fireEvent.click(screen.getByText(/favorite/i));
+    expect(screen.getByText(/pikachu/i)).toBeInTheDocument();
+  });
+});

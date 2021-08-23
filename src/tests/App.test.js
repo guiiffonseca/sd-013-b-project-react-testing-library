b953030541e35f781a69bb16704dd2ee015ea2b9
btriz <00btrz@gmail.com>
2021-08-23 00:14:45 -0300

    :white_check_mark: requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,2 +1,99 @@
-test('', () => {});
-//
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+
+import App from '../App';
+
+describe('Testa o componente App.js', () => {
+  test('O topo da aplicação contém um conjunto fixo de links de navegação.', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+
+    const home = screen.getByRole('link', {
+      name: /home/i,
+    });
+
+    const about = screen.getByRole('link', {
+      name: /about/i,
+    });
+
+    const favorite = screen.getByRole('link', {
+      name: /favorite/i,
+    });
+
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favorite).toBeInTheDocument();
+  });
+
+  test('Redireciona para a página inicial ao clicar no link \'Home\'', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+
+    const home = screen.getByRole('link', {
+      name: /home/i,
+    });
+
+    userEvent.click(home);
+
+    const homeTitle = screen.getByRole('heading', {
+      level: 2,
+      name: /Encountered pokémons/i,
+    });
+
+    expect(homeTitle).toBeInTheDocument();
+  });
+
+  test('Redireciona para a página sobre a Pokedéx ao clicar no link \'About\'', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+
+    const about = screen.getByRole('link', {
+      name: /about/i,
+    });
+
+    userEvent.click(about);
+
+    const aboutTitle = screen.getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
+    });
+
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  test('Redireciona para Favoritados ao clicar no link \'Favorite Pokémons\'', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+
+    const favorite = screen.getByRole('link', {
+      name: /favorite/i,
+    });
+
+    userEvent.click(favorite);
+
+    const favoriteTitle = screen.getByRole('heading', {
+      level: 2,
+      name: /Favorite pokémons/i,
+    });
+
+    expect(favoriteTitle).toBeInTheDocument();
+  });
+
+  test('Redireciona para a página não encontrada', () => {
+    const customHistory = createMemoryHistory();
+
+    render(
+      <Router history={ customHistory }>
+        <App />
+      </Router>,
+    );
+
+    customHistory.push('/rota-nao-existente');
+
+    const notFoundTitle = screen.getByRole('heading', {
+      level: 2,
+      name: /not found/i,
+    });
+
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+});

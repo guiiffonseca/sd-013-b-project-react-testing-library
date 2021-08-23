e7b7d63c2e8f88c922670ad98ff0779aeb48454f
Thaís Sampaio <thais_castrocs@outlook.com>
2021-08-23 00:29:39 -0300

    Requisito 2 Realizado

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from '../utilities/renderWithRouter';
+
+describe('Testando o Componente About', () => {
+  test('Teste se a página contém as informações sobre a Pokédex.', () => {
+    renderWithRouter(<About />);
+    expect(screen.getByText(/This application simulates a Pokédex/i)).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+    renderWithRouter(<About />);
+    const header = screen.getByRole('heading', { level: 2 });
+    expect(header.textContent).toBe('About Pokédex');
+    expect(header).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    renderWithRouter(<About />);
+    expect(screen.getAllByText(/Pokémons/i).length).toBe(2);
+  });
+
+  test('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
+    renderWithRouter(<About />);
+    const image = screen.getByAltText('Pokédex');
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});

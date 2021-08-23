9109c8fa165ceff228d2c2a17a3ca7d2e43a6750
Camila Dornas <camiladornas@gmail.com>
2021-08-23 20:44:07 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,45 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa os links do arquivo App.js', () => {
+  it('Testa se redireciona para a página inicial ao clicar no link Home.', () => {
+    // Renderizar a pag resgatando do history
+    const { history } = renderWithRouter(<App />);
+    // 1. Acessar os elementos da tela
+    const renderHome = screen.getByRole('link', { name: /Home/i });
+    // 2. Interagir com os elementos acessados (caso necessário)
+    fireEvent.click(renderHome);
+    // 3. Criar o teste
+    const { pathname } = history.location;
+    expect(renderHome).toBeInTheDocument();
+    expect(pathname).toBe('/');
+  });
+
+  it('Testa se redireciona para a página de about ao clicar no link About.', () => {
+    // Renderizar a pag resgatando do history
+    const { history } = renderWithRouter(<App />);
+    // 1. Acessar os elementos da tela
+    const renderAbout = screen.getByRole('link', { name: /about/i });
+    // 2. Interagir com os elementos acessados (caso necessário)
+    fireEvent.click(renderAbout);
+    // 3. Criar o teste
+    const { pathname } = history.location;
+    expect(renderAbout).toBeInTheDocument();
+    expect(pathname).toBe('/about');
+  });
+
+  it('Testa se redireciona para a página de about ao clicar no link Favorites.', () => {
+    // Renderizar a pag resgatando do history
+    const { history } = renderWithRouter(<App />);
+    // 1. Acessar os elementos da tela
+    const renderFavorites = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    // 2. Interagir com os elementos acessados (caso necessário)
+    fireEvent.click(renderFavorites);
+    // 3. Criar o teste
+    const { pathname } = history.location;
+    expect(renderFavorites).toBeInTheDocument();
+    expect(pathname).toBe('/favorites');
+  });
+});

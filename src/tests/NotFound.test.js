ce60dcaa28121ae2c432d2701b2142070228ca1c
Daniel Teixeira <daniel.o.teixeira@gmail.com>
2021-08-23 03:29:03 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente NotFound', () => {
+  test('Deveria aparecer um h2 com o texto: Page requested not found', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/page-not-found');
+
+    const h2Text = screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    });
+
+    expect(h2Text).toBeInTheDocument();
+  });
+
+  test('Deveria exibir um gif na pagina', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/page-not-found');
+
+    const notFoundGif = screen.getByAltText(/pikachu crying/i);
+
+    expect(notFoundGif).toBeInTheDocument();
+    expect(notFoundGif).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

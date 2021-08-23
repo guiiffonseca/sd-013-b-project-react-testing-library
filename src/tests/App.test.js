52122dbbf568ec9e895aa1d315343bc583e18101
thiagoteixas <thiagoteixas@gmail.com>
2021-08-23 10:46:03 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+import App from '../App';
+
+test('Primeiro link deve possuir o texto home', () => {
+  renderWithRouter(<App />);
+  const linkElement = screen.getByText(/home/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Segundo link deve possuir o texto about', () => {
+  renderWithRouter(<App />);
+  const linkElement = screen.getByText(/about/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Terceiro link deve possuir o texto Favorite Pokémons', () => {
+  renderWithRouter(<App />);
+  const linkElement = screen.getByText('Favorite Pokémons');
+  expect(linkElement).toBeInTheDocument();
+});

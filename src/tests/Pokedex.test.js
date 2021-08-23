f55220cf6e1679ae309b8fe5a4ce1ff9f096b664
Mayu <mayusatori@gmail.com>
2021-08-23 01:20:48 -0300

    Requisito 5 resolvido

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,10 +1,63 @@
-// import React from 'react';
-// import { fireEvent, screen, within } from '@testing-library/react';
-// import renderWithRouter from '../renderWithRouter';
-// import App from '../App';
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import Pokedex from '../components/Pokedex';
+import pokemons from '../data';
 
-describe('', () => {
-  test('', () => {
-
+describe('Testa o componente Pokedex', () => {
+  const POKEMON_NAME_ID = 'pokemon-name';
+  test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+    renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ { 25: true } }
+    />);
+    expect(screen.getByRole('heading', { level: 2 }).textContent)
+      .toBe('Encountered pokémons');
+  });
+  test('Teste se o botão Próximo pokémon funciona', () => {
+    renderWithRouter(<Pokedex
+      pokemons={ [pokemons[0], pokemons[1]] }
+      isPokemonFavoriteById={ { 25: true, 4: true } }
+    />);
+    const btn = screen.getByTestId('next-pokemon');
+    fireEvent.click(btn);
+    expect(screen.getByText(/charmander/i)).toBeInTheDocument();
+    fireEvent.click(btn);
+    expect(screen.getByText(/pikachu/i)).toBeInTheDocument();
+  });
+  test('Teste se é mostrado apenas um pokemon por vez', () => {
+    renderWithRouter(<Pokedex
+      pokemons={ [pokemons[0], pokemons[1]] }
+      isPokemonFavoriteById={ { 25: true, 4: true } }
+    />);
+    const pokemon = screen.getAllByTestId(POKEMON_NAME_ID);
+    expect(pokemon.length).toBe(1);
+  });
+  test('Teste se a Pokédex tem os botões de filtro', () => {
+    renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ { 25: true, 65: true, 151: true } }
+    />);
+    const btns = screen.getAllByTestId('pokemon-type-button');
+    const btnsLenght = 7;
+    expect(btns.length).toBe(btnsLenght);
+    fireEvent.click(btns[4]);
+    expect(btns[4].textContent).toBe('Psychic');
+    expect(screen.getByText('All')).toBeInTheDocument();
+    expect(screen.getByTestId(POKEMON_NAME_ID).textContent).toBe('Alakazam');
+    fireEvent.click(screen.getByTestId('next-pokemon'));
+    expect(screen.getByTestId(POKEMON_NAME_ID).textContent).toBe('Mew');
+  });
+  test('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+    renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ { 25: true, 65: true, 151: true } }
+    />);
+    expect(screen.getByText('All')).toBeInTheDocument();
+    const btns = screen.getAllByTestId('pokemon-type-button');
+    fireEvent.click(btns[4]);
+    expect(screen.getByTestId(POKEMON_NAME_ID).textContent).toBe('Alakazam');
+    fireEvent.click(screen.getByText('All'));
+    expect(screen.getByTestId(POKEMON_NAME_ID).textContent).toBe('Pikachu');
   });
 });

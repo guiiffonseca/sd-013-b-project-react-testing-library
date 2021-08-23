ccbbceb44827001d9c9f8566ce7009e9ea869399
Aleilton Chavenco <aleilton@gmail.com>
2021-08-22 22:48:33 -0300

    Requsito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,86 +1,11 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-// import userEvent from '@testing-library/user-event';
-import Pokemon from '../components/Pokemon';
+import userEvent from '@testing-library/user-event';
 
-const pokemonsMock = [
-  {
-    id: 25,
-    name: 'Pikachu',
-    type: 'Electric',
-    averageWeight: { value: '6.0', measurementUnit: 'kg' },
-    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
-  },
-  {
-    id: 4,
-    name: 'Charmander',
-    type: 'Fire',
-    averageWeight: { value: '8.5', measurementUnit: 'kg' },
-    image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
-  },
-  {
-    id: 10,
-    name: 'Caterpie',
-    type: 'Bug',
-    averageWeight: { value: '2.9', measurementUnit: 'kg' },
-    image: 'https://cdn2.bulbagarden.net/upload/8/83/Spr_5b_010.png',
-  },
-  {
-    id: 23,
-    name: 'Ekans',
-    type: 'Poison',
-    averageWeight: { value: '6.9', measurementUnit: 'kg' },
-    image: 'https://cdn2.bulbagarden.net/upload/1/18/Spr_5b_023.png',
-  },
-  {
-    id: 65,
-    name: 'Alakazam',
-    type: 'Psychic',
-    averageWeight: { value: '48.0', measurementUnit: 'kg' },
-    image: 'https://cdn2.bulbagarden.net/upload/8/88/Spr_5b_065_m.png',
-  },
-  {
-    id: 151,
-    name: 'Mew',
-    type: 'Psychic',
-    averageWeight: { value: '4.0', measurementUnit: 'kg' },
-    image: 'https://cdn2.bulbagarden.net/upload/4/43/Spr_5b_151.png',
-  },
-  {
-    id: 78,
-    name: 'Rapidash',
-    type: 'Fire',
-    averageWeight: { value: '95.0', measurementUnit: 'kg' },
-    image: 'https://cdn2.bulbagarden.net/upload/5/58/Spr_5b_078.png',
-  },
-  {
-    id: 143,
-    name: 'Snorlax',
-    type: 'Normal',
-    averageWeight: { value: '460.0', measurementUnit: 'kg' },
-    image: 'https://cdn2.bulbagarden.net/upload/4/40/Spr_5b_143.png',
-  },
-  {
-    id: 148,
-    name: 'Dragonair',
-    type: 'Dragon',
-    averageWeight: { value: '16.5', measurementUnit: 'kg' },
-    image: 'https://cdn2.bulbagarden.net/upload/2/2c/Spr_5b_148.png',
-  },
-];
-
-const isPokemonFavoriteByIdMock = {
-  4: false,
-  10: false,
-  23: false,
-  25: false,
-  65: false,
-  78: false,
-  143: false,
-  148: false,
-  151: false,
-};
+import App from '../App';
+import pokemons from '../data';
+import renderWithRouter from '../utils/renderWithRouter';
 
 function getPokemonName() {
   return screen.getByTestId('pokemon-name');
@@ -104,24 +29,67 @@ function getPokemonLinkDetail() {
   });
 }
 
-describe('Teste o componente <Pokemon.js />', () => {
-  test('é renderizado um card com as informações de determinado pokémon.', () => {
+function getDetailPage() {
+  return screen.getByRole('heading', {
+    level: 2,
+    name: `${getPokemonName().textContent} Details`,
+  });
+}
+
+function getFavoriteCheckbox() {
+  return screen.getByRole('checkbox');
+}
+
+function getStarFavorite() {
+  return screen.queryByAltText(`${getPokemonName().textContent} is marked as favorite`);
+}
+
+function getPokemonSelected() {
+  return pokemons.find((pokemon) => pokemon.name === getPokemonName().textContent);
+}
+
+describe('é renderizado um card com as informações de determinado pokémon.', () => {
+  test('O nome correto do Pokémon deve ser mostrado na tela;', () => {
     render(
       <MemoryRouter>
-        <Pokemon
-          pokemon={ pokemonsMock[0] }
-          isFavorite={ isPokemonFavoriteByIdMock[pokemonsMock[0].id] }
-        />
+        <App />
       </MemoryRouter>,
     );
 
-    expect(getPokemonName()).toHaveTextContent(pokemonsMock[0].name);
-    expect(getPokemonType()).toHaveTextContent(pokemonsMock[0].type);
-    const { value, measurementUnit } = pokemonsMock[0].averageWeight;
+    expect(getPokemonName()).toHaveTextContent(pokemons[0].name);
+  });
+
+  test('O tipo correto do pokémon deve ser mostrado na tela.', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(getPokemonType()).toHaveTextContent(pokemons[0].type);
+  });
+
+  test('Peso médio deve ser exibido Average weight: <value> <measurementUnit>', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const { value, measurementUnit } = pokemons[0].averageWeight;
     expect(getPokemonWeight())
       .toHaveTextContent(`Average weight: ${value} ${measurementUnit}`);
-    expect(getPokemonImg()).toHaveAttribute('src', pokemonsMock[0].image);
-    expect(getPokemonImg()).toHaveAttribute('alt', `${pokemonsMock[0].name} sprite`);
+  });
+
+  test('A imagem do Pokémon deve ser exibida.', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(getPokemonImg()).toHaveAttribute('src', pokemons[0].image);
+    expect(getPokemonImg()).toHaveAttribute('alt', `${pokemons[0].name} sprite`);
   });
 });
 
@@ -129,39 +97,62 @@ describe('O card do Pokémon contém um link de navegação para exibir detalhes
   test(' O link deve possuir a URL /pokemons/<id>', () => {
     render(
       <MemoryRouter>
-        <Pokemon
-          pokemon={ pokemonsMock[0] }
-          isFavorite={ isPokemonFavoriteByIdMock[pokemonsMock[0].id] }
-        />
+        <App />
       </MemoryRouter>,
     );
 
     expect(getPokemonLinkDetail())
-      .toHaveAttribute('href', `/pokemons/${pokemonsMock[0].id}`);
+      .toHaveAttribute('href', `/pokemons/${pokemons[0].id}`);
   });
 });
 
-// describe('ao clicar no link de navegação do Pokémon, é feito o redirecionamento ', () => {
-//   test('Página de detalhes', async () => {
-//     render(
-//       <MemoryRouter>
-//         <Pokemon
-//           pokemon={ pokemonsMock[0] }
-//           isFavorite={ isPokemonFavoriteByIdMock[pokemonsMock[0].id] }
-//         />
-//       </MemoryRouter>,
-//     );
+describe('ao clicar no link de navegação do Pokémon, é feito o redirecionamento', () => {
+  test('Página de detalhes', async () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
 
-//     userEvent.click(getPokemonLinkDetail());
-//     const pikachuDetail = await screen.findByRole('heading', {
-//       level: 2,
-//       name: 'Pikachu Details',
-//     });
-//     // expect(pikachuDetail).toBeInTheDocument();
-//     // expect(headingDetail).toHaveTextContent(`${pokemonsMock[0].name} Details`);
-//   });
-// });
+    userEvent.click(getPokemonLinkDetail());
+    expect(getDetailPage()).toBeInTheDocument();
+  });
+});
 
-// describe('', () => {
-//   test('', () => {});
-// });
+describe('a URL exibida no navegador muda para /pokemons/<id>', () => {
+  test('Direcionamento', async () => {
+    const { history } = renderWithRouter(<App />);
+
+    userEvent.click(getPokemonLinkDetail());
+    console.log(history.location.pathname);
+    expect(history.location.pathname).toBe(`/pokemons/${getPokemonSelected().id}`);
+  });
+});
+
+describe('Teste se existe um ícone de estrela nos Pokémons favoritados', () => {
+  test('O ícone deve ser uma imagem com o atributo src  /star-icon.svg;', async () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    userEvent.click(getPokemonLinkDetail());
+    userEvent.click(getFavoriteCheckbox());
+    expect(getStarFavorite()).toBeInTheDocument();
+    expect(getStarFavorite()).toHaveAttribute('src', '/star-icon.svg');
+  });
+
+  test('Imagem deve ter o atributo alt: <pokemon> is marked as favorite', async () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    userEvent.click(getPokemonLinkDetail());
+    expect(getStarFavorite()).toBeInTheDocument();
+    expect(getStarFavorite())
+      .toHaveAttribute('alt', `${getPokemonName().textContent} is marked as favorite`);
+  });
+});

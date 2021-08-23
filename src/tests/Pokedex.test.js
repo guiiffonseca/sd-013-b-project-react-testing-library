ca301f104b812be6a7d2a524f3c9c9fe7b09029a
Alexandre Altgauzen <altgauzen@gmail.com>
2021-08-23 13:54:10 +0100

    requisito 5 finalizado

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,101 @@
-test('', () => {});
+import React from 'react';
+import { getByTestId, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../utils/renderWithRouter';
+
+describe('Verifica o componente Pokedex', () => {
+  it('Verifica se há heading h2 com Encountered pokémons', () => {
+    renderWithRouter(<App />);
+    const headingFind = screen.getByRole('heading', {
+      level: 2,
+      name: /Encountered pokémons/i,
+    });
+    expect(headingFind).toBeInTheDocument();
+  });
+
+  it('Verifica se o botão contem o texto Próximo pokémon', () => {
+    renderWithRouter(<App />);
+    const nextButton = screen.getByRole('button', {
+      name: /Próximo pokémon/i,
+    });
+    expect(nextButton).toBeInTheDocument();
+  });
+
+  it('Verifica se são mostrados proximos pokemons da lista ao clicar no botao', () => {
+    renderWithRouter(<App />);
+    const nextButton = screen.getByRole('button', {
+      name: /Próximo pokémon/i,
+    });
+    userEvent.click(nextButton);
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName.textContent).toBe('Charmander');
+    userEvent.click(nextButton);
+    expect(pokemonName.textContent).toBe('Caterpie');
+    userEvent.click(nextButton);
+    expect(pokemonName.textContent).toBe('Ekans');
+  });
+
+  it('O primeiro deve ser mostrado ao clicar, se estiver no último Pokémon', () => {
+    renderWithRouter(<App />);
+    const nextButton = screen.getByRole('button', {
+      name: /Próximo pokémon/i,
+    });
+    userEvent.click(nextButton);
+    userEvent.click(nextButton);
+    userEvent.click(nextButton);
+    userEvent.click(nextButton);
+    userEvent.click(nextButton);
+    userEvent.click(nextButton);
+    userEvent.click(nextButton);
+    userEvent.click(nextButton);
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName.textContent).toBe('Dragonair');
+    userEvent.click(nextButton);
+    expect(pokemonName.textContent).toBe('Pikachu');
+  });
+
+  it('Se é mostrado apenas um Pokémon por vez', () => {
+    renderWithRouter(<App />);
+    const pokemonName = screen.getAllByTestId('pokemon-name');
+    expect(pokemonName.length).toBe(1);
+  });
+
+  it('Se existe apenas um botão de filtragem para cada tipo de Pokémon', () => {
+    renderWithRouter(<App />);
+    const CATEGORIES_COUNT = 7;
+    const categoriesButtons = screen.getAllByTestId('pokemon-type-button');
+    expect(categoriesButtons.length).toBe(CATEGORIES_COUNT);
+  });
+
+  it('Se deve circular somente pelos pokémons daquele tipo selecionado', () => {
+    renderWithRouter(<App />);
+    const nextButton = screen.getByRole('button', {
+      name: /Próximo pokémon/i,
+    });
+    const categoriesButtons = screen.getAllByTestId('pokemon-type-button');
+    expect(categoriesButtons[0].textContent).toBe('Electric');
+    userEvent.click(nextButton);
+    expect(categoriesButtons[1].textContent).toBe('Fire');
+  });
+
+  it('Se o botão All está sempre visível', () => {
+    renderWithRouter(<App />);
+    const allButton = screen.getByText('All');
+    expect(allButton.textContent).toBe('All');
+  });
+
+  it('se a Pokédex contém um botão para resetar o filtro', () => {
+    renderWithRouter(<App />);
+    const nextButton = screen.getByRole('button', {
+      name: /Próximo pokémon/i,
+    });
+    const reset = screen.getByText('All');
+    const categoriesButtons = screen.getAllByTestId('pokemon-type-button');
+    expect(categoriesButtons[0].textContent).toBe('Electric');
+    userEvent.click(nextButton);
+    expect(categoriesButtons[1].textContent).toBe('Fire');
+    userEvent.click(reset);
+    expect(categoriesButtons[0].textContent).toBe('Electric');
+  });
+});

09cbe49c6559654a12c2f5d767572d35af29e3a4
Dororops <leeurw@hotmail.com>
2021-08-23 19:09:37 -0300

    Adicionando Requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,74 @@
-test('', () => {});
+import React from 'react';
+// import { Router } from 'react-router';
+// import { BrowserRouter } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+// import { createMemoryHistory } from 'history';
+// import { MemoryRouter } from 'react-router';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './utils/renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <Pokemon.js />', () => {
+  test('Se é renderizado um card com as informações de determinado pokémon', () => {
+    renderWithRouter(<App />);
+
+    const pokemonName = screen.getByTestId('pokemon-name');
+    const pokemonType = screen.getByTestId('pokemon-type');
+    const pokemonWeight = screen.getByTestId('pokemon-weight');
+
+    expect(pokemonName).toBeInTheDocument();
+    expect(pokemonType).toBeInTheDocument();
+    expect(pokemonWeight).toBeInTheDocument();
+  });
+
+  test('Se o card do Pokémon contém um link de navegação para exibir detalhes', () => {
+    renderWithRouter(<App />);
+
+    const linkMoreDetails = screen.getByRole('link', {
+      name: 'More details',
+    });
+    expect(linkMoreDetails).toBeInTheDocument();
+  });
+
+  test('Se ao clicar no link é feito o redirecionamento para página de detalhes', () => {
+    renderWithRouter(<App />);
+
+    const linkMoreDetails = screen.getByRole('link', {
+      name: 'More details',
+    });
+    userEvent.click(linkMoreDetails);
+
+    const pokemonDetails = screen.getByRole('heading', {
+      level: 2,
+      name: 'Pikachu Details',
+    });
+    expect(pokemonDetails).toBeInTheDocument();
+  });
+
+  test('Se a URL exibida no navegador muda para /pokemon/<id>', () => {
+    const { history } = renderWithRouter(<App />);
+    const pikachuUrl = '/pokemons/25';
+
+    const linkMoreDetails = screen.getByRole('link', {
+      name: 'More details',
+    });
+    userEvent.click(linkMoreDetails);
+    expect(history.location.pathname).toBe(pikachuUrl);
+  });
+
+  test('Se existe um ícone de estrela nos Pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    const pikachuUrl = '/pokemons/25';
+    history.push(pikachuUrl);
+
+    const clickFavoriteButton = screen.getByText('Pokémon favoritado?');
+    userEvent.click(clickFavoriteButton);
+
+    const favoritedIcon = screen.getByAltText('Pikachu is marked as favorite');
+    expect(favoritedIcon).toBeInTheDocument();
+    expect(favoritedIcon).toHaveAttribute(
+      'src',
+      '/star-icon.svg',
+    );
+  });
+});

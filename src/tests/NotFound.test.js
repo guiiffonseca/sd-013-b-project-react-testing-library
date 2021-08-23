15bf36e1cc3140c9fa7edba67c450cd1ca48519d
Henrique Remedios <henriqueremedios@gmail.com>
2021-08-22 23:16:40 -0300

    Requisito 4 feito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Verifica o componente "NotFound.js"', () => {
+  test('Pagina contém uma tag "h2" com texto "Page Requested not found"', () => {
+    renderWithRouter(<NotFound />);
+    const h2TitleText = screen.getByRole('heading', { name: /Page requested no/i });
+    expect(h2TitleText).toBeInTheDocument();
+  });
+
+  test('Pagina renderiza a imagem selecionada', () => {
+    renderWithRouter(<NotFound />);
+    const text = /Pikachu crying because the page requested was not found/i;
+    const img = screen.getByAltText(text);
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

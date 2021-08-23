eee056ae8f13e2ad712cbbd57d116fca8279d429
Alef Sloan <Alef_sloan@hotmail.com>
2021-08-23 12:28:37 -0300

    Requisito 4 finalizado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import renderWithRouter from './utility/renderWithRouter';
+import App from '../App';
+
+test('Verify if the text "Page requested not found" is in the page', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/page-not-found');
+
+  const textWithRoleH2 = screen.getByRole('heading', {
+    level: 2,
+    name: /Page requested not found/,
+  });
+  expect(textWithRoleH2).toBeInTheDocument();
+});
+
+test('Verify if the img of crying pikachu is shown in the page', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/page-not-found');
+
+  const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  const imageOfPikachu = screen.getAllByRole('img')[1];
+  expect(imageOfPikachu).toHaveAttribute('src', URL);
+});

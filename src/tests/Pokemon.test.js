68dbd2c493efd76622f0f897f40b7b32f1a7c73d
Leonardo <leo_mc_inter@hotmail.com>
2021-08-22 22:18:23 -0300

    Requisito pokemon para 80% BORA

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import renderWithRouter from './router/renderWithRouter';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './router/renderWithRouter';
 import App from '../App';
+
+const POKEMON_LINK = '/pokemons/25';
 
 describe('Testa o componente Pokemon', () => {
   test('verifica se é renderizado um card com as informações do pokémon', () => {
@@ -20,10 +22,16 @@ describe('Testa o componente Pokemon', () => {
     expect(screen.getByRole('img')).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
   });
 
-  test('verifica se há um link com id do pokemon e se redireciona para a página correta',
+  test('verifica se há um link com id do pokemon',
+    () => {
+      renderWithRouter(<App />);
+      const moreDetailsLink = screen.getByText(/More details/);
+      expect(moreDetailsLink).toHaveAttribute('href', POKEMON_LINK);
+    });
+
+  test('verifica se o link redireciona para a página correta',
     () => {
       const { history } = renderWithRouter(<App />);
-      const POKEMON_LINK = '/pokemons/25';
       const moreDetailsLink = screen.getByText('More details');
       expect(moreDetailsLink).toHaveAttribute('href', POKEMON_LINK);
       userEvent.click(moreDetailsLink);

e25fb1fdf601f457d98e79c80129d5b4ecfbed33
Isaddora Bayma <isadora.bayma@gmail.com>
2021-08-23 15:40:24 -0300

    first test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from '../App';
+
+describe('Requisito 1 - App.js test', () => {
+  test('testando a renderização dos links da nav', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const home = screen.getByRole('link', {
+      name: /home/i,
+    });
+
+    expect(home).toBeInTheDocument();
+  });
+});

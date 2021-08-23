936a38ab0d99a53581ddd277c5c1501711fc3775
Mayu <mayusatori@gmail.com>
2021-08-22 23:18:35 -0300

    Requisito 4 resolvido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente NotFound', () => {
+  test('Teste se página contém um heading h2 com o texto certo', () => {
+    renderWithRouter(<NotFound />);
+    expect(screen.getByRole('heading', { level: 2 }).textContent)
+      .toBe('Page requested not found 😭');
+  });
+  test('Teste se página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    expect(screen.getByAltText(/crying/i).src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

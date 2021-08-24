82c744ec4308b0a567223f8408600d1b92b09407
btriz <00btrz@gmail.com>
2021-08-24 15:24:26 -0300

    :white_check_mark: requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+describe('Testa o componente NotFound.js', () => {
+  beforeEach(() => {
+    const customHistory = createMemoryHistory();
+
+    render(
+      <Router history={ customHistory }>
+        <App />
+      </Router>,
+    );
+
+    customHistory.push('/rota-nao-existente');
+  });
+
+  test('Deve exibir um heading com o texto /"Page requested not found 😭/"', () => {
+    const notFoundTitle = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/,
+    });
+
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+
+  test('Deve mostrar uma imagem de Pikachu desolado', () => {
+    const image = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

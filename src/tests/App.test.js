007f2089da04d758577cb7f43a06dd40c2fe7cc4
Bruno <brunophilippitrybe@gmail.com>
2021-08-23 23:15:18 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,13 +5,13 @@ import App from '../App';
 
 describe('tests for the App component', () => {
   it('Tests whether the Pokédex main page is rendered in the URL path /', () => {
-    const { screen.getByText } = renderWithRouter(<App />);
+    const { getByText } = renderWithRouter(<App />);
     expect(getByText('Encountered pokémons')).toBeInTheDocument();
   });
 
   it('test redirect with home link', () => {
-    const { screen.getByText, history } = renderWithRouter(<App />);
-    const linkHome = screen.getByText(/Home/i);
+    const { getByText, history } = renderWithRouter(<App />);
+    const linkHome = getByText(/Home/i);
     expect(linkHome).toBeInTheDocument();
 
     fireEvent.click(linkHome);
@@ -19,8 +19,8 @@ describe('tests for the App component', () => {
   });
 
   it('test redirect with about link', () => {
-    const { screen.getByText, history } = renderWithRouter(<App />);
-    const linkAbout = screen.getByText(/About/i);
+    const { getByText, history } = renderWithRouter(<App />);
+    const linkAbout = getByText(/About/i);
     expect(linkAbout).toBeInTheDocument();
 
     fireEvent.click(linkAbout);
@@ -28,8 +28,8 @@ describe('tests for the App component', () => {
   });
 
   it('test redirect with favorite link', () => {
-    const { screen.getByText, history } = renderWithRouter(<App />);
-    const linkFavorite = screen.getByText(/Favorite Pokémon/i);
+    const { getByText, history } = renderWithRouter(<App />);
+    const linkFavorite = getByText(/Favorite Pokémon/i);
     expect(linkFavorite).toBeInTheDocument();
 
     fireEvent.click(linkFavorite);

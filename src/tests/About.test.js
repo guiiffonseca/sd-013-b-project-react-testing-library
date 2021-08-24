cff273893647948f86aeb15b9e6d4e7e5a564c04
Genivaldo <cgenivaldo46@gmail.com>
2021-08-24 11:57:39 -0300

    Tstando de novo

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -13,3 +13,4 @@ describe('Testando o componente About', () => {
     expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
+// mudando

fbe3ed08b02816678df0b90f0c2b0c69bde46ea9
Heitor Santos <heitorg33@gmail.com>
2021-08-24 20:37:15 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Testando Componente About', () => {
+  test('Verifica se a página contém informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    expect(screen.getByText(/This application simulates/i)).toBeInTheDocument();
+  });
+
+  test('Verifica se contem dois h2 na aplicação', () => {
+    renderWithRouter(<About />);
+
+    const aboutText = screen.getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
+    });
+
+    expect(aboutText).toBeInTheDocument();
+  });
+
+  test('Verificado se o teste contém dois paragráfos', () => {
+    renderWithRouter(<About />);
+
+    expect(screen.getByText(/digital /i)).toBeInTheDocument();
+    expect(screen.getByText(/details/i)).toBeInTheDocument();
+  });
+
+  test('Verifica se contém imagem', () => {
+    renderWithRouter(<About />);
+
+    expect(screen.getByRole('img')).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});

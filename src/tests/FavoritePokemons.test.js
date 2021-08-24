a2e543b16561f9a2ee82d65588b2e11ab8ca9993
Raphael Rodrigues <raphael_rpc@outlook.com>
2021-08-24 19:23:58 -0300

    entrega do requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,27 +1,27 @@
-test('', () => {});
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import { FavoritePokemons } from '../components';
-// import userEvent from '@testing-library/user-event';
-// import { MemoryRouter } from 'react-router-dom';
-// import { Promise } from 'bluebird';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { FavoritePokemons } from '../components';
+import pokemons from '../data';
 
-// const pokemonMock = {"id":25,"name":"Pikachu","type":"Electric","averageWeight":{"value":"6.0","measurementUnit":"kg"},"image":"https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png","moreInfo":"https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)","foundAt":[{"location":"Kanto Viridian Forest","map":"https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png"},{"location":"Kanto Power Plant","map":"https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png"}],"summary":"This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat."}
-
-// describe('testando o componente FavoritePokemons', () => {
-//   test('renderiza o texto No favorite pokemon found', () => {
-//     render(<FavoritePokemons />);
-//     const favoritePText = screen.getByText(/No favorite pokemon found/i);
-//     expect(favoritePText).toBeInTheDocument();
-//   });
-//   test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
-//     // render(<FavoritePokemons />);
-//     // global.fetch = jest.fn(() => Promise.resolve({
-//     //   jason: () => Promise.resolve(pokemonMock);
-//     // }))
-//     // const imgStar = screen.getByRole('img');
-//     // expect(imgStar.src).toBe('http://localhost:3000/star-icon.svg');
-//     // // const details = screen.getByTestId(/More details/i);
-//     // // expect(details).toBeInTheDocument();
-//   });
-// });
+describe('testando o componente FavoritePokemons', () => {
+  test('renderiza o texto No favorite pokemon found, se nao houver favoritos', () => {
+    render(
+      // renderiza o FavoritePokemosn com o array pokemons vazio
+      <MemoryRouter>
+        <FavoritePokemons pokemons={ [] } />
+      </MemoryRouter>,
+    );
+    screen.getByText('No favorite pokemon found');
+  });
+  test('renderizar card favorito', () => {
+    render(
+      // renderiza o FavoritePokemosn ja na pag /favorites com array pokemons povuado;
+      <MemoryRouter>
+        <FavoritePokemons pokemons={ pokemons } />
+      </MemoryRouter>,
+    );
+    const namesPokemons = screen.getAllByTestId('pokemon-name');
+    expect(namesPokemons.length).toBe(pokemons.length);
+  });
+});

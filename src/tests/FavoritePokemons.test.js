3dcf6c978ac848675db4da00d7ad8c13132b5448
Camila Dornas <camiladornas@gmail.com>
2021-08-24 20:11:13 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa o arquivo FavoritePokemons.js', () => {
+  it('Testa a exibição da mensagem "No favorite pokemon found" se não há pokémons fav..',
+    () => {
+      renderWithRouter(<FavoritePokemons />);
+      expect(screen.getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+    });
+});

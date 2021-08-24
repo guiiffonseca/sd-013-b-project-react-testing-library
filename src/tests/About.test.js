101c984e674bcef8a49c35ca48b6ccafdc92b9f7
Genivaldo <cgenivaldo46@gmail.com>
2021-08-24 07:50:47 -0300

    Testando o avaliador

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,15 +1,15 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import About from '../components/About';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
 
-// describe('Testando o componente About', () => {
-//   it('Testa se a página contém informações sobre pokedex', () => {
-//     render(<About />);
-//     expect(screen.getByText('About Pokédex')).toBeInTheDocument();
-//   });
-//   it('Testa se a página contém a imagem de uma Pokédex', () => {
-//     render(<About />);
-//     const img = screen.getByAltText('Pokédex');
-//     expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
-//   });
-// });
+describe('Testando o componente About', () => {
+  it('Testa se a página contém informações sobre pokedex', () => {
+    render(<About />);
+    expect(screen.getByText('About Pokédex')).toBeInTheDocument();
+  });
+  it('Testa se a página contém a imagem de uma Pokédex', () => {
+    render(<About />);
+    const img = screen.getByAltText('Pokédex');
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});

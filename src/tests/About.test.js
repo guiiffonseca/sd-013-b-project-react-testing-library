4e3bda64d8bea681791461b318e53a9f062d2b90
Camila Dornas <camiladornas@gmail.com>
2021-08-23 22:29:18 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../components';
+
+describe('Testa o componente About.js', () => {
+  it('Testa se a página contém as informações sobre uma Pokédex.', () => {
+    renderWithRouter(<About />);
+    const infos = screen.getByRole('heading', { name: /About Pokédex/i });
+    expect(infos).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém a imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const img = screen.getByRole('img');
+    expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});

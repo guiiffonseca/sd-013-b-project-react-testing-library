aec7225a28e84b5c17b4bc3cfa9b765e0348395c
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-08-24 11:37:06 -0300

    Atualizando projeto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from '../routes/script';
 
 test('O componente <App /> é renderizado.', () => {
-
+  const { history } = renderWithRouter(<App />);
+  history.push('/');
+  const rendersHome = screen.getByRole('link', {
+    name: 'Home',
+  });
+  expect(rendersHome).toBeInTheDocument();
+  userEvent.click(rendersHome);
 });

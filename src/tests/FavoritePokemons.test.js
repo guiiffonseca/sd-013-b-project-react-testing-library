96d8cada87229f28005d60f735804dc2df54d48b
btriz <00btrz@gmail.com>
2021-08-23 23:52:00 -0300

    :white_check_mark: requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { FavoritePokemons } from '../components';
+import pokemons from '../data';
+
+describe('Testa o componente FavoritePokemons.js', () => {
+  test('Deve exibir \'No favorite pokemon found\' se não houverem favoritados', () => {
+    render(<FavoritePokemons />);
+
+    const message = screen.getByText('No favorite pokemon found');
+
+    expect(message).toBeInTheDocument();
+  });
+
+  test('Deve exibir os cards de todos os pokémons favoritados', () => {
+    render(
+      <MemoryRouter>
+        <FavoritePokemons pokemons={ pokemons } />
+      </MemoryRouter>,
+    );
+
+    const names = screen.getAllByTestId('pokemon-name');
+
+    pokemons.forEach((pokemon) => {
+      const cardName = names.find((name) => (
+        name.textContent === pokemon.name
+      ));
+
+      expect(cardName).toBeDefined();
+    });
+  });
+});

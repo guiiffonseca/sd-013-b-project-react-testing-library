4099ccfdd853701d00f1629adbdb80ff2ce1034b
Guilherme <guilhermerg1234@gmail.com>
2021-08-24 23:57:34 -0300

    Req-3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('Favorite test', () => {
+  test('A mensagegem "No favorite pokemon found" deve ser exibida na tela', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noFavorite = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
+
+  test('Deverá aparecer na tela, todos os pokemons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+
+    const average = screen.getAllByText(/Average weigh/i);
+
+    const qtdPokemons = 9;
+
+    expect(average).toHaveLength(qtdPokemons);
+  });
+});

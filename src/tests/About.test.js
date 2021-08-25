4f898a6e34a312483d2d5e297632790e4ec9ee37
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-08-25 16:09:42 -0300

    Arquivo atualizado

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../routes/router';
+
+import About from '../components/About';
+
+test('O link para a página Home funciona corretamente', () => {
+  const { history } = renderWithRouter(<About />);
+  history.push('/');
+  const rendersImg = screen.getByRole('img', {
+    name: /home/i,
+  });
+  expect(rendersImg).toBeInTheDocument();
+  // userEvent.click(rendersImg);
+});

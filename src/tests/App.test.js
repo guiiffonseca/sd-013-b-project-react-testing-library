989ef6a64b5d3d9ffb7df4fbc855c7b8564793a8
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-24 22:27:52 -0300

    Arquivo de testes App.test.js implementado corretamente.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,7 +4,7 @@ import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../routes/script';
 
-test('O componente <App /> é renderizado.', () => {
+test('O link para a página Home funciona corretamente', () => {
   const { history } = renderWithRouter(<App />);
   history.push('/');
   const rendersHome = screen.getByRole('link', {
@@ -12,4 +12,24 @@ test('O componente <App /> é renderizado.', () => {
   });
   expect(rendersHome).toBeInTheDocument();
   userEvent.click(rendersHome);
+});
+
+test('O link para a página About funciona corretamente', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/about');
+  const rendersAbout = screen.getByRole('link', {
+    name: 'About',
+  });
+  expect(rendersAbout).toBeInTheDocument();
+  userEvent.click(rendersAbout);
+});
+
+test('O link para a página Favorite Pokemons funciona corretamente', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/favorites');
+  const rendersFavorites = screen.getByRole('link', {
+    name: 'Favorite Pokémons',
+  });
+  expect(rendersFavorites).toBeInTheDocument();
+  userEvent.click(rendersFavorites);
 });

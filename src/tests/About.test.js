21672993be31def05f6d5510cc32b3118dc53577
bvottri <bruna.votre@gmail.com>
2021-08-24 22:04:34 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,7 +1,28 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import About from '../components/About';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+// import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
 
-// describe('Teste o componente About', () => {
-//   test('', () => {});
-// });
+describe('Teste o componente About', () => {
+  test('Teste se a página contém as informações sobre a Pokédex', () => {});
+  //   renderWithRouter(<About />);
+  //   const info = getByText
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    render(<About />);
+    const h2 = screen.getByText(/About Pokédex/);
+    expect(h2).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    render(<About />);
+    const p = screen.getAllByText(/pokémons/i);
+    expect(p).toHaveLength(2);
+  });
+
+  test('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
+    render(<About />);
+    const imgLink = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const img = screen.getByRole('img');
+    expect(img.src).toBe(imgLink);
+  });
+});
--- src/tests/renderWithRouter.js
+++ src/tests/renderWithRouter.js
@@ -3,15 +3,11 @@ import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
 
-const renderWithRouter = (componentToRender) => {
-  const customHistory = createMemoryHistory();
-  return {
-    ...render(
-      <Router history={ customHistory }>
-        {componentToRender}
-      </Router>,
-    ),
-    history: customHistory,
-  };
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={ history }>{component}</Router>), history,
+  });
 };
+
 export default renderWithRouter;

cadadeb9de028783f4fba8680bc5d5c215cc4d7a
Matheus Figueiredo <mathhsoares2@live.com>
2021-08-25 10:21:20 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente NotFound', () => {
+  it('Testa se existe um h2 com o texto "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+    const notFound = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/,
+    });
+
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Testa se a página mostra o gif apropriado', () => {
+    renderWithRouter(<NotFound />);
+    const gif = screen.getByAltText(/Pikachu crying/);
+
+    expect(gif.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

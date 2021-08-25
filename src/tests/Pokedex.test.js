973deac4bb7423bd34e183cb60cf2a34b969330f
Henrique Remedios <henriqueremedios@gmail.com>
2021-08-25 00:24:47 -0300

    Requisito 5 feito

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,55 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import data from '../data';
+import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Verifica o componente "Pokedex.js"', () => {
+  test('Verifica se contém a tag h2 com o texto "Encountered pokémons"', () => {
+    renderWithRouter(<App />);
+    const h2Title = screen.getByRole('heading', { name: /Encountered pokémons/i });
+    expect(h2Title).toBeInTheDocument();
+  });
+
+  test('Verifica se tem 1 pokemon por vez', () => {
+    renderWithRouter(<App />);
+    const verifyAmount = screen.getAllByText(/More Details/i);
+    expect(verifyAmount).toHaveLength(1);
+  });
+
+  test('Verifica o próximo pokémon ao ser clicado o botão', () => {
+    renderWithRouter(<App />);
+    const button = screen.getByRole('button', { name: /Próximo pokémon/i });
+    expect(button).toBeDefined();
+
+    userEvent.click(button);
+    const nextPokemon = screen.getByText(/Charmander/i);
+    expect(nextPokemon).toBeInTheDocument();
+  });
+
+  test('Verifica se o componente possui botões', () => {
+    renderWithRouter(<App />);
+    const buttons = screen.getByRole('button', { name: /Fire/i });
+    expect(buttons).toBeInTheDocument();
+    userEvent.click(buttons);
+
+    const typePokemons = [];
+    data.forEach((pokemon) => {
+      const findPokemon = typePokemons.find((poke) => poke === pokemon.type);
+      if (!findPokemon) {
+        typePokemons.push(pokemon.type);
+      }
+    });
+  });
+
+  test('Verifica se o botao All reseta o filtro', () => {
+    renderWithRouter(<App />);
+    const buttonAll = screen.getByRole('button', { name: /All/i });
+    expect(buttonAll).toBeInTheDocument();
+
+    userEvent.click(buttonAll);
+    const resetPokemon = screen.getByTestId('pokemon-type');
+    expect(resetPokemon).toHaveTextContent(/Electric/i);
+  });
+});

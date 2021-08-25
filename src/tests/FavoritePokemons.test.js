11ebe5d3b4f897ccc9fafe886c22e8799b8df7d5
Camila Dornas <camiladornas@gmail.com>
2021-08-24 21:14:40 -0300

    Corrige requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
+import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
 
@@ -9,4 +10,22 @@ describe('Testa o arquivo FavoritePokemons.js', () => {
       renderWithRouter(<FavoritePokemons />);
       expect(screen.getByText(/No favorite pokemon found/i)).toBeInTheDocument();
     });
+
+  it('Testa se é exibido todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<App />);
+    const details = screen.getByRole('link', { name: 'More details' });
+    expect(details).toBeInTheDocument();
+    fireEvent.click(details);
+
+    const checkFavorite = screen.getByRole('checkbox', { name: 'Pokémon favoritado?' });
+    expect(checkFavorite).toBeInTheDocument();
+    fireEvent.click(checkFavorite);
+
+    const favoritePokemons = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    fireEvent.click(favoritePokemons);
+    expect(favoritePokemons).toBeInTheDocument();
+
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName).toBeInTheDocument();
+  });
 });

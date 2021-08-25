8f9c41a82110fc8ece9bbbdc2039920af9beca50
Genivaldo <cgenivaldo46@gmail.com>
2021-08-25 08:41:40 -0300

    Requisitos 03-04

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+
+import { MemoryRouter } from 'react-router-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+import * as pokedexService from '../services/pokedexService';
+import App from '../App';
+
+describe('Testando o componenete FavoritePokémon', () => {
+  it('Testa se há pokémons favoritados no FavoritePokémon', () => {
+    pokedexService.readFavoritePokemonIds([]);
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const favorite = screen.getByText(/Favorite pokémons/i);
+
+    fireEvent.click(favorite);
+
+    const thereNoFavorite = screen.getByText(/No favorite pokemon found/i);
+    expect(thereNoFavorite).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Testando componete NoFound', () => {
+  it('Testa se há um heding h2 Page requested not found 😭', () => {
+    render(<NotFound />);
+    const theresNoText = screen.getByText(/Page requested not found/i);
+    expect(theresNoText).toBeInTheDocument();
+  });
+  it('Testa se há a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    render(<NotFound />);
+    const imgText = /Pikachu crying because the page requested was not found/;
+    const img = screen.getByAltText(imgText);
+    expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

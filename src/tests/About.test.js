da4d84d4ae0cebcbaaa1574c5ec8c2266fba539a
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-08-25 18:34:08 -0300

    Teste About passando no stryker

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -5,12 +5,18 @@ import renderWithRouter from '../routes/router';
 
 import About from '../components/About';
 
-test('O link para a página Home funciona corretamente', () => {
+test('O título da seção é renderizado com o texto "About Pokédex".', () => {
   const { history } = renderWithRouter(<About />);
-  history.push('/');
-  const rendersImg = screen.getByRole('img', {
-    name: /home/i,
+  const rendersTitle = screen.getByRole('heading', {
+    name: /about pokédex/i,
   });
-  expect(rendersImg).toBeInTheDocument();
-  // userEvent.click(rendersImg);
+  expect(rendersTitle).toBeInTheDocument();
+});
+
+test('A imagem da pokédex é renderizada.', () => {
+  const { history } = renderWithRouter(<About />);
+  const rendersImg = screen.getByAltText('Pokédex');
+  const dex = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/';
+  const image = 'Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  expect(rendersImg.src).toBe(`${dex}${image}`);
 });

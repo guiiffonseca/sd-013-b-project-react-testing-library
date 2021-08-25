45ef6d9b2241fbd5de5fb9a5b54396d5f0a4332d
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-08-25 15:00:04 -0300

    Rejects colocados no lugar de strings.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../routes/router';
+
 import App from '../App';
-import renderWithRouter from '../routes/script';
 
 test('O link para a página Home funciona corretamente', () => {
   const { history } = renderWithRouter(<App />);
   history.push('/');
   const rendersHome = screen.getByRole('link', {
-    name: 'Home',
+    name: /home/i,
   });
   expect(rendersHome).toBeInTheDocument();
   userEvent.click(rendersHome);
@@ -18,7 +19,7 @@ test('O link para a página About funciona corretamente', () => {
   const { history } = renderWithRouter(<App />);
   history.push('/about');
   const rendersAbout = screen.getByRole('link', {
-    name: 'About',
+    name: /about/i,
   });
   expect(rendersAbout).toBeInTheDocument();
   userEvent.click(rendersAbout);
@@ -28,7 +29,7 @@ test('O link para a página Favorite Pokemons funciona corretamente', () => {
   const { history } = renderWithRouter(<App />);
   history.push('/favorites');
   const rendersFavorites = screen.getByRole('link', {
-    name: 'Favorite Pokémons',
+    name: /favorite pokémons/i,
   });
   expect(rendersFavorites).toBeInTheDocument();
   userEvent.click(rendersFavorites);

865ccc510f99646037f11ecb2f869daa57e49904
Gustavo <gu.bressan@outlook.com>
2021-08-26 00:57:33 -0300

    Estou criando outro pr

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Test NotFound', () => {
+  test('', () => {
+    render(
+      <NotFound />,
+    );
+    const heading = screen.getByRole('heading');
+
+    expect(heading.textContent).toBe('Page requested not found 😭');
+  });
+
+  test(' Text image', () => {
+    render(
+      <NotFound />,
+    );
+
+    const image = screen.getAllByRole('img')[1];
+
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Text implementation Pokemon', () => {
+  test('test card Pokemon', () => {
+    renderWithRouter(<App />);
+    const pokemon = screen.getByTestId('pokemon-name');
+    expect(pokemon).toHaveTextContent('Pikachu');
+    const type = screen.getByTestId('pokemon-type');
+    expect(type).toHaveTextContent('Electric');
+    const weight = screen.getByTestId('pokemon-weight');
+    expect(weight).toHaveTextContent('Average weight: 6.0 kg');
+    const image = screen.getByRole('img');
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    expect(image.alt).toBe('Pikachu sprite');
+  });
+
+  test('Text Pokemon details', () => {
+    renderWithRouter(<App />);
+    const link = screen.getByRole('link', { name: /more details/i });
+    expect(link).toBeInTheDocument();
+    userEvent.click(link);
+    const favorite = screen.getByRole('checkbox', { name: /Pokémon favoritado?/i });
+    userEvent.click(favorite);
+    expect(favorite).toBeChecked();
+    const icon = screen.getByAltText('Pikachu is marked as favorite');
+    expect(icon.src).toContain('star-icon.svg');
+  });
+});

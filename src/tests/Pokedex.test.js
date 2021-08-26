4e62705e968ec67534a37bdf2bea516f72a452ed
VictorFrancozi <victor@victor-pc.victor>
2021-08-26 02:36:18 -0300

    Pokedex fix

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,53 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+import App from '../App';
+
+describe('Testa a pagina de pokedex', () => {
+  it('contem um heading', () => {
+    renderWithRouter(<App />);
+    const heading = screen.getByRole('heading', { name: 'Encountered pokémons' });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('testa o botão de próximo pokémon', () => {
+    renderWithRouter(<App />);
+    const nextButton = screen.getByRole('button', { name: 'Próximo pokémon' });
+    const pokeName = screen.getByTestId('pokemon-name');
+    const type = screen.getByTestId('pokemon-type');
+
+    expect(nextButton).toBeInTheDocument();
+
+    fireEvent.click(nextButton);
+
+    expect(pokeName).toBeInTheDocument();
+    expect(type).toBeInTheDocument();
+  });
+
+  it('Testa os botoes de filtrar pelo tipo de pokemon', () => {
+    renderWithRouter(<App />);
+    const allButton = screen.getByRole('button', { name: 'All' });
+    const fireButton = screen.getByRole('button', { name: 'Fire' });
+    const pokeName = screen.getByTestId('pokemon-name');
+    const type = screen.getByTestId('pokemon-type');
+
+    fireEvent.click(allButton);
+
+    expect(pokeName).toHaveTextContent('Pikachu');
+    expect(type).toHaveTextContent('Electric');
+    expect(screen.getByAltText('Pikachu sprite')).toBeInTheDocument();
+
+    fireEvent.click(fireButton);
+
+    expect(pokeName).toHaveTextContent('Charmander');
+    expect(type).toHaveTextContent('Fire');
+    expect(screen.getAllByAltText('Charmander sprite'));
+  });
+
+  it('Verifica se a página contém todos botões de filtro', () => {
+    renderWithRouter(<App />);
+    const typeButtons = screen.getAllByTestId('pokemon-type-button');
+    const buttonAmount = 7;
+    expect(typeButtons.length).toBe(buttonAmount);
+  });
+});

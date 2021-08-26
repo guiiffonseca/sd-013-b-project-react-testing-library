014a2b3aa179cc41ac564bd9f0eae8799d34c9dd
Gustavo <gu.bressan@outlook.com>
2021-08-26 00:17:05 -0300

    depois de muitas dificuldades, incluindo formatar o pc, requisito 7 e fim

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,68 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+function renderWithRouter(componentRender) {
+  const history = createMemoryHistory();
+  return {
+    ...render(
+      <Router history={ history }>
+        {componentRender}
+      </Router>,
+    ),
+    history: history,
+  };
+}
+
+describe('Test PokemonDetails', () => {
+  it('pokemons details', () => {
+    renderWithRouter(<App />);
+    const details = screen.getByRole('link', { name: /More Details/i });
+    userEvent.click(details);
+    const componentDetails = screen.getByRole('heading', {
+      level: 2,
+      name: /Pikachu Detail/i,
+    });
+    expect(componentDetails).toBeInTheDocument();
+  });
+
+  it('pokemon map', () => {
+
+    renderWithRouter(<App />);
+
+    const details = screen.getByRole('link', { name: /More Details/i });
+    userEvent.click(details);
+    const imgem = screen.getAllByRole('img', { name: /Pikachu location/i })[0];
+    expect(imgem.src).toBe('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(imgem.alt).toBe('Pikachu location');
+    const location = screen.getByRole('heading', {
+      level: 2,
+      name: /Game Locations/i,
+    });
+    expect(location).toBeInTheDocument();
+
+    const locationGame = screen.getByRole('heading', {
+      level: 2,
+      name: 'Game Locations of Pikachu',
+    });
+    expect(locationGame).toBeInTheDocument();
+    const sumary = screen.getByRole('heading', {
+      level: 2,
+      name: /Summary/i,
+    });
+    expect(sumary).toBeInTheDocument();
+    const sumaryP = screen.getByText(/This intelligent Pokémon/i);
+    expect(sumaryP).toBeInTheDocument();
+  });
+
+  it('favorite pokemon', () => {
+    renderWithRouter(<App />);
+    const details = screen.getByRole('link', { name: /More Details/i });
+    userEvent.click(details);
+    const favorite = screen.getByRole('checkbox', { name: /Pokémon favoritado?/i });
+    expect(favorite).toBeInTheDocument();
+  });
+});

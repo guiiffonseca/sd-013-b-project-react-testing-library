f935d3b796ba6f78f9df2ed94a9e22c7b9498dd8
Caio-Veloso <caio.veloso@gmail.com>
2021-08-26 07:30:33 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Requisito 3 - Tenstando o componente <FavoritePokemons />', () => {
+  beforeEach(() => {
+    render(<FavoritePokemons />);
+  });
+
+  it('Testa quando não há Pokémons favoritados', () => {
+    const noFavoritePkm = screen.getByText('No favorite pokemon found');
+    expect(noFavoritePkm).toBeDefined();
+  });
+});

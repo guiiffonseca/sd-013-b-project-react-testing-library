a976a01ccec2230014ba784d1bbc6cba42f770ed
Arthur Palomo <arthurpalomo@DSK-PCSSD0001.localdomain>
2021-08-26 10:17:17 -0300

    Requisito 2 correções $ Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
 
 describe('Test do componente <FavoritePokemons />', () => {
   test('Exibe o texto "No favorite pokemon found" se não houver nenhum salvo', () => {
     render(<FavoritePokemons />);
     const notFound = screen.getByText('No favorite pokemon found');
     expect(notFound).toBeInTheDocument();
+  });
+
+  test('Todos os cards de Pokémon são exibidos', () => {
+    renderWithRouter(<App />);
+    const pokeDetails = screen.getAByRole('link', {
+      name: /more details/i,
+    });
+    userEvent.click(pokeDetails);
+
+    const favoriteCheck = screen.getByRole('checkbox');
+    expect(favoriteCheck).toBeInTheDocument();
+    userEvent.click(favoriteCheck);
+
+    const favLink = screen.getByRole('link', {
+      name: /Favorite Pokémons/i,
+    });
+    userEvent.click(favLink);
+    const pokemon = screen.getByText(/Pikachu/i);
+    expect(pokemon).toBeInTheDocument();
   });
 });

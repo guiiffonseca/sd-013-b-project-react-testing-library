3f97743569c9d9cd6a705c8f4e5a736795735d25
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-27 20:55:30 -0300

    Erros de lint em About.test corrigidos

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../routes/router';
 
 import About from '../components/About';
 
 test('O título da seção é renderizado com o texto "About Pokédex".', () => {
-  const { history } = renderWithRouter(<About />);
+  renderWithRouter(<About />);
   const rendersTitle = screen.getByRole('heading', {
     level: 2,
     name: /about pokédex/i,
@@ -15,7 +14,7 @@ test('O título da seção é renderizado com o texto "About Pokédex".', () => 
 });
 
 test('A imagem da pokédex é renderizada.', () => {
-  const { history } = renderWithRouter(<About />);
+  renderWithRouter(<About />);
   const rendersImg = screen.getByAltText('Pokédex');
   const dex = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/';
   const image = 'Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';

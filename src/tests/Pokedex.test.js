a63c393255300b4e4527f243c302d6ed09448309
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-27 21:07:05 -0300

    Erros de lint em Pokedex.test corrigidos

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../routes/router';
 import pokemons from '../data';
 
@@ -8,7 +7,7 @@ import Pokedex from '../components/Pokedex';
 import App from '../App';
 
 test('A página contém um heading h2 com o texto "Encountered pokémons".', () => {
-  const { history } = renderWithRouter(<Pokedex
+  renderWithRouter(<Pokedex
     pokemons={ pokemons }
     isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
   />);
@@ -20,7 +19,7 @@ test('A página contém um heading h2 com o texto "Encountered pokémons".', () 
 });
 
 test('A página contém um botão para renderizar o próximo pokémon.', () => {
-  const { history } = renderWithRouter(<Pokedex
+  renderWithRouter(<Pokedex
     pokemons={ pokemons }
     isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
   />);

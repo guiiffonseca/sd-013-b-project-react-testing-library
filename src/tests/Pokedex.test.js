da452e9f1f9e6e86838729e86d4c2f3ccc23d6b8
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-27 23:21:25 -0300

    Pokedex.test quase completo.

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -6,24 +6,71 @@ import pokemons from '../data';
 import Pokedex from '../components/Pokedex';
 import App from '../App';
 
-test('A página contém um heading h2 com o texto "Encountered pokémons".', () => {
-  renderWithRouter(<Pokedex
-    pokemons={ pokemons }
-    isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
-  />);
-  const rendersTitle = screen.getByRole('heading', {
-    name: /encountered pokémons/i,
-    level: 2,
+describe('O título da seção Pokédex deve ser renderizado.', () => {
+  test('A página contém um heading h2 com o texto "Encountered pokémons".', () => {
+    renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+    />);
+    const rendersTitle = screen.getByRole('heading', {
+      name: /encountered pokémons/i,
+      level: 2,
+    });
+    expect(rendersTitle).toBeInTheDocument();
   });
-  expect(rendersTitle).toBeInTheDocument();
 });
 
-test('A página contém um botão para renderizar o próximo pokémon.', () => {
-  renderWithRouter(<Pokedex
-    pokemons={ pokemons }
-    isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
-  />);
-  const rendersButton = screen.getByTestId('next-pokemon');
-  const renderedButtonText = rendersButton.innerHTML;
-  expect(renderedButtonText).toBe('Próximo pokémon');
+describe('A página contém um botão para renderizar o próximo pokémon.', () => {
+  test('Um botão de trocar o pokémon é renderizado na base da página.', () => {
+    renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+    />);
+    const rendersButton = screen.getByTestId('next-pokemon');
+    expect(rendersButton).toBeInTheDocument();
+  });
+
+  test('O botão apresenta uma legenda "Próximo pokémon".', () => {
+    renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+    />);
+    const rendersButton = screen.getByTestId('next-pokemon');
+    const renderedButtonText = rendersButton.innerHTML;
+    expect(renderedButtonText).toBe('Próximo pokémon');
+  });
+});
+
+const TYPE_AMOUNT = 7;
+
+describe('A página contém vários botões para selecionar por tipo.', () => {
+  test('Existe um botão para cada tipo de pokémon.', () => {
+    renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+    />);
+    const typeButton = screen.getAllByTestId('pokemon-type-button');
+    expect(typeButton[0]).toBeInTheDocument();
+    expect(typeButton.length).toBe(TYPE_AMOUNT);
+  });
+
+  test('O botão deve filtrar a lista de pokémons disponíveis por tipo.', () => {
+    renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+    />);
+    const typeButton = screen.getAllByTestId('pokemon-type-button');
+    expect(typeButton[1].innerHTML).toBe('Fire');
+    expect(typeButton[4].innerHTML).toBe('Psychic');
+    expect(typeButton[6].innerHTML).toBe('Dragon');
+  });
+
+  test('O botão "All" torna todos os pokémons disponíveis novamente.', () => {
+    renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+    />);
+    const allButton = screen.getByText('All');
+    expect(allButton.innerHTML).toBe('All');
+  });
 });

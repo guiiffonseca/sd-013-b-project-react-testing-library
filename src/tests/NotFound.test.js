144bfa65f74a0da1a75d5186787603cd8cff61cb
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-27 21:03:51 -0300

    Erros de lint em NotFound.test corrigidos

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../routes/router';
 
 import NotFound from '../components/NotFound';
 
 test('A seção apresenta os dizeres "Page requested not found 😭".', () => {
-  const { history } = renderWithRouter(<NotFound />);
+  renderWithRouter(<NotFound />);
   const rendersNotFound = screen.getByRole('heading').innerHTML;
-  const renderedResult = rendersNotFound;
-  const headingText = "Page requested not found";
-  const headingEmoji = "<span role=\"img\" aria-label=\"Crying emoji\"> 😭</span>"
-  expect(renderedResult).toBe(`${headingText}${headingEmoji}`);
+  const headingText = 'Page requested not found';
+  const headingEmoji = '<span role="img" aria-label="Crying emoji"> 😭</span>';
+  expect(rendersNotFound).toBe(`${headingText}${headingEmoji}`);
 });
 
 test('Pikachu está triste 😢.', () => {
-  const { history } = renderWithRouter(<NotFound />);
+  renderWithRouter(<NotFound />);
   const rendersImg = screen.getAllByRole('img')[1];
   const cryingPikachu = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
   expect(rendersImg.src).toBe(`${cryingPikachu}`);
 });
-  
-  

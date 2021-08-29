944fb21c3b6becd7fecfdeb18d51f55731bd60e5
Mariana Garcia <maridgarcia045@gmail.com>
2021-08-28 21:52:55 -0300

    Corrige requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -5,15 +5,13 @@ import About from '../components/About';
 
 describe('Testando o componente About', () => {
   test('Se a página contém informações sobre a Pokédex', () => {
-    const { history } = renderWithRouter(<About />);
-    history.push('/about');
+    renderWithRouter(<About />);
     const aboutPokedex = screen.getByText(/About Pokédex/i);
 
     expect(aboutPokedex).toBeInTheDocument();
   });
   test('Se a página possui um h2 com o texto About Pokédex', () => {
-    const { history } = renderWithRouter(<About />);
-    history.push('/about');
+    renderWithRouter(<About />);
     const aboutPokedex = screen.getByRole('heading', {
       level: 2,
       name: /About Pokédex/i,
@@ -21,14 +19,12 @@ describe('Testando o componente About', () => {
     expect(aboutPokedex).toBeInTheDocument();
   });
   test('Se possui dois parágrafos sobre a Pokédex', () => {
-    const { history } = renderWithRouter(<About />);
-    history.push('/about');
+    renderWithRouter(<About />);
     const paragraph = screen.getAllByText(/Pokémons/i);
     expect(paragraph.length).toBe(2);
   });
   test('Se a página possui imagem da Pokédex', () => {
-    const { history } = renderWithRouter(<About />);
-    history.push('/about');
+    renderWithRouter(<About />);
     const img = screen.getByAltText(/Pokédex/i);
     expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });

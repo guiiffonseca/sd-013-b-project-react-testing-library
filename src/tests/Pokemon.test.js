2b11e6a245e6400bf7de44186b5ba5a36b466d9c
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-09-04 07:15:38 -0300

    Teste do componente Pokemon implementados

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -13,8 +13,8 @@ const isPokemonFavoriteById = App.setIsPokemonFavoriteById();
 describe('É renderizado um card com as informações de determinado pokémon.', () => {
   test('O nome correto do Pokémon deve ser mostrado na tela.', () => {
     renderWithRouter(<Pokemon
-      pokemon={pokemons[3]}
-      isFavorite={isPokemonFavoriteById[pokemons[3].id]}
+      pokemon={ pokemons[3] }
+      isFavorite={ isPokemonFavoriteById[pokemons[3].id] }
     />);
     const renderedPokemon = screen.getByTestId('pokemon-name', {
       name: /ekans/i,
@@ -23,10 +23,10 @@ describe('É renderizado um card com as informações de determinado pokémon.',
     expect(renderedPokemon.innerHTML).toBe('Ekans');
   });
 
-  test('O tipo correto do Pokémon deve ser mostrado na tela.',() => {
+  test('O tipo correto do Pokémon deve ser mostrado na tela.', () => {
     renderWithRouter(<Pokemon
-      pokemon={pokemons[3]}
-      isFavorite={isPokemonFavoriteById[pokemons[3].id]}
+      pokemon={ pokemons[3] }
+      isFavorite={ isPokemonFavoriteById[pokemons[3].id] }
     />);
     const renderedType = screen.getByTestId('pokemon-type', {
       type: /poison/i,
@@ -35,10 +35,10 @@ describe('É renderizado um card com as informações de determinado pokémon.',
     expect(renderedType.innerHTML).toBe('Poison');
   });
 
-  test('O peso médio do pokémon deve ser exibido.',() => {
+  test('O peso médio do pokémon deve ser exibido.', () => {
     renderWithRouter(<Pokemon
-      pokemon={pokemons[3]}
-      isFavorite={isPokemonFavoriteById[pokemons[3].id]}
+      pokemon={ pokemons[3] }
+      isFavorite={ isPokemonFavoriteById[pokemons[3].id] }
     />);
     const renderedWeight = screen.getByTestId('pokemon-weight');
     const { value: weight, measurementUnit: unit } = pokemons[3].averageWeight;
@@ -48,10 +48,10 @@ describe('É renderizado um card com as informações de determinado pokémon.',
     expect(renderedWeight.innerHTML).toBe(`Average weight: ${weight} ${unit}`);
   });
 
-  test('A imagem do Pokémon deve ser exibida na tela.',() => {
+  test('A imagem do Pokémon deve ser exibida na tela.', () => {
     renderWithRouter(<Pokemon
-      pokemon={pokemons[3]}
-      isFavorite={isPokemonFavoriteById[pokemons[3].id]}
+      pokemon={ pokemons[3] }
+      isFavorite={ isPokemonFavoriteById[pokemons[3].id] }
     />);
     const renderedImage = screen.getByRole('img');
     expect(renderedImage).toBeInTheDocument();
@@ -60,14 +60,14 @@ describe('É renderizado um card com as informações de determinado pokémon.',
   });
 });
 
-describe('O card do Pokémon indicado na Pokédex contém um link para exibir detalhes.', () => {
-  test('O link deve possuir a URL /pokemons/<id>, onde <id> é o id do Pokémon exibido.', () => {
-/*     const { history } = renderWithRouter(<Pokemon
+describe('O card do Pokémon indicado na Pokédex contém um link para detalhes.', () => {
+  test('O link deve possuir a URL /pokemons/<id>.', () => {
+    /* const { history } = renderWithRouter(<Pokemon
       pokemon={pokemons[3]}
       isFavorite={isPokemonFavoriteById[pokemons[3].id]}
     />);
     history.push('/pokemons/23'); */
-    const { history } = renderWithRouter(<Pokedex
+    renderWithRouter(<Pokedex
       pokemons={ pokemons }
       isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
     />);
@@ -75,10 +75,44 @@ describe('O card do Pokémon indicado na Pokédex contém um link para exibir de
       key: /poison/i,
     })[4];
     const detailsLink = screen.getByText('More details');
-    expect(detailsLink.pathname).toBe("/pokemons/25");
+    expect(detailsLink).toBeInTheDocument();
+    expect(detailsLink.pathname).toBe('/pokemons/25');
     userEvent.click(poisonButton);
-    expect(detailsLink.pathname).toBe("/pokemons/23");
-    // history.push('/pokemons/23'); 
-    expect(window.location.pathname).toBe('I was expecting to see something');
+    expect(detailsLink.pathname).toBe('/pokemons/23');
+  });
+});
+
+describe('Existe um ícone de estrela nos pokémons favoritados.', () => {
+  test('O ícone aparece na tela quando o pokémon é favorito.', () => {
+    renderWithRouter(<Pokemon
+      pokemon={ pokemons[3] }
+      isFavorite
+    />);
+    const favoriteIcon = screen.getAllByRole('img')[1];
+    expect(favoriteIcon).toBeInTheDocument();
+  });
+
+  const description1 = 'A imagem deve ter o atributo alt';
+  const description2 = 'igual a <pokemon> is marked as favorite.';
+
+  test(`${description1} ${description2}`, () => {
+    renderWithRouter(<Pokemon
+      pokemon={ pokemons[3] }
+      isFavorite
+    />);
+    const favoriteIcon = screen.getAllByRole('img')[1];
+    expect(favoriteIcon.alt).toBe('Ekans is marked as favorite');
+  });
+
+  const description3 = 'O ícone deve ser uma imagem com o atributo';
+  const description4 = '"src" contendo o caminho /star-icon.svg';
+
+  test(`${description3} ${description4}`, () => {
+    renderWithRouter(<Pokemon
+      pokemon={ pokemons[3] }
+      isFavorite
+    />);
+    const favoriteIcon = screen.getAllByRole('img')[1];
+    expect(favoriteIcon.src).toBe('http://localhost/star-icon.svg');
   });
 });

75547df332f80d9b8042ee0dab96528997548d6e
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-09-04 08:15:59 -0300

    PokemonDetails.test 40% concluído.

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,23 +1,69 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../routes/router';
 import pokemons from '../data';
 
 import PokemonDetails from '../components/PokemonDetails';
+import Pokemon from '../components/Pokemon';
 import App from '../App';
-import { Pokedex } from '../components';
 
-test('A página contém um heading h2 com o texto "Encountered pokémons".', () => {
-  console.log(<Pokedex />);
-  const { history } = renderWithRouter(<Pokedex
-    pokemons={ pokemons }
-    isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
-  />);
-  const rendersTitle = screen.getByRole('heading', {
-    name: /encountered pokémons/i,
-    level: 2,
+const match = {
+  isExact: true,
+  path: '/pokemons/:id',
+  url: '/pokemons/23',
+  params: {
+    id: '23',
+  },
+};
+
+describe('As informações do Pokémon são mostradas na tela', () => {
+  test('A página deve conter um texto <name> Details.', () => {
+    renderWithRouter(<PokemonDetails
+      isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+      match={ match }
+      pokemons={ pokemons }
+      onUpdateFavoritePokemons={ () => ('zero') }
+    />);
+    const detailsTitle = screen.getAllByRole('heading', {
+      level: 2,
+    })[0];
+    expect(detailsTitle.innerHTML).toBe('Ekans Details');
   });
-  console.log(<Pokedex />);
-  expect(rendersTitle).toBeInTheDocument();
+
+  test('Não deve existir o link de navegação para os detalhes do Pokémon.', () => {
+    renderWithRouter(<Pokemon
+      pokemon={ pokemons[3] }
+      isFavorite
+      showDetailsLink={ false }
+    />);
+    const detailsLink = screen.queryByText('More details');
+    expect(detailsLink).not.toBeInTheDocument();
+  });
+
+  test('A seção de detalhes deve conter um h2 com o texto Summary.', () => {
+    renderWithRouter(<PokemonDetails
+      isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+      match={ match }
+      pokemons={ pokemons }
+      onUpdateFavoritePokemons={ () => ('zero') }
+    />);
+    const detailsSummary = screen.getAllByRole('heading', {
+      level: 2,
+    })[1];
+    expect(detailsSummary.innerHTML).toBe('Summary');
+  });
+
+  test('A seção de detalhes deve conter um parágrafo com o resumo do Pokémon.', () => {
+    renderWithRouter(<PokemonDetails
+      isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+      match={ match }
+      pokemons={ pokemons }
+      onUpdateFavoritePokemons={ () => ('zero') }
+    />);
+    const text1 = 'It can freely detach its jaw';
+    const text2 = 'to swallow large prey whole.';
+    const text3 = 'It can become too heavy to move, however.';
+    const text = screen.getByText(`${text1} ${text2} ${text3}`);
+    expect(text.innerHTML).toContain('It can freely detach its jaw');
+  });
 });

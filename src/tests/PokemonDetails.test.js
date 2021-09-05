72abe4788e69aa164eddd1ed83dcc35a18e63082
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-09-04 21:23:23 -0300

    Teste PokemonDetails implementado

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -16,6 +16,15 @@ const match = {
   },
 };
 
+const match2 = {
+  isExact: true,
+  path: '/pokemons/:id',
+  url: '/pokemons/25',
+  params: {
+    id: '25',
+  },
+};
+
 describe('As informações do Pokémon são mostradas na tela', () => {
   test('A página deve conter um texto <name> Details.', () => {
     renderWithRouter(<PokemonDetails
@@ -65,5 +74,70 @@ describe('As informações do Pokémon são mostradas na tela', () => {
     const text3 = 'It can become too heavy to move, however.';
     const text = screen.getByText(`${text1} ${text2} ${text3}`);
     expect(text.innerHTML).toContain('It can freely detach its jaw');
+  });
+});
+
+describe('Existe uma seção com os mapas contendo as localizações do pokémon', () => {
+  test('Existe um h2 com o texto Game Locations of <name>', () => {
+    renderWithRouter(<PokemonDetails
+      isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+      match={ match }
+      pokemons={ pokemons }
+      onUpdateFavoritePokemons={ () => ('zero') }
+    />);
+    const locationTitle = screen.getAllByRole('heading', {
+      level: 2,
+    })[2];
+    expect(locationTitle).toBeInTheDocument();
+    expect(locationTitle.innerHTML).toBe('Game Locations of Ekans');
+  });
+
+  test('Todas as localizações do Pokémon devem ser mostradas', () => {
+    renderWithRouter(<PokemonDetails
+      isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+      match={ match2 }
+      pokemons={ pokemons }
+      onUpdateFavoritePokemons={ () => ('zero') }
+    />);
+    const locationList = screen.getAllByAltText('Pikachu location');
+    const location0 = locationList[0];
+    const location1 = locationList[1];
+    expect(location0).toBeInTheDocument();
+    expect(location1).toBeInTheDocument();
+    expect(location1.alt).toBe('Pikachu location');
+  });
+
+  const description1 = 'Devem ser exibidos, o nome da localização';
+  const description2 = 'e uma imagem do mapa em cada localização';
+
+  test(`${description1} ${description2}`, () => {
+    renderWithRouter(<PokemonDetails
+      isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+      match={ match2 }
+      pokemons={ pokemons }
+      onUpdateFavoritePokemons={ () => ('zero') }
+    />);
+    const bulbAddress = 'https://cdn2.bulbagarden.net/upload';
+    const location1 = screen.getByText(/kanto viridian forest/i);
+    const location2 = screen.getByText(/kanto power plant/i);
+    const image1 = screen.getAllByRole('img')[1];
+    const image2 = screen.getAllByRole('img')[2];
+    expect(image1.src).toBe(`${bulbAddress}/0/08/Kanto_Route_2_Map.png`);
+    expect(image2.src).toBe(`${bulbAddress}/b/bd/Kanto_Celadon_City_Map.png`);
+    expect(location1.innerHTML).toContain('Kanto Viridian Forest');
+    expect(location2.innerHTML).toContain('Kanto Power Plant');
+  });
+});
+
+describe('O pokémon pode ser favoritado na página de detalhes.', () => {
+  test('A página deve exibir um checkbox que permite favoritar o Pokémon', () => {
+    renderWithRouter(<PokemonDetails
+      isPokemonFavoriteById={ App.setIsPokemonFavoriteById() }
+      match={ match }
+      pokemons={ pokemons }
+      onUpdateFavoritePokemons={ () => ('zero') }
+    />);
+    const favoritedCheck = screen.getByLabelText('Pokémon favoritado?');
+    expect(favoritedCheck).toBeInTheDocument();
   });
 });

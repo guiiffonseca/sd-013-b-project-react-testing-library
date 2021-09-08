7d0b54f162e399bd115bee883df3a5f9e5fbe83f
Bernardo Raffide <bernardoraffide@pop-os.localdomain>
2021-09-08 17:08:46 -0300

    requisito 3 concluido

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+import { renderWithRouter } from '../components/utils/renderWithRouter';
+
+describe('testes do componente FavoritePokemons', () => {
+  test('testa se aparece a mensagem "No favorite pokemon found"', () => {
+    const { history } = renderWithRouter(<FavoritePokemons />);
+    history.push('/favorites');
+    const favoritePokemons = screen.getByText(/no favorite pokemon found/i);
+    expect(favoritePokemons).toBeInTheDocument();
+  });
+  test('testa se renderiza os pokemons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/');
+    const moreDetails = screen.getByText(/more details/i);
+    userEvent.click(moreDetails);
+    const checkbox = screen.getByRole('checkbox');
+    userEvent.click(checkbox);
+    const favoritePokemonsLink = screen.getByRole('link', {
+      name: /favorite Pokémon/i,
+    });
+    userEvent.click(favoritePokemonsLink);
+    const pokemonName = screen.getByText(/pikachu/i);
+    expect(pokemonName).toBeInTheDocument();
+  });
+});

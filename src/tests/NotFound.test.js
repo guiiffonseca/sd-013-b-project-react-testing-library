9fbdc5eafbe2b7453d964ede4d8fa9d6c2fda7f1
Bernardo Raffide <bernardoraffide@pop-os.localdomain>
2021-09-08 17:25:58 -0300

    requisito 4 concluido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import { renderWithRouter } from '../components/utils/renderWithRouter';
+
+describe('testes do componente "NotFound"', () => {
+  test('testa se a page contem um h2 com certo texto', () => {
+    const { history } = renderWithRouter(<NotFound />);
+    history.push('/shuahsuahs');
+    const heading = screen.getByText(/page requested not found/i);
+    expect(heading).toBeInTheDocument();
+    const imagem = screen.getAllByRole('img')[1];
+    expect(imagem).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

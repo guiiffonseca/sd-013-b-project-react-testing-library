932013063403c112bd5f3df819c831134c28e76f
Raphael Rodrigues <raphael_rpc@outlook.com>
2021-09-09 16:15:01 -0300

    entregado do requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,45 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('test Card Pokemons', () => {
+  test('test 1', () => {
+    renderWithRouter(<App />);
+
+    const btnElectric = screen.getByRole('button', {
+      name: /Electric/i,
+    });
+    userEvent.click(btnElectric);
+    const namePokemon = screen.getByText(/Pikachu/i);
+    expect(namePokemon).toBeInTheDocument();
+    const typePokemon = screen.getByTestId('pokemon-type');
+    expect(typePokemon.innerHTML).toBe('Electric');
+    const average = screen.getByText(/Average weight/i);
+    expect(average).toBeInTheDocument();
+    const valueWeight = screen.getByText(/6.0/i);
+    expect(valueWeight).toBeInTheDocument();
+    const imgPokemon = screen.getByRole('img');
+    expect(imgPokemon).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    expect(imgPokemon).toHaveAttribute('alt', 'Pikachu sprite');
+  });
+  test('Verifica se contem o more dethils ', () => {
+    const { history } = renderWithRouter(<App />);
+    const btnDetails = screen.getByRole('link', {
+      name: /More details/i,
+    });
+    expect(btnDetails).toBeInTheDocument();
+    userEvent.click(btnDetails);
+    const checkboxFavorite = screen.getByRole('checkbox');
+    expect(checkboxFavorite).toBeInTheDocument();
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+    userEvent.click(checkboxFavorite);
+    const imgStar = screen.getByAltText('Pikachu is marked as favorite');
+    expect(imgStar).toBeInTheDocument();
+    expect(imgStar).toHaveAttribute('src', '/star-icon.svg');
+  });
+});
+
+// test('', () => {});

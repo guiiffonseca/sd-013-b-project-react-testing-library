137566e8a5c3f94e2b053a5ef755e3181819dbde
Bernardo Raffide <bernardoraffide@pop-os.localdomain>
2021-09-12 21:23:59 -0300

    requisito 7 concluido

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { renderWithRouter } from '../components/utils/renderWithRouter';
+import { Pokedex } from '../components';
+import pokemons from '../data';
+
+describe('testes do componente Pokemon', () => {
+  test('testa se renderiza um card c infos de um pokemon', () => {
+    const { history } = renderWithRouter(<Pokedex />);
+    history.push('/');
+    const pikachuName = screen.getByText(pokemons[0].name);
+    expect(pikachuName).toBeInTheDocument();
+    const pikachuType = screen.getAllByText(pokemons[0].type);
+    expect(pikachuType).toBeInTheDocument();
+    const pikachuWeight = screen.getByText('Average weight: 6.0 kg');
+    expect(pikachuWeight).toBeInTheDocument();
+    const pikachuImage = screen.getByRole('img');
+    expect(pikachuImage).toHaveAttribute(
+      'src',
+      'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    );
+    expect(pikachuImage).toHaveAttribute('alt', `${pokemons[0].name} sprite`);
+  });
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,64 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithRouter } from '../components/utils/renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('testes do componente "PokemonDetails"', () => {
+  test('testa se aparece o texto com o "pokemonName Details"', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/');
+    const moreDetails = screen.getByText(/more details/i);
+    userEvent.click(moreDetails);
+    const pikachuDetails = screen.getByText('Pikachu Details');
+    expect(pikachuDetails).toBeInTheDocument();
+    expect(moreDetails).not.toBeInTheDocument();
+    const summary = screen.getByRole('heading', {
+      name: /summary/i,
+      level: 2,
+    });
+    expect(summary).toBeInTheDocument();
+    const pokemonResumo = screen.getByText(/this intelligent Pokémon roasts hard/i);
+    expect(pokemonResumo).toBeInTheDocument();
+  });
+  test('testa se existe uma secao c os mapas e as locations do pokemon', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/');
+    const moreDetails = screen.getByText(/more details/i);
+    userEvent.click(moreDetails);
+    const gameLocations = screen.getByRole('heading', {
+      name: /game Locations of/i,
+      level: 2,
+    });
+    expect(gameLocations).toBeInTheDocument();
+    const pokemonFirstLocationText = screen.getByText(/Kanto Viridian Forest/i);
+    expect(pokemonFirstLocationText).toBeInTheDocument();
+    const pikachuSecondLocation = screen.getByText(/Kanto Power Plant/i);
+    expect(pikachuSecondLocation).toBeInTheDocument();
+    const pikachuMaps = screen.getAllByAltText('Pikachu location');
+    expect(pikachuMaps[0]).toHaveAttribute(
+      'src',
+      pokemons[0].foundAt[0].map,
+    );
+    expect(pikachuMaps[1]).toHaveAttribute(
+      'src',
+      pokemons[0].foundAt[1].map,
+    );
+    expect(pikachuMaps[0]).toHaveAttribute('alt', `${pokemons[0].name} location`);
+    expect(pikachuMaps[1]).toHaveAttribute('alt', `${pokemons[0].name} location`);
+  });
+  test('testa se eh possivel favoritar o pokemon', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/');
+    const moreDetails = screen.getByText(/more details/i);
+    userEvent.click(moreDetails);
+    const favoritePokemonCheckbox = screen.getByRole('checkbox', {
+      name: /Pokémon favoritado?/i,
+    });
+    expect(favoritePokemonCheckbox).toBeInTheDocument();
+    const originalChecked = favoritePokemonCheckbox.checked;
+    userEvent.click(favoritePokemonCheckbox);
+    expect(favoritePokemonCheckbox.checked).toBe(!originalChecked);
+  });
+});

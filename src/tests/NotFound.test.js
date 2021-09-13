3a51d7e07502c1a35cd11a385d8b9b332be5d527
ViniciusMossi <vini_agg@hotmail.com>
2021-09-13 11:21:57 -0400

    implementa teste NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import RouterMemory from './RouterMemory';
+import NotFound from '../components/NotFound';
+
+describe('Requisito 4 NotFound', () => {
+  test('Verifica se há um h2 com o texto "Page requested not found 😭"', () => {
+    RouterMemory(<NotFound />);
+
+    const header = screen.getByRole('heading', { level: 2 });
+    expect(header.textContent).toBe('Page requested not found 😭');
+  });
+
+  test('página possui uma imagem', () => {
+    RouterMemory(<NotFound />);
+
+    const image = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(image).toBeInTheDocument();
+  });
+});

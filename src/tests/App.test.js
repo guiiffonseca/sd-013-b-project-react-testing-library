dcb6a5e736c30fd665722559adf34367577a1418
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-09-16 21:24:29 -0300

    RQ 1 - arquivo app

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,39 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+
+import App from '../App';
+
+describe('Teste se possui um nav', () => {
+  it('abrir a página inicial na URL /', () => {
+    const { history } = renderWithRouter(<App />);
+    userEvent.click(screen.getByText(/Home/i));
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/');
+    // const aboutAll = screen.getByText(/Encountered pokémons/);
+    // expect(aboutAll).toBeInTheDocument();
+  });
+  it('abrir About na URL /about', () => {
+    const { history } = renderWithRouter(<App />);
+    userEvent.click(screen.getByText(/About/i));
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/about');
+    // const aboutAll = screen.getByText(/About Pokédex/);
+    // expect(aboutAll).toBeInTheDocument();
+  });
+  it('abrir Pokémons Favoritados na URL /favorites', () => {
+    const { history } = renderWithRouter(<App />);
+    userEvent.click(screen.getByText(/Favorite Pokémons/i));
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/favorites');
+    // const aboutAll = screen.getByText(/Favorite pokémons/);
+    // expect(aboutAll).toBeInTheDocument();
+  });
+  it('abrir Not Found em outra URL.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe/');
+    const noMatch = screen.getByText(/Page requested not found/i);
+    expect(noMatch).toBeInTheDocument();
+  });
+});

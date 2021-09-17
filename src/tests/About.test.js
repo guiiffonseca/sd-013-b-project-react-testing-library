f3d1274a233e1efd5e4994e0ed8a12b9fc699802
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-09-16 22:04:17 -0300

    RQ 2 - About_V2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,12 +4,18 @@ import { screen } from '@testing-library/react';
 import About from '../components/About';
 
 const CONTEXT_HEADER = /One can filter Pokémons by type/;
-const POKEDEX_HEADER = 'About Pokédex';
+const POKEDEX_ABOUT = 'About Pokédex';
+// const CONTEXT_P = 'About Pokédex';
 
 describe('Teste o componente About', () => {
   it('Se contem Pokédex infos', () => {
     render(<About />);
     const info = screen.getByText(CONTEXT_HEADER);
     expect(info).toBeInTheDocument();
+  });
+  it('Se contem um heading', () => {
+    render(<About />);
+    const heading = screen.getByText(POKEDEX_ABOUT);
+    expect(heading).toBeInTheDocument();
   });
 });

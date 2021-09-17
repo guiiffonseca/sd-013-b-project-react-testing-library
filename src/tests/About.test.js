ec5166aff8125c4de34a625375646bfb4ba0287f
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-09-16 22:14:44 -0300

    RQ 2 - About_V4

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -9,6 +9,8 @@ const CONTEXT_P1 = (
   'This application simulates a Pokédex, a digital encyclopedia containing all Pokémons');
 const CONTEXT_P2 = (
   'One can filter Pokémons by type, and see more details for each one of them');
+const IMG_PATH = (
+  'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
 
 describe('Teste o componente About', () => {
   it('Se contem Pokédex infos', () => {
@@ -27,5 +29,10 @@ describe('Teste o componente About', () => {
     expect(pFirst).toBeInTheDocument();
     const pSecond = screen.getByText(CONTEXT_P2);
     expect(pSecond).toBeInTheDocument();
+  });
+  it('Se contem imagem', () => {
+    render(<About />);
+    const image = screen.getByRole('img');
+    expect(image.src).toEqual(IMG_PATH);
   });
 });

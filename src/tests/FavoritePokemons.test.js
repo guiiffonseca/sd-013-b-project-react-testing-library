e028b1d33efda6fa783a0a055e7f94766f02c73c
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-09-17 21:12:48 -0300

    RQ 3 - Pokemons Favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+import data from '../data';
+
+const NO_FAVORITE_MSG = /No favorite pokemon found/;
+
+describe('Teste o componente FavoritePokemons.js', () => {
+  it('Testa se há msg No "favorite pokemon found".', () => {
+    render(<FavoritePokemons pokemons={ [] } />);
+    expect(screen.getByText(NO_FAVORITE_MSG)).toBeInTheDocument();
+  });
+  it('Testa se exibe os pokémons favoritados.', () => {
+    const cards = [data[0], data[2]];
+    renderWithRouter(<FavoritePokemons pokemons={ cards } />);
+    expect(screen.getByText(/Pikachu/)).toBeInTheDocument();
+    expect(screen.getByText(/Caterpie/)).toBeInTheDocument();
+  });
+});

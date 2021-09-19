a289abbb19e2099bfefef0807e273d2815079360
Itaopara Neto <itaopara@gmail.com>
2021-09-19 14:37:55 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Teste o componente NotFound.js', () => {
+  test('Página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    expect(screen
+      .getByRole('heading', { level: 2, name: /Page requested not found/i }))
+      .toBeInTheDocument();
+  });
+  test('Página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByAltText(/rying because the page requested was not found/i);
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,5 +1,15 @@
-describe('', () => {
-  test('', () => {
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+import { NotFound } from '../components';
 
+describe('Teste o componente NotFound.js', () => {
+  test('Página contém um heading h2 com o texto Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+    expect(screen
+      .getByRole('heading', { level: 2, name: 'Page requested not found 😭' }))
+      .toBeInTheDocument();
   });
 });

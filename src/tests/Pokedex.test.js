7b9e588cc3aa1e3687f676fd8787444879bd19f3
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-09-20 13:42:15 -0300

    RQ 5 - Pokedex Page, Test 1 e 2

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import data from '../data';
+import App from '../App';
+
+const NEXT_POKEMON = 'next-pokemon';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  // it('Possui um Heading h2 com texto Encountered pokémons', () => {}); teste feito no app
+  it('Possui um Heading h2 com texto Encountered pokémons', () => {
+    renderWithRouter(<App />);
+    const heading = screen.getByText(/Encountered pokémons/);
+    expect(heading).toBeInTheDocument();
+  });
+  it('Exibe o próximo pokémon', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByTestId(NEXT_POKEMON)).toHaveTextContent('Próximo pokémon');
+
+    userEvent.click(screen.getByTestId(NEXT_POKEMON));
+    expect(screen.getByText('Charmander'));
+
+    for (let i = 1; i <= data.length - 1; i += 1) {
+      userEvent.click(screen.getByTestId(NEXT_POKEMON));
+    }
+    expect(screen.getByText('Pikachu'));
+  });
+  /* it('Exibe o próximo pokémon', () => {
+    renderWithRouter(<App />);
+
+  }); */
+});

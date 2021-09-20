e9ece2b61d9afe7c21ab6944ddebf1195d8c3aa9
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-09-20 17:31:51 -0300

    RQ 6 - Pokemon Page, 1, 2 e 3

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import data from '../data';
+import { Pokemon } from '../components';
+
+describe('Teste o componente <Pokemon.js />', () => {
+  const showPokemon = data[0];
+  renderWithRouter(<Pokemon pokemon={ showPokemon } isFavorite={ false } />);
+  it('Exibe o nome do pokemon corretamente', () => {
+    expect(screen.getByText(showPokemon.name)).toBeInTheDocument();
+  });
+  it('Exibe o tipo do pokemon corretamente', () => {
+    renderWithRouter(<Pokemon pokemon={ showPokemon } isFavorite={ false } />);
+    expect(screen.getByText(showPokemon.type)).toBeInTheDocument();
+  });
+  it('Exibe o peso do pokemon corretamente', () => {
+    renderWithRouter(<Pokemon pokemon={ showPokemon } isFavorite={ false } />);
+    const weightValue = showPokemon.averageWeight.value;
+    const unitM = showPokemon.averageWeight.measurementUnit;
+    const MSG_WEIGHT = `Average weight: ${weightValue} ${unitM}`;
+    expect(screen.getByTestId('pokemon-weight')).toHaveTextContent(MSG_WEIGHT);
+  });
+});

7618c1059884be68bc2b13d795085dc01775c593
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-09-20 15:01:12 -0300

    RQ 5 - Pokedex Page, Test 3

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import data from '../data';
 import App from '../App';
@@ -8,7 +8,6 @@ import App from '../App';
 const NEXT_POKEMON = 'next-pokemon';
 
 describe('Teste o componente <Pokedex.js />', () => {
-  // it('Possui um Heading h2 com texto Encountered pokémons', () => {}); teste feito no app
   it('Possui um Heading h2 com texto Encountered pokémons', () => {
     renderWithRouter(<App />);
     const heading = screen.getByText(/Encountered pokémons/);
@@ -21,10 +20,23 @@ describe('Teste o componente <Pokedex.js />', () => {
     userEvent.click(screen.getByTestId(NEXT_POKEMON));
     expect(screen.getByText('Charmander'));
 
+    // Clica no botão próximo 'i' vezes igual a quantidade de pokemons
     for (let i = 1; i <= data.length - 1; i += 1) {
       userEvent.click(screen.getByTestId(NEXT_POKEMON));
     }
     expect(screen.getByText('Pikachu'));
+  });
+  it('Exibe um pokémon por vez', () => {
+    renderWithRouter(<App />);
+    const pokemonsOnScreen = screen.getAllByTestId('pokemon-name');
+    expect(pokemonsOnScreen.length).toBe(1);
+  });
+  it('Exibe o botoes de filtro', () => {
+    const QTY_FILTERS = 7;
+    renderWithRouter(<App />);
+    const filtersOnScreen = screen.getAllByTestId('pokemon-type-button');
+    expect(filtersOnScreen.length).toBe(QTY_FILTERS);
+
   });
   /* it('Exibe o próximo pokémon', () => {
     renderWithRouter(<App />);

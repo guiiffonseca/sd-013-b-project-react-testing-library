99660fe342dcea059e48cb2aba650aaa3f971ce1
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-09-20 20:11:09 -0300

    RQ 7 - PokemonDetails Page, 1 e 2

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import data from '../data';
+import App from '../App';
+
+describe('Teste o componente <PokemonDetails.js />', () => {
+  const showPokemon = data[0];
+  const favorite = false;
+  beforeEach(() => {
+    const { history } = renderWithRouter(<App />);
+    history.push(`/pokemons/${showPokemon.id}`);
+  });
+
+  it('Exibe texto "Summary".', () => {
+    const h2 = screen.getByRole('heading', {
+      level: 2,
+      name: 'Summary',
+    });
+    expect(h2).toBeInTheDocument();
+  });
+  it('Não deve existir o link para Detalhes', () => {
+    expect(screen.queryByText(/More details/i)).not.toBeInTheDocument();
+  });
+});
+// it('', () => {});

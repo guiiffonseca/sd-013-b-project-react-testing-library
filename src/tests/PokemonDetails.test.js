daf89105f1effd8d1662da5215de99873acb1e23
paulrpn <paulrpn@live.com>
2021-09-20 16:59:41 -0300

    inicio requisito 7 + linter

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,134 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+const MAX_DETAILS = 3;
+
+describe('<PokemonDetails.js /> Integration Tests:', () => {
+  test('1) Se as informações detalhadas do Pokémon selecionado são mostradas na tela.',
+    () => {
+      const pokemonSelected = pokemons[0];
+      renderWithRouter(<App />);
+
+      const detailsLink = screen.getByRole('link', {
+        name: /more details/i,
+      });
+      userEvent.click(detailsLink);
+
+      const headingText = screen.getByRole('heading', {
+        level: 2,
+        name: `${pokemonSelected.name} Details`,
+      });
+      expect(headingText).toBeInTheDocument();
+
+      const pokemonDetailsText = screen.getAllByTestId(/pokemon-/);
+      expect(pokemonDetailsText).toHaveLength(MAX_DETAILS);
+      expect(detailsLink).not.toBeInTheDocument();
+
+      const summaryHeadingText = screen.getByRole('heading', {
+        level: 2,
+        name: 'Summary',
+      });
+      expect(summaryHeadingText).toBeInTheDocument();
+
+      const summaryContentText = screen.getByText('This intelligent Pokémon roasts'
+      + ' hard berries with electricity to make them tender enough to eat.');
+      expect(summaryContentText).toBeInTheDocument();
+    });
+
+  // test('2) Se é exibido o próximo Pokémon da lista quando'
+  // + ' o botão "Próximo pokémon" é clicado.',
+  // () => {
+  //   renderWithRouter(<Pokedex
+  //     pokemons={ [pokemons[0], pokemons[4], pokemons[5]] }
+  //     isPokemonFavoriteById={ { 25: true, 65: true, 151: true } }
+  //   />);
+
+  //   const nextPokemonButton = screen.getByRole('button', { name: NEXT_POKEMON });
+  //   expect(nextPokemonButton).toBeInTheDocument();
+
+  //   userEvent.click(nextPokemonButton);
+  //   const firstPokemonNameText = screen.getByText('Alakazam');
+  //   expect(firstPokemonNameText).toBeInTheDocument();
+
+  //   userEvent.click(nextPokemonButton);
+  //   const secondPokemonNameText = screen.getByText('Mew');
+  //   expect(secondPokemonNameText).toBeInTheDocument();
+
+  //   userEvent.click(nextPokemonButton);
+  //   const thirdPokemonNameText = screen.getByText('Pikachu');
+  //   expect(thirdPokemonNameText).toBeInTheDocument();
+  // });
+
+  // test('3) Se é mostrado apenas um Pokémon por vez.',
+  //   () => {
+  //     renderWithRouter(<Pokedex
+  //       pokemons={ pokemons }
+  //       isPokemonFavoriteById={ { 25: true } }
+  //     />);
+
+  //     const showPokemonsOnScreen = screen.getAllByTestId('pokemon-name');
+
+  //     expect(showPokemonsOnScreen).toHaveLength(1);
+  //   });
+
+  // test('4) Se a Pokédex tem os botões de filtro.',
+  //   () => {
+  //     const MAX_TYPES = 7;
+  //     renderWithRouter(<Pokedex
+  //       pokemons={ pokemons }
+  //       isPokemonFavoriteById={ { 25: true, 65: true, 151: true } }
+  //     />);
+
+  //     const allFilterButton = screen.getByRole('button', { name: 'All' });
+  //     const electricFilterButton = screen.getByRole('button', { name: 'Electric' });
+  //     const fireFilterButton = screen.getByRole('button', { name: 'Fire' });
+  //     const bugFilterButton = screen.getByRole('button', { name: 'Bug' });
+  //     const poisonFilterButton = screen.getByRole('button', { name: 'Poison' });
+  //     const psychicFilterButton = screen.getByRole('button', { name: 'Psychic' });
+  //     const normalFilterButton = screen.getByRole('button', { name: 'Normal' });
+  //     const dragonFilterButton = screen.getByRole('button', { name: 'Dragon' });
+
+  //     expect(allFilterButton).toBeInTheDocument();
+  //     expect(electricFilterButton).toBeInTheDocument();
+  //     expect(fireFilterButton).toBeInTheDocument();
+  //     expect(bugFilterButton).toBeInTheDocument();
+  //     expect(poisonFilterButton).toBeInTheDocument();
+  //     expect(psychicFilterButton).toBeInTheDocument();
+  //     expect(normalFilterButton).toBeInTheDocument();
+  //     expect(dragonFilterButton).toBeInTheDocument();
+
+  //     const pokemonTypeButtons = screen.getAllByTestId('pokemon-type-button');
+  //     expect(pokemonTypeButtons).toHaveLength(MAX_TYPES);
+
+  //     userEvent.click(psychicFilterButton);
+  //     const firstPokemonFilteredText = screen.getByText('Alakazam');
+  //     expect(firstPokemonFilteredText).toBeInTheDocument();
+
+  //     const nextPokemonButton = screen.getByRole('button', { name: NEXT_POKEMON });
+  //     userEvent.click(nextPokemonButton);
+  //     const secondPokemonFilteredText = screen.getByText('Mew');
+  //     expect(secondPokemonFilteredText).toBeInTheDocument();
+  //   });
+
+  // test('5) Se a Pokédex contém um botão para resetar o filtro.',
+  //   () => {
+  //     renderWithRouter(<Pokedex
+  //       pokemons={ [pokemons[0], pokemons[4], pokemons[5]] }
+  //       isPokemonFavoriteById={ { 25: true, 65: true, 151: true } }
+  //     />);
+
+  //     const allFilterButton = screen.getByRole('button', { name: 'All' });
+  //     userEvent.click(allFilterButton);
+  //     const firstPokemonFilteredText = screen.getByText('Pikachu');
+  //     expect(firstPokemonFilteredText).toBeInTheDocument();
+
+  //     const nextPokemonButton = screen.getByRole('button', { name: NEXT_POKEMON });
+  //     userEvent.click(nextPokemonButton);
+  //     const secondPokemonFilteredText = screen.getByText('Alakazam');
+  //     expect(secondPokemonFilteredText).toBeInTheDocument();
+  //   });
+});

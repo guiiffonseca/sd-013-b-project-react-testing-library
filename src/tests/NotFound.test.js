b1ce0c857db201d1dc98e078b4eb7cd77a8588cc
RafaelBamberg <raafaelbamberg@gmail.com>
2021-09-20 21:30:46 -0300

    notFound | PokemonDetails

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testing the component <NotFound.js />', () => {
+  it('if the page contains a h2 with the text: Page not found', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundText = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  it('Test if the page shows the image https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByAltText(/Pikachu crying/i);
+    expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,46 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testing the component <PokemonDetails.js />', () => {
+  beforeEach(() => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+  });
+  it('Test if the informations of the selected Pokemon are show in the screen', () => {
+    const detailsText = screen.getByRole('heading', {
+      name: /Pikachu details/i,
+      level: 2,
+    });
+    expect(detailsText).toBeInTheDocument();
+    const summaryText = screen.getByRole('heading', {
+      name: /summary/i,
+      level: 2,
+    });
+    expect(summaryText).toBeInTheDocument();
+    const aboutPokemon = screen.getByText(
+      /This intelligent Pokémon roasts hard berries/i,
+    );
+    expect(aboutPokemon).toBeInTheDocument();
+  });
+
+  it(`Test if exists in the page a section with the maps 
+  containing the locations of the pokemon`, () => {
+    const locations = screen.getByRole('heading', {
+      name: /Game Locations of Pikachu/i,
+      level: 2,
+    });
+    expect(locations).toBeInTheDocument();
+
+    const img = screen.getAllByAltText(/pikachu location/i);
+    expect(img[0].src).toContain('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+  });
+
+  it('Test if the user can favourite a pokemon using the details page', () => {
+    const favoriteButton = screen.getByLabelText(/Pokémon favoritado?/i);
+    fireEvent.click(favoriteButton);
+    const favoriteIcon = screen.getByAltText(/pikachu is marked as favorite/i);
+    expect(favoriteIcon).toBeInTheDocument();
+  });
+});

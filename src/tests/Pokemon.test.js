0f78d4af7d323c1ba32117d4a43177a63e7690b4
Bernardo Raffide <bernardoraffide@pop-os.localdomain>
2021-09-21 23:58:47 -0300

    n foi dessa vez haha

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -2,18 +2,17 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { renderWithRouter } from '../components/utils/renderWithRouter';
-import { Pokedex } from '../components';
 import pokemons from '../data';
 import App from '../App';
 
 describe('testes do componente Pokemon', () => {
   test('testa se renderiza um card c infos de um pokemon', () => {
-    const { history } = renderWithRouter(<Pokedex />);
+    const { history } = renderWithRouter(<App />);
     history.push('/');
     const pikachuName = screen.getByText(pokemons[0].name);
     expect(pikachuName).toBeInTheDocument();
     const pikachuType = screen.getAllByText(pokemons[0].type);
-    expect(pikachuType).toBeInTheDocument();
+    expect(pikachuType[0]).toBeInTheDocument();
     const pikachuWeight = screen.getByText('Average weight: 6.0 kg');
     expect(pikachuWeight).toBeInTheDocument();
     const pikachuImage = screen.getAllByRole('img');
@@ -22,6 +21,19 @@ describe('testes do componente Pokemon', () => {
       'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
     );
     expect(pikachuImage[0]).toHaveAttribute('alt', `${pokemons[0].name} sprite`);
+    pokemons.forEach(({ type, name, averageWeight, image }) => {
+      expect(screen.getByTestId('pokemon-name')).toHaveTextContent(name);
+      expect(screen.getByTestId('pokemon-type')).toHaveTextContent(type);
+      expect(screen.getByTestId('pokemon-weight')).toHaveTextContent(
+        `Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`,
+      );
+      expect(screen.getByRole('img')).toHaveAttribute('src', image);
+      expect(screen.getByRole('img')).toHaveAttribute('alt', `${name} sprite`);
+      const nextButton = screen.getByRole('button', {
+        name: /Próximo pokémon/i,
+      });
+      userEvent.click(nextButton);
+    });
   });
 
   test('if it is able to redirect to PokemonDetails', () => {
